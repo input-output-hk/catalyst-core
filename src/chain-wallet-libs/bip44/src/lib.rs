09207d59bce57d8b38db845dd3ae39227c4ecfb7
Nicolas Di Prima <nicolas@primetype.co.uk>
2020-03-23 14:17:14 +0000

    small cleanup and removal of tech debts

--- src/chain-wallet-libs/bip44/src/lib.rs
+++ src/chain-wallet-libs/bip44/src/lib.rs
@@ -4,90 +4,60 @@
 //! using BIP44 specification.
 //!
 
-use std::{error, fmt, result};
+use std::{fmt, result};
+use thiserror::Error;
 
 /// the BIP44 derivation path has a specific length
 pub const BIP44_PATH_LENGTH: usize = 5;
 
 /// the BIP44 derivation path has a specific purpose
-pub const BIP44_PURPOSE: u32 = 0x8000002C;
+pub const BIP44_PURPOSE: u32 = 0x8000_002C;
 
 /// the BIP44 coin type is set, by default, to cardano ada.
-pub const BIP44_COIN_TYPE: u32 = 0x80000717;
+pub const BIP44_COIN_TYPE: u32 = 0x8000_0717;
 
 /// the soft derivation is upper bounded
-pub const BIP44_SOFT_UPPER_BOUND: u32 = 0x80000000;
+pub const BIP44_SOFT_UPPER_BOUND: u32 = 0x8000_0000;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct Path(pub Vec<u32>);
 
 /// Error relating to `bip44`'s `Addressing` operations
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Error)]
 pub enum Error {
     /// this means the given `Path` has an incompatible length
     /// for bip44 derivation. See `BIP44_PATH_LENGTH` and `Addressing::from_path`.
+    #[error("Invalid length, expecting 5 but received {0}")]
     InvalidLength(usize),
 
     /// this means the given `Path` has an incompatible purpose
     /// for bip44 derivation. See `BIP44_PURPOSE` and `Addressing::from_path`.
+    #[error("Invalid purpose, expecting 0x8000002C but received 0x{0:x}")]
     InvalidPurpose(u32),
 
     /// this means the given `Path` has an incompatible coin type
     /// for bip44 derivation. See `BIP44_COIN_TYPE` and `Addressing::from_path`.
+    #[error("Invalid type, expecting 0x80000717 but received 0x{0:x}")]
     InvalidType(u32),
 
     /// this means the given `Path` has an incompatible account
     /// for bip44 derivation. That it is out of bound. Indeed
     /// the account derivation is expected to be a hard derivation.
+    #[error("Account out of bound, should have a hard derivation but received 0x{0:x}")]
     AccountOutOfBound(u32),
 
     /// this means the given `Path` has an incompatible change
     /// for bip44 derivation. That it is out of bound. Indeed
     /// the change derivation is expected to be a soft derivation.
+    #[error("Change out of bound, should have a soft derivation but received 0x{0:x}")]
     ChangeOutOfBound(u32),
 
     /// this means the given `Path` has an incompatible index
     /// for bip44 derivation. That it is out of bound. Indeed
     /// the index derivation is expected to be a soft derivation.
+    #[error("Index out of bound, should have a soft derivation but received 0x{0:x}")]
     IndexOutOfBound(u32),
 }
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            &Error::InvalidLength(given) => write!(
-                f,
-                "Invalid length, expecting {} but received {}",
-                BIP44_PATH_LENGTH, given
-            ),
-            &Error::InvalidPurpose(given) => write!(
-                f,
-                "Invalid purpose, expecting 0x{:x} but received 0x{:x}",
-                BIP44_PURPOSE, given
-            ),
-            &Error::InvalidType(given) => write!(
-                f,
-                "Invalid type, expecting 0x{:x} but received 0x{:x}",
-                BIP44_COIN_TYPE, given
-            ),
-            &Error::AccountOutOfBound(given) => write!(
-                f,
-                "Account out of bound, should have a hard derivation but received 0x{:x}",
-                given
-            ),
-            &Error::ChangeOutOfBound(given) => write!(
-                f,
-                "Change out of bound, should have a soft derivation but received 0x{:x}",
-                given
-            ),
-            &Error::IndexOutOfBound(given) => write!(
-                f,
-                "Index out of bound, should have a soft derivation but received 0x{:x}",
-                given
-            ),
-        }
-    }
-}
-impl error::Error for Error {}
 
 pub type Result<T> = result::Result<T, Error>;
 
@@ -95,31 +65,31 @@ pub type Result<T> = result::Result<T, Error>;
 pub struct Account(u32);
 impl Account {
     pub fn new(account: u32) -> Result<Self> {
-        if account >= 0x80000000 {
+        if account >= BIP44_SOFT_UPPER_BOUND {
             return Err(Error::AccountOutOfBound(account));
         }
         Ok(Account(account))
     }
 
-    pub fn get_account_number(&self) -> u32 {
+    pub fn get_account_number(self) -> u32 {
         self.0
     }
-    pub fn get_scheme_value(&self) -> u32 {
-        self.0 | 0x80000000
+    pub fn get_scheme_value(self) -> u32 {
+        self.0 | BIP44_SOFT_UPPER_BOUND
     }
 
-    pub fn change(&self, typ: AddrType) -> Result<Change> {
+    pub fn change(self, typ: AddrType) -> Result<Change> {
         match typ {
             AddrType::Internal => self.internal(),
             AddrType::External => self.external(),
         }
     }
 
-    pub fn internal(&self) -> Result<Change> {
-        Change::new(*self, 1)
+    pub fn internal(self) -> Result<Change> {
+        Change::new(self, 1)
     }
-    pub fn external(&self) -> Result<Change> {
-        Change::new(*self, 0)
+    pub fn external(self) -> Result<Change> {
+        Change::new(self, 0)
     }
 }
 impl fmt::Display for Account {
@@ -132,26 +102,26 @@ impl fmt::Display for Account {
 pub struct Index(u32);
 impl Index {
     pub fn new(index: u32) -> Result<Self> {
-        if index >= 0x80000000 {
+        if index >= BIP44_SOFT_UPPER_BOUND {
             return Err(Error::IndexOutOfBound(index));
         }
         Ok(Index(index))
     }
-    pub fn get_scheme_value(&self) -> u32 {
+    pub fn get_scheme_value(self) -> u32 {
         self.0
     }
-    pub fn incr(&self, i: u32) -> Result<Self> {
-        if i >= 0x80000000 {
+    pub fn incr(self, i: u32) -> Result<Self> {
+        if i >= BIP44_SOFT_UPPER_BOUND {
             return Err(Error::IndexOutOfBound(i));
         }
         let r = self.0 + i;
-        if r >= 0x80000000 {
+        if r >= BIP44_SOFT_UPPER_BOUND {
             return Err(Error::IndexOutOfBound(r));
         }
         Ok(Index(r))
     }
 
-    pub fn decr(&self, i: u32) -> Result<Self> {
+    pub fn decr(self, i: u32) -> Result<Self> {
         if self.0 < i {
             return Err(Error::IndexOutOfBound(0));
         }
@@ -167,20 +137,17 @@ pub struct Change {
 }
 impl Change {
     pub fn new(account: Account, change: u32) -> Result<Self> {
-        if change >= 0x80000000 {
+        if change >= BIP44_SOFT_UPPER_BOUND {
             return Err(Error::ChangeOutOfBound(change));
         }
-        Ok(Change {
-            account: account,
-            change: change,
-        })
+        Ok(Change { account, change })
     }
-    pub fn get_scheme_value(&self) -> u32 {
+    pub fn get_scheme_value(self) -> u32 {
         self.change
     }
 
-    pub fn index(&self, index: u32) -> Result<Addressing> {
-        Addressing::new_from_change(*self, index)
+    pub fn index(self, index: u32) -> Result<Addressing> {
+        Addressing::new_from_change(self, index)
     }
 }
 
@@ -214,7 +181,7 @@ impl Addressing {
         };
         Ok(Addressing {
             account: Account::new(account)?,
-            change: change,
+            change,
             index: Index::new(index)?,
         })
     }
@@ -272,7 +239,7 @@ impl Addressing {
     /// try to generate a new `Addressing` starting from the given
     /// `Addressing`'s index incremented by the given parameter;
     pub fn incr(&self, incr: u32) -> Result<Self> {
-        let mut addr = self.clone();
+        let mut addr = *self;
         addr.index = addr.index.incr(incr)?;
         Ok(addr)
     }

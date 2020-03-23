9ab9f2fffbf60215a02246ac4e424f8de26ab00a
Nicolas Di Prima <nicolas@primetype.co.uk>
2020-03-23 11:33:40 +0000

    Move the error type in its own repo

--- src/chain-wallet-libs/bip39/src/bip39.rs
+++ src/chain-wallet-libs/bip39/src/bip39.rs
@@ -47,52 +47,11 @@
 //! ```
 //!
 
+use crate::{Error, Result};
 use cryptoxide::hmac::Hmac;
 use cryptoxide::pbkdf2::pbkdf2;
 use cryptoxide::sha2::Sha512;
 use std::{fmt, ops::Deref, result, str};
-use thiserror::Error;
-
-/// Error regarding BIP39 operations
-#[derive(Debug, Error, PartialEq, Eq)]
-pub enum Error {
-    /// Received an unsupported number of mnemonic words. The parameter
-    /// contains the unsupported number. Supported values are
-    /// described as part of the [`Type`](./enum.Type.html).
-    #[error("Unsupported number of mnemonic words: {0}")]
-    WrongNumberOfWords(usize),
-
-    /// The entropy is of invalid size. The parameter contains the invalid size,
-    /// the list of supported entropy size are described as part of the
-    /// [`Type`](./enum.Type.html).
-    #[error("Unsupported mnemonic entropy size: {0}")]
-    WrongKeySize(usize),
-
-    /// The given mnemonic is out of bound, i.e. its index is above 2048 and
-    /// is invalid within BIP39 specifications.
-    #[error("The given mnemonic is out of bound, {0}")]
-    MnemonicOutOfBound(u16),
-
-    /// Forward error regarding dictionary operations.
-    #[error("Unknown mnemonic word")]
-    LanguageError(#[source] #[from] dictionary::Error),
-
-    /// the Seed is of invalid size. The parameter is the given seed size,
-    /// the expected seed size is [`SEED_SIZE`](./constant.SEED_SIZE.html).
-    #[error("Invalid Seed Size, expected 64 bytes, but received {0} bytes.")]
-    InvalidSeedSize(usize),
-
-    /// checksum is invalid. The first parameter is the expected checksum,
-    /// the second id the computed checksum. This error means that the given
-    /// mnemonics are invalid to retrieve the original entropy. The user might
-    /// have given an invalid mnemonic phrase.
-    #[error("Invalid Entropy's Checksum, expected {0:08b} but found {1:08b}")]
-    InvalidChecksum(u8, u8),
-}
-
-/// convenient Alias to wrap up BIP39 operations that may return
-/// an [`Error`](./enum.Error.html).
-pub type Result<T> = result::Result<T, Error>;
 
 /// BIP39 entropy is used as root entropy for the HDWallet PRG
 /// to generate the HDWallet root keys.
--- src/chain-wallet-libs/bip39/src/lib.rs
+++ src/chain-wallet-libs/bip39/src/lib.rs
@@ -1,4 +1,8 @@
 mod bits;
 mod bip39;
+mod error;
 
-pub use self::bip39::*;
+pub use self::{
+    bip39::*,
+    error::{Error, Result},
+};

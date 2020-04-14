6e86b44a7859179e7c2577bc8a07b923cf8f76f5
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2020-04-14 19:51:08 +0300

    Implement Display for Peer

--- src/chain-libs/chain-network/src/data/p2p.rs
+++ src/chain-libs/chain-network/src/data/p2p.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::net::SocketAddr;
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
@@ -18,5 +19,11 @@ impl From<SocketAddr> for Peer {
     #[inline]
     fn from(addr: SocketAddr) -> Self {
         Peer { addr }
+    }
+}
+
+impl fmt::Display for Peer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.addr)
     }
 }

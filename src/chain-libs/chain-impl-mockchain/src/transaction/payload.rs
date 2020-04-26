3644128b63919801d07042951686fb95cc350ecb
Nicolas Di Prima <nicolas@primetype.co.uk>
2020-04-26 22:24:05 +0100

    since the function consume the payload, use `into_` instead of `to_`

--- src/chain-libs/chain-impl-mockchain/src/transaction/payload.rs
+++ src/chain-libs/chain-impl-mockchain/src/transaction/payload.rs
@@ -91,7 +91,7 @@ impl<'a, P: Payload> PayloadAuthSlice<'a, P> {
 }
 
 impl<'a, P: Payload> PayloadSlice<'a, P> {
-    pub fn to_certificate_slice(self) -> Option<CertificateSlice<'a>> {
+    pub fn into_certificate_slice(self) -> Option<CertificateSlice<'a>> {
         <P as Payload>::to_certificate_slice(self)
     }
 }

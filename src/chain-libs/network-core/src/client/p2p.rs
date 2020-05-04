6bdc31aaef9d211d56584e7fbde7c618d641a234
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2020-05-04 14:06:34 +0300

    Remove folders network-core and network-grpc
    Clean up the code for old tokio 0.1 crates.

--- src/chain-libs/network-core/src/client/p2p.rs
+++
@@ -1,8 +0,0 @@
-use crate::gossip::NodeId;
-
-/// Base trait for the client services that use node identifiers to
-/// distinguish subscription streams.
-pub trait P2pService {
-    /// Network node identifier.
-    type NodeId: NodeId;
-}

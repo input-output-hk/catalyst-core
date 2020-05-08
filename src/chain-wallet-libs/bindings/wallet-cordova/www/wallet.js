b1625ec90b69783e5f67599c652a736b056d59d6
Enzo Cioppettini <ecioppettini@atixlabs.com>
2020-05-08 16:44:36 -0300

    improve plugin documentation
    also remove ad-hoc base64 function as cordova does that by default
    (magically) for ArrayBuffer arguments

--- src/chain-wallet-libs/bindings/wallet-cordova/www/wallet.js
+++ src/chain-wallet-libs/bindings/wallet-cordova/www/wallet.js
@@ -1,7 +1,3 @@
-/*
-    TODO: Add license?
- */
-
 var exec = require('cordova/exec');
 
 const NATIVE_CLASS_NAME = 'WalletPlugin';
@@ -10,18 +6,12 @@ const WALLET_RESTORE_ACTION_TAG = 'WALLET_RESTORE';
 const WALLET_RETRIEVE_FUNDS_ACTION_TAG = 'WALLET_RETRIEVE_FUNDS';
 const WALLET_TOTAL_FUNDS_ACTION_TAG = 'WALLET_TOTAL_FUNDS';
 const WALLET_ID_TAG = 'WALLET_ID';
+const WALLET_CONVERT_ACTION_TAG = 'WALLET_CONVERT';
+const CONVERSION_TRANSACTIONS_SIZE_ACTION_TAG = 'CONVERSION_TRANSACTIONS_SIZE';
+const CONVERSION_TRANSACTIONS_GET_ACTION_TAG = 'CONVERSION_TRANSACTIONS_GET';
 const WALLET_DELETE_ACTION_TAG = 'WALLET_DELETE';
 const SETTINGS_DELETE_ACTION_TAG = 'SETTINGS_DELETE';
-
-function _arrayBufferToBase64 (buffer) {
-    var binary = '';
-    var bytes = new Uint8Array(buffer);
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode(bytes[i]);
-    }
-    return window.btoa(binary);
-}
+const CONVERSION_DELETE_ACTION_TAG = 'CONVERSION_DELETE';
 
 /**
  * THOUGHTS/TODO
@@ -29,31 +19,43 @@ function _arrayBufferToBase64 (buffer) {
  * I'm still not sure what javascript features can we use here (ES6, can we bring dependencies?, promises?)
 */
 
+/**
+ * wallet module.
+ * @exports wallet-cordova-plugin.wallet
+ */
 var plugin = {
     /**
+     * @callback pointerCallback
+     * @param {number} ptr - callback that returns a pointer to a native object
+     */
+
+    /**
+     * @callback errorCallback
+     * @param {string} error - error description
+     */
+
+    /**
      * @param {string} mnemonics description (TODO)
-     * @param {function} successCallback description (TODO)
-     * @param {function} errorCallback description (TODO)
+     * @param {pointerCallback} successCallback description (TODO)
+     * @param {errorCallback} errorCallback this function can fail if the mnemonics are invalid
      */
     walletRestore: function (mnemonics, successCallback, errorCallback) {
-        console.log(mnemonics);
         exec(successCallback, errorCallback, NATIVE_CLASS_NAME, WALLET_RESTORE_ACTION_TAG, [mnemonics]);
     },
 
     /**
-     * @param block0 description (TODO)
-     * @param {function} successCallback description (TODO)
-     * @param {function} errorCallback description (TODO)
+     * @param {Uint8Array} block0 a byte array representing the block
+     * @param {function} successCallback returns a pointer to the blockchain settings extracted from the block
+     * @param {errorCallback} errorCallback this can fail if the block or the pointer are invalid
      */
     walletRetrieveFunds: function (ptr, block0, successCallback, errorCallback) {
-        const base64 = _arrayBufferToBase64(block0);
-        exec(successCallback, errorCallback, NATIVE_CLASS_NAME, WALLET_RETRIEVE_FUNDS_ACTION_TAG, [ptr, base64]);
+        exec(successCallback, errorCallback, NATIVE_CLASS_NAME, WALLET_RETRIEVE_FUNDS_ACTION_TAG, [ptr, block0.buffer]);
     },
 
     /**
-     * @param {string} native_id description (TODO)
-     * @param {string} successCallback description (TODO)
-     * @param {string} errorCallback description (TODO)
+     * @param {number} ptr a pointer to a wallet obtained with walletRestore
+     * @param {function} successCallback returns a number
+     * @param {errorCallback} errorCallback description (TODO)
      */
     walletTotalFunds: function (ptr, successCallback, errorCallback) {
         exec(successCallback, errorCallback, NATIVE_CLASS_NAME, WALLET_TOTAL_FUNDS_ACTION_TAG, [ptr]);
@@ -80,21 +82,59 @@ var plugin = {
     },
 
     /**
-     * @param {string} native_id description (TODO)
-     * @param {string} successCallback description (TODO)
-     * @param {string} errorCallback description (TODO)
+     * @param {number} walletPtr a pointer to a wallet obtained with walletRestore
+     * @param {number} settingsPtr a pointer to a settings object obtained with walletRetrieveFunds
+     * @param {pointerCallback} successCallback returns a Conversion object
+     * @param {errorCallback} errorCallback description (TODO)
+     */
+    walletConvert: function (walletPtr, settingsPtr, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, NATIVE_CLASS_NAME, WALLET_CONVERT_ACTION_TAG, [walletPtr, settingsPtr]);
+    },
+
+    /**
+     * @param {number} ptr a pointer to a Conversion object obtained with walletConvert
+     * @param {function} successCallback returns a number representing the number of transactions produced by the conversion
+     * @param {errorCallback} errorCallback description (TODO)
+     */
+    conversionTransactionsSize: function (ptr, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, NATIVE_CLASS_NAME, CONVERSION_TRANSACTIONS_SIZE_ACTION_TAG, [ptr]);
+    },
+
+    /**
+     * @param {number} ptr a pointer to a Conversion object obtained with walletConvert
+     * @param {number} index an index (starting from 0). Use conversionTransactionsSize to get the upper bound
+     * @param {function} successCallback callback that receives a transaction in binary form
+     * @param {errorCallback} errorCallback this function can fail if the index is out of range
+     */
+    conversionTransactionsGet: function (ptr, index, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, NATIVE_CLASS_NAME, CONVERSION_TRANSACTIONS_GET_ACTION_TAG, [ptr, index]);
+    },
+
+    /**
+     * @param {number} walletPtr a pointer to a Wallet obtained with walletRestore
+     * @param successCallback  indicates success. Does not return anything.
+     * @param {errorCallback} errorCallback
      */
     walletDelete: function (ptr, successCallback, errorCallback) {
         exec(successCallback, errorCallback, NATIVE_CLASS_NAME, WALLET_DELETE_ACTION_TAG, [ptr]);
     },
 
     /**
-     * @param {string} native_id description (TODO)
-     * @param {string} successCallback description (TODO)
-     * @param {string} errorCallback description (TODO)
+     * @param {number} ptr description (TODO)
+     * @param successCallback  indicates success. Does not return anything.
+     * @param {errorCallback} errorCallback
      */
     settingsDelete: function (ptr, successCallback, errorCallback) {
         exec(successCallback, errorCallback, NATIVE_CLASS_NAME, SETTINGS_DELETE_ACTION_TAG, [ptr]);
+    },
+
+    /**
+     * @param {number} ptr a pointer to a Conversion object obtained with walletConvert
+     * @param successCallback  indicates success. Does not return anything.
+     * @param {errorCallback} errorCallback
+     */
+    conversionDelete: function (ptr, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, NATIVE_CLASS_NAME, CONVERSION_DELETE_ACTION_TAG, [ptr]);
     }
 };
 

77c004ff9b5c25e3cc42b64a8b5521c2628e2149
Enzo Cioppettini <ecioppettini@atixlabs.com>
2020-05-08 17:13:32 -0300

    add negative index test for conversion transaction

--- src/chain-wallet-libs/bindings/wallet-jni/java/WalletTest.java
+++ src/chain-wallet-libs/bindings/wallet-jni/java/WalletTest.java
@@ -14,14 +14,14 @@ import static org.junit.Assert.assertEquals;
 public class WalletTest {
     @Test
     public void recoverWallet() throws IOException {
-        long walletPtr = Wallet.recover(
+        final long walletPtr = Wallet.recover(
                 "neck bulb teach illegal soul cry monitor claw amount boring provide village rival draft stone");
 
-        byte[] block0 = Files.readAllBytes(Paths.get("../../../test-vectors/block0"));
+        final byte[] block0 = Files.readAllBytes(Paths.get("../../../test-vectors/block0"));
 
-        long settingsPtr = Wallet.initialFunds(walletPtr, block0);
+        final long settingsPtr = Wallet.initialFunds(walletPtr, block0);
 
-        int totalValue = Wallet.totalValue(walletPtr);
+        final int totalValue = Wallet.totalValue(walletPtr);
 
         assertEquals(1000000 + 10000 + 10000 + 1 + 100, totalValue);
 
@@ -31,27 +31,48 @@ public class WalletTest {
 
     @Test
     public void convertWallet() throws IOException {
-        long walletPtr = Wallet.recover(
+        final long walletPtr = Wallet.recover(
                 "neck bulb teach illegal soul cry monitor claw amount boring provide village rival draft stone");
 
-        byte[] block0 = Files.readAllBytes(Paths.get("../../../test-vectors/block0"));
+        final byte[] block0 = Files.readAllBytes(Paths.get("../../../test-vectors/block0"));
 
-        long settingsPtr = Wallet.initialFunds(walletPtr, block0);
+        final long settingsPtr = Wallet.initialFunds(walletPtr, block0);
 
-        long conversionPtr = Wallet.convert(walletPtr, settingsPtr);
+        final long conversionPtr = Wallet.convert(walletPtr, settingsPtr);
 
-        int transactionsSize = Conversion.transactionsSize(conversionPtr);
+        final int transactionsSize = Conversion.transactionsSize(conversionPtr);
 
-        // System.out.println(Integer.toString(transactionsSize));
+        assertEquals(1, transactionsSize);
 
-        byte[] transaction = Conversion.transactionsGet(conversionPtr, 0);
+        final byte[] transaction = Conversion.transactionsGet(conversionPtr, 0);
 
-        // System.out.println(Integer.toString(transaction.length));
-        // TODO: try to assert something
-        assertEquals(true, true);
+        assertEquals(723, transaction.length);
 
         Conversion.delete(conversionPtr);
         Settings.delete(settingsPtr);
         Wallet.delete(walletPtr);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void negativeIndexConversionTransaction() throws IOException {
+        final long walletPtr = Wallet.recover(
+                "neck bulb teach illegal soul cry monitor claw amount boring provide village rival draft stone");
+
+        final byte[] block0 = Files.readAllBytes(Paths.get("../../../test-vectors/block0"));
+
+        final long settingsPtr = Wallet.initialFunds(walletPtr, block0);
+
+        final long conversionPtr = Wallet.convert(walletPtr, settingsPtr);
+
+        final int transactionsSize = Conversion.transactionsSize(conversionPtr);
+
+        try {
+            final byte[] transaction = Conversion.transactionsGet(conversionPtr, -1);
+        } catch (final Exception e) {
+            Conversion.delete(conversionPtr);
+            Settings.delete(settingsPtr);
+            Wallet.delete(walletPtr);
+            throw e;
+        }
     }
 }

889ec3551321414e208aabf48b0655af3a391f31
danielsanchezq <daniel.sanchez@iohk.io>
2020-05-20 10:44:05 +0200

    Bring back main

--- src/vit-servicing-station/src/main.rs
+++ src/vit-servicing-station/src/main.rs
@@ -5,7 +5,7 @@ use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() {
-    // let context = v0::context::new_default_context();
-    // let app = v0::filter(context);
-    // warp::serve(app).run(([127, 0, 0, 1], 3030)).await;
+    let context = v0::context::new_default_context();
+    let app = v0::filter(context);
+    warp::serve(app).run(([127, 0, 0, 1], 3030)).await;
 }

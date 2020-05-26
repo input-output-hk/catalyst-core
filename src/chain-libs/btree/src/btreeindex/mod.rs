be2206bfb46f33408c753c11e568ee3f73934cb4
Nicolas Di Prima <nicolas@primetype.co.uk>
2020-05-26 11:04:39 +0100

    remove non needed static lifetime details

--- src/chain-libs/btree/src/btreeindex/mod.rs
+++ src/chain-libs/btree/src/btreeindex/mod.rs
@@ -970,7 +970,7 @@ mod tests {
 
         let n: u64 = 2000;
 
-        tree.insert_many((0..n).into_iter().map(|i| (U64Key(i), i)))
+        tree.insert_many((0..n).map(|i| (U64Key(i), i)))
             .unwrap();
 
         assert_eq!(tree.get(&U64Key(100), |v| v.cloned()), Some(100));

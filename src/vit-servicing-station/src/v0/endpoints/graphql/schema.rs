fa287edf09d0102562de1a47dd9ee9040cd3fa6c
danielsanchezq <daniel.sanchez@iohk.io>
2020-05-28 16:03:44 +0200

    Move Proposal to db module Implement Serialize/Deserialize for Proposal

--- src/vit-servicing-station/src/v0/endpoints/graphql/schema.rs
+++ src/vit-servicing-station/src/v0/endpoints/graphql/schema.rs
@@ -1,32 +1,11 @@
 use crate::db;
-use crate::db::schema::proposals::dsl::proposals;
+use crate::db::{models::Proposal, schema::proposals::dsl::proposals};
 use async_graphql::*;
 use diesel::prelude::*;
-use diesel::Queryable;
 use std::sync::Arc;
 
-#[derive(Queryable)]
-pub struct Proposal {
-    pub id: i32,
-    pub proposal_category: String,
-    pub proposal_id: String,
-    pub proposal_title: String,
-    pub proposal_summary: String,
-    pub proposal_problem: String,
-    pub proposal_solution: String,
-    pub proposal_funds: i64,
-    pub proposal_url: String,
-    pub proposal_files_url: String,
-    pub proposer_name: String,
-    pub proposer_contact: String,
-    pub proposer_url: String,
-    pub chain_proposal_id: String,
-    pub chain_voteplan_id: String,
-    pub chain_proposal_index: i64,
-    pub chain_vote_start_time: i64,
-    pub chain_vote_end_time: i64,
-    pub chain_committee_end_time: i64,
-    pub chain_vote_options: String,
+pub struct QueryRoot {
+    pub db_connection_pool: Arc<db::DBConnectionPool>,
 }
 
 #[async_graphql::Object]
@@ -106,10 +85,6 @@ impl Proposal {
     pub async fn chain_vote_options(&self) -> &str {
         &self.chain_vote_options
     }
-}
-
-pub struct QueryRoot {
-    pub db_connection_pool: Arc<db::DBConnectionPool>,
 }
 
 #[Object]

c264f07dda4045095dcc25acd762297a3b8b7d86
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2020-06-02 18:36:01 +0300

    Add client support for legacy node IDs
    With this, introduce the client::Builder API to customize
    client features.

---
+++ src/chain-libs/chain-network/src/grpc/legacy.rs
@@ -0,0 +1,23 @@
+//! Support for legacy features.
+
+use rand_core::RngCore;
+
+const NODE_ID_LEN: usize = 24;
+
+/// Represents a randomly generated node ID such as was present in subscription
+/// requests and responses in Jormungandr versions prior to 0.9
+/// (as implemented in the old `network-grpc` crate).
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct NodeId([u8; NODE_ID_LEN]);
+
+impl NodeId {
+    pub fn generate(rng: &mut impl RngCore) -> Result<Self, rand_core::Error> {
+        let mut bytes = [0; NODE_ID_LEN];
+        rng.try_fill_bytes(&mut bytes)?;
+        Ok(NodeId(bytes))
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.0
+    }
+}

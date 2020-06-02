b75f11e2b45cf780b52bf17b6a40cf86a23c55f1
Enzo Cioppettini <ecioppettini@atixlabs.com>
2020-06-02 13:05:35 -0300

    add docs to plugin params

--- src/chain-wallet-libs/bindings/wallet-cordova/www/wallet.js
+++ src/chain-wallet-libs/bindings/wallet-cordova/www/wallet.js
@@ -19,6 +19,8 @@ const SETTINGS_DELETE_ACTION_TAG = 'SETTINGS_DELETE';
 const CONVERSION_DELETE_ACTION_TAG = 'CONVERSION_DELETE';
 const PROPOSAL_DELETE_ACTION_TAG = 'PROPOSAL_DELETE';
 
+const VOTE_PLAN_ID_LENGTH = 32;
+
 /**
  * THOUGHTS/TODO
  * add a more idiomatic abstraction on top of these primitive functions and expose that, something more similar to what wasm-bindgen does
@@ -134,16 +136,17 @@ var plugin = {
 
     /**
      *
+     * Get a signed transaction with a vote of `choice` to the given proposal, ready to be sent to the network.
      *
      * # Errors
      *
      * this function may fail if if any of the pointers are is null;
-     * @param {string} walletPtr a pointer to a Wallet object obtained with WalletRestore
-     * @param {string} settingsPtr
-     * @param {string} proposalPtr
-     * @param {number} choice
-     * @param {function} successCallback
-     * @param {function} errorCallback
+     * @param {string} walletPtr a pointer to a Wallet object obtained with walletRestore
+     * @param {string} settingsPtr a pointer to a Settings object obtained with walletRetrieveFunds
+     * @param {string} proposalPtr a pointer to a Proposal object obtained with proposalNew
+     * @param {number} choice a number between 0 and Proposal's numChoices - 1
+     * @param {function} successCallback on success the callback returns a byte array representing a transaction
+     * @param {function} errorCallback can fail if the choice doesn't validate with the given proposal
      *
      */
     walletVote: function (walletPtr, settingsPtr, proposalPtr, choice, successCallback, errorCallback) {
@@ -194,16 +197,21 @@ var plugin = {
     },
 
     /**
-     * @param {string} votePlanId
+     * Get a proposal object, used to validate the vote on `walletVote`
+     *
+     * @param {Uint8Array} votePlanId a byte array of 32 elements that identifies the voteplan
      * @param {PayloadType} payloadType
-     * @param {number} index
-     * @param {number} numChoices
+     * @param {number} index the index of the proposal in the voteplan
+     * @param {number} numChoices the number of choices of the proposal, used to validate the choice
      * @param {function} successCallback returns an object with ignored, and value properties
      * @param {errorCallback} errorCallback
      */
     proposalNew: function (votePlanId, payloadType, index, numChoices, successCallback, errorCallback) {
         argscheck.checkArgs('*nnnff', 'proposalNew', arguments);
-        if (require('cordova/utils').typeName(votePlanId) === 'Uint8Array') {
+
+        var isArray = require('cordova/utils').typeName(votePlanId) === 'Uint8Array';
+
+        if (isArray && votePlanId.length === VOTE_PLAN_ID_LENGTH) {
             exec(successCallback, errorCallback, NATIVE_CLASS_NAME, PROPOSAL_NEW_ACTION_TAG, [votePlanId, payloadType, index, numChoices]);
         } else {
             throw TypeError('expected votePlanId to be a Uint8Array in proposalNew');

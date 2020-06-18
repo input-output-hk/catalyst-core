13c027a0cc53add3edf8d9c1dc5e053db31afa17
danielsanchezq <daniel.sanchez@iohk.io>
2020-06-18 12:10:58 +0200

    Added sharedcontext constructor functions for testing

--- src/vit-servicing-station/src/v0/context.rs
+++ src/vit-servicing-station/src/v0/context.rs
@@ -11,10 +11,10 @@ pub struct Context {
 }
 
 impl Context {
-    pub fn new(db_connection_pool: db::DBConnectionPool, block0_path: &str) -> Self {
+    pub fn new(db_connection_pool: db::DBConnectionPool, block0: Vec<u8>) -> Self {
         Self {
             db_connection_pool,
-            block0: load_block0(block0_path),
+            block0,
         }
     }
 }
@@ -23,10 +23,28 @@ pub fn new_shared_context(
     db_connection_pool: db::DBConnectionPool,
     block0_path: &str,
 ) -> SharedContext {
-    let context = Context::new(db_connection_pool, block0_path);
+    let block0 = load_block0(block0_path);
+    let context = Context::new(db_connection_pool, block0);
     Arc::new(RwLock::new(context))
 }
 
 pub fn load_block0(block0_path: &str) -> Vec<u8> {
     std::fs::read(block0_path).unwrap()
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    use crate::db;
+
+    pub fn new_empty_test_shared_context() -> SharedContext {
+        let pool = db::load_db_connection_pool("").unwrap();
+        let block0: Vec<u8> = Vec::new();
+        Arc::new(RwLock::new(Context::new(pool, block0)))
+    }
+
+    pub fn new_test_shared_context(db_url: &str, block0_path: &str) -> SharedContext {
+        let pool = db::load_db_connection_pool(db_url).unwrap();
+        new_shared_context(pool, block0_path)
+    }
 }

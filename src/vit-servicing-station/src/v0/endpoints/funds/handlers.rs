94108eeebf2e1be96dee0d829e0b603d0eba81f8
danielsanchezq <daniel.sanchez@iohk.io>
2020-06-19 13:30:38 +0200

    Implemented test for get_fund_by_id

--- src/vit-servicing-station/src/v0/endpoints/funds/handlers.rs
+++ src/vit-servicing-station/src/v0/endpoints/funds/handlers.rs
@@ -53,7 +53,7 @@ pub mod test {
     }
 
     #[tokio::test]
-    async fn fund_by_id() {
+    async fn get_fund_handler() {
         // build context
         let shared_context = new_in_memmory_db_test_shared_context();
         let filter_context = shared_context.clone();
@@ -72,6 +72,36 @@ pub mod test {
             .and_then(get_fund);
 
         let result = warp::test::request().method("GET").reply(&filter).await;
+        assert_eq!(result.status(), warp::http::StatusCode::OK);
+        let result_fund: Fund =
+            serde_json::from_str(&String::from_utf8(result.body().to_vec()).unwrap()).unwrap();
+        assert_eq!(fund, result_fund);
+    }
+
+    #[tokio::test]
+    async fn get_fund_by_id_handler() {
+        // build context
+        let shared_context = new_in_memmory_db_test_shared_context();
+        let filter_context = shared_context.clone();
+        let with_context = warp::any().map(move || filter_context.clone());
+
+        // initialize db
+        let pool = &shared_context.read().await.db_connection_pool;
+        db_testing::initialize_db_with_migration(&pool);
+        let fund: Fund = get_test_fund();
+        populate_db_with_fund(&fund, &pool);
+
+        // build filter
+        let filter = warp::path!(i32)
+            .and(warp::get())
+            .and(with_context)
+            .and_then(get_fund_by_id);
+
+        let result = warp::test::request()
+            .method("GET")
+            .path("/1")
+            .reply(&filter)
+            .await;
         assert_eq!(result.status(), warp::http::StatusCode::OK);
         let result_fund: Fund =
             serde_json::from_str(&String::from_utf8(result.body().to_vec()).unwrap()).unwrap();

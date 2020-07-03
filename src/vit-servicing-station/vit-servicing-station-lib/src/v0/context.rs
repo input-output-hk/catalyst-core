36813294f4c6a390fac9f25f70703af72e496616
danielsanchezq <daniel.sanchez@iohk.io>
2020-07-03 11:19:33 +0200

    Gracefully exit and error reporting on application startup

--- src/vit-servicing-station/vit-servicing-station-lib/src/v0/context.rs
+++ src/vit-servicing-station/vit-servicing-station-lib/src/v0/context.rs
@@ -21,15 +21,10 @@ impl Context {
 
 pub fn new_shared_context(
     db_connection_pool: db::DBConnectionPool,
-    block0_path: &str,
+    block0: Vec<u8>,
 ) -> SharedContext {
-    let block0 = load_block0(block0_path);
     let context = Context::new(db_connection_pool, block0);
     Arc::new(RwLock::new(context))
-}
-
-pub fn load_block0(block0_path: &str) -> Vec<u8> {
-    std::fs::read(block0_path).unwrap()
 }
 
 #[cfg(test)]
@@ -45,6 +40,7 @@ pub mod test {
 
     pub fn new_test_shared_context(db_url: &str, block0_path: &str) -> SharedContext {
         let pool = db::load_db_connection_pool(db_url).unwrap();
-        new_shared_context(pool, block0_path)
+        let block0 = std::fs::read(block0_path).unwrap();
+        new_shared_context(pool, block0)
     }
 }

858f1126ed487164badffe1f3d319e127c2587d1
danielsanchezq <daniel.sanchez@iohk.io>
2020-07-13 15:05:33 +0200

    Added insert api_token db method Added test for insterting/retrieving api tokens

--- src/vit-servicing-station/vit-servicing-station-lib/src/db/queries/api_tokens.rs
+++ src/vit-servicing-station/vit-servicing-station-lib/src/db/queries/api_tokens.rs
@@ -5,6 +5,7 @@ use crate::db::{
 };
 use crate::v0::api_token::APIToken;
 use crate::v0::errors::HandleError;
+use chrono::{Duration, Utc};
 use diesel::query_dsl::RunQueryDsl;
 use diesel::{ExpressionMethods, OptionalExtension, QueryDsl};
 
@@ -22,4 +23,44 @@ pub async fn query_token(
     })
     .await
     .map_err(|_| HandleError::InternalError("Error executing request".to_string()))?
+}
+
+pub async fn insert_token(token: &APIToken, pool: &DBConnectionPool) -> Result<(), HandleError> {
+    let db_conn = pool.get().map_err(HandleError::DatabaseError)?;
+    let values = (
+        api_tokens::token.eq(token.as_ref().to_vec()),
+        api_tokens::creation_time.eq(Utc::now().timestamp()),
+        api_tokens::expire_time.eq((Utc::now() + Duration::days(365)).timestamp()),
+    );
+    tokio::task::spawn_blocking(move || {
+        diesel::insert_into(api_tokens::table)
+            .values(values)
+            .execute(&db_conn)
+            .map(|_| ())
+            .map_err(|e| HandleError::InternalError(e.to_string()))
+    })
+    .await
+    .map_err(|_| HandleError::InternalError("Error executing request".to_string()))?
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::db::{
+        load_db_connection_pool, models::api_tokens::APITokenData, testing as db_testing,
+        DBConnectionPool,
+    };
+
+    #[tokio::test]
+    async fn api_token_insert_and_retrieve() {
+        // initialize db
+        let pool: DBConnectionPool = load_db_connection_pool("").unwrap();
+        db_testing::initialize_db_with_migration(&pool);
+
+        // checks
+        let token = APIToken::new(b"foo_bar_zen".to_vec());
+        insert_token(&token, &pool).await.unwrap();
+        let token_data: APITokenData = query_token(token.clone(), &pool).await.unwrap().unwrap();
+        assert_eq!(token_data.token, token);
+    }
 }

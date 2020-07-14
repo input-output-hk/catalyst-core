6949f20cc53b11bd8bc56e33f1a1d6e4537499dc
Daniel <daniel.sanchez@iohk.io>
2020-07-14 15:48:47 +0200

    Implemented first cli version of generating and adding token

--- src/vit-servicing-station/vit-servicing-station-cli/src/app.rs
+++ src/vit-servicing-station/vit-servicing-station-cli/src/app.rs
@@ -1,4 +1,15 @@
+use chrono::{Duration, Utc};
+use rand::prelude::thread_rng;
+use rand::Rng;
 use structopt::StructOpt;
+use vit_servicing_station_lib::{
+    db::{
+        load_db_connection_pool, models::api_tokens::APITokenData,
+        queries::api_tokens::insert_token_data,
+    },
+    v0::api_token::APIToken,
+};
+
 pub trait ExecTask {
     type ResultValue;
     fn exec(&self) -> std::io::Result<<Self as ExecTask>::ResultValue>;
@@ -6,15 +17,18 @@ pub trait ExecTask {
 
 #[derive(StructOpt)]
 pub enum CLIApp {
-    APIToken(APIToken),
+    APIToken(APITokenCmd),
 }
 
 #[derive(Debug, PartialEq, StructOpt)]
-enum APIToken {
+enum APITokenCmd {
     // Add token to database
     Add {
         #[structopt(long = "token")]
         token: String,
+
+        #[structopt(long = "db-url")]
+        db_url: String,
     },
 
     // Generate a new token
@@ -24,24 +38,35 @@ enum APIToken {
     },
 }
 
-impl APIToken {
+impl APITokenCmd {
     fn generate(size: usize) -> String {
-        "foo".to_string()
+        let random_bytes: Vec<u8> = (0..size).map(|_| rand::thread_rng().gen::<u8>()).collect();
+        base64::encode_config(random_bytes, base64::URL_SAFE_NO_PAD)
     }
 
-    fn add_token(base64_token: String) {}
+    fn add_token(base64_token: String, db_url: &str) {
+        let token = base64::decode_config(base64_token, base64::URL_SAFE_NO_PAD).unwrap();
+        let api_token_data = APITokenData {
+            token: APIToken::new(token),
+            creation_time: Utc::now().timestamp(),
+            expire_time: (Utc::now() + Duration::days(365)).timestamp(),
+        };
+        let pool = load_db_connection_pool(db_url).unwrap();
+        let connection = pool.get().unwrap();
+        insert_token_data(api_token_data, &connection).unwrap();
+    }
 }
 
-impl ExecTask for APIToken {
+impl ExecTask for APITokenCmd {
     type ResultValue = ();
 
     fn exec(&self) -> std::io::Result<()> {
         match self {
-            APIToken::Add { token } => {
-                APIToken::add_token(token.clone());
+            APITokenCmd::Add { token, db_url } => {
+                APITokenCmd::add_token(token.clone(), &db_url);
             }
-            APIToken::Generate { size } => {
-                let token = APIToken::generate(*size);
+            APITokenCmd::Generate { size } => {
+                let token = APITokenCmd::generate(*size);
                 println!("{}", token);
             }
         };

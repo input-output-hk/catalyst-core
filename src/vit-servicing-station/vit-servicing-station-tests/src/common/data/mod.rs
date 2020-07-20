b863bd9f4eb3b06be361f9e0c7de910fc39caf76
dkijania <dariusz.kijania@iohk.io>
2020-07-20 11:37:22 +0200

    random token util

--- src/vit-servicing-station/vit-servicing-station-tests/src/common/data/mod.rs
+++ src/vit-servicing-station/vit-servicing-station-tests/src/common/data/mod.rs
@@ -1,6 +1,19 @@
+use rand_core::OsRng;
+use rand_core::RngCore;
 use vit_servicing_station_lib::db::models::{
     api_tokens::APITokenData, funds::Fund, proposals::Proposal, voteplans::Voteplan,
 };
+
+fn bytes() -> [u8; 32] {
+    let mut random_bytes: [u8; 32] = [0; 32];
+    let mut rng = OsRng;
+    rng.fill_bytes(&mut random_bytes);
+    random_bytes
+}
+
+pub fn invalid_token_hash() -> String {
+    base64::encode_config(bytes().to_vec(), base64::URL_SAFE_NO_PAD)
+}
 
 mod generator;
 pub use generator::{Generator, Snapshot};

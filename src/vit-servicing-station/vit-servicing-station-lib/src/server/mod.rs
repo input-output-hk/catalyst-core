08cca4c1e3a7366619d9195430c9a31b8b270619
danielsanchezq <daniel.sanchez@iohk.io>
2020-07-22 13:11:50 +0200

    Implemented log settings Implemented file logging

--- src/vit-servicing-station/vit-servicing-station-lib/src/server/mod.rs
+++ src/vit-servicing-station/vit-servicing-station-lib/src/server/mod.rs
@@ -1,5 +1,6 @@
 pub mod bootstrapping;
 pub mod exit_codes;
+pub mod log;
 pub mod settings;
 
 pub use bootstrapping::start_server;
--- src/vit-servicing-station/vit-servicing-station-server/src/main.rs
+++ src/vit-servicing-station/vit-servicing-station-server/src/main.rs
@@ -1,8 +1,7 @@
 use structopt::StructOpt;
-
 use vit_servicing_station_lib::{
-    db, server, server::exit_codes::ApplicationExitCode, server::settings as server_settings,
-    server::settings::ServiceSettings, v0,
+    db, server, server::exit_codes::ApplicationExitCode, server::log::config_log,
+    server::settings as server_settings, server::settings::ServiceSettings, v0,
 };
 
 #[tokio::main]
@@ -24,21 +23,32 @@ async fn main() {
     // dump settings and exit if specified
     if let Some(settings_file) = &settings.out_settings_file {
         server_settings::dump_settings_to_file(settings_file, &settings).unwrap_or_else(|e| {
-            println!("Error writing settings to file {}: {}", settings_file, e);
+            log::error!("Error writing settings to file {}: {}", settings_file, e);
             std::process::exit(ApplicationExitCode::WriteSettingsError.into())
         });
         std::process::exit(0);
     }
 
+    // setup log
+    config_log(
+        settings.log.log_level.into(),
+        settings.log.log_output_path.clone(),
+        None,
+    )
+    .unwrap_or_else(|e| {
+        log::error!("Error setting up log: {}", e);
+        std::process::exit(ApplicationExitCode::LoadSettingsError.into())
+    });
+
     // load db pool
     let db_pool = db::load_db_connection_pool(&settings.db_url).unwrap_or_else(|e| {
-        println!("Error connecting to database: {}", e);
+        log::error!("Error connecting to database: {}", e);
         std::process::exit(ApplicationExitCode::DBConnectionError.into())
     });
 
     // load block0
     let block0 = std::fs::read(&settings.block0_path).unwrap_or_else(|e| {
-        println!("Error loading block0 from {}: {}", &settings.block0_path, e,);
+        log::error!("Error loading block0 from {}: {}", &settings.block0_path, e,);
         std::process::exit(ApplicationExitCode::LoadBlock0Error.into())
     });
 
@@ -46,9 +56,10 @@ async fn main() {
 
     let app = v0::filter(context).await;
 
-    println!(
+    log::info!(
         "Running server at {}, database located at {}",
-        settings.address, settings.db_url
+        settings.address,
+        settings.db_url
     );
 
     // run server with settings

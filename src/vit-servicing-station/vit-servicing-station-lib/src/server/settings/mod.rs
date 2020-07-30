2b3b44f9ebfe71352e1932a7f76dc1d7d6421706
dkijania <dariusz.kijania@iohk.io>
2020-07-30 20:59:45 +0200

    exposed log level

--- src/vit-servicing-station/vit-servicing-station-lib/src/server/settings/mod.rs
+++ src/vit-servicing-station/vit-servicing-station-lib/src/server/settings/mod.rs
@@ -2,5 +2,6 @@ mod config;
 mod default;
 
 pub use config::{
-    dump_settings_to_file, load_settings_from_file, Cors, CorsOrigin, ServiceSettings, Tls,
+    dump_settings_to_file, load_settings_from_file, Cors, CorsOrigin, LogLevel, ServiceSettings,
+    Tls,
 };

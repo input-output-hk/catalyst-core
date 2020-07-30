824590d2a9329fe90c1d2bb69eb5bfed4c3ea9fa
dkijania <dariusz.kijania@iohk.io>
2020-07-30 21:00:36 +0200

    added Logger handled to server

--- src/vit-servicing-station/vit-servicing-station-tests/src/common/server.rs
+++ src/vit-servicing-station/vit-servicing-station-tests/src/common/server.rs
@@ -1,15 +1,22 @@
 use super::clients::{GraphqlClient, RestClient};
+use super::logger::Logger;
+use std::path::PathBuf;
 use std::process::Child;
 use vit_servicing_station_lib::server::settings::ServiceSettings;
 
 pub struct Server {
     process: Child,
     settings: ServiceSettings,
+    log_file: PathBuf,
 }
 
 impl Server {
-    pub fn new(process: Child, settings: ServiceSettings) -> Self {
-        Self { process, settings }
+    pub fn new(process: Child, settings: ServiceSettings, log_file: PathBuf) -> Self {
+        Self {
+            process,
+            settings,
+            log_file,
+        }
     }
 
     pub fn rest_client(&self) -> RestClient {
@@ -34,6 +41,10 @@ impl Server {
         let mut graphql_client = self.graphql_client();
         graphql_client.set_api_token(token.to_string());
         graphql_client
+    }
+
+    pub fn logger(&self) -> Logger {
+        Logger::new(self.log_file.clone())
     }
 
     pub fn is_token_valid(&self, token: &str) -> bool {

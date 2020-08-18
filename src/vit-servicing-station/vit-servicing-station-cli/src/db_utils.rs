82bf5941bd9cf7b673f7acccdb88406b1e0356a0
danielsanchezq <daniel.sanchez@iohk.io>
2020-08-18 14:42:29 +0200

    Added utils for backup db file

--- src/vit-servicing-station/vit-servicing-station-cli/src/db_utils.rs
+++ src/vit-servicing-station/vit-servicing-station-cli/src/db_utils.rs
@@ -1,4 +1,7 @@
+use std::fs;
 use std::io;
+use std::io::{Read, Write};
+use tempfile;
 
 pub fn db_file_exists(db_url: &str) -> io::Result<()> {
     // check if db file exists
@@ -9,4 +12,17 @@ pub fn db_file_exists(db_url: &str) -> io::Result<()> {
         ));
     }
     Ok(())
+}
+
+pub fn backup_db_file(db_url: &str) -> io::Result<fs::File> {
+    db_file_exists(db_url)?;
+    let mut tmp_file = tempfile::tempfile()?;
+    tmp_file.write_all(&fs::read(db_url)?)?;
+    Ok(tmp_file)
+}
+
+pub fn restore_db_file(mut backup_file: fs::File, db_url: &str) -> io::Result<()> {
+    let mut buff = Vec::new();
+    backup_file.read(&mut buff)?;
+    fs::write(db_url, &buff)
 }

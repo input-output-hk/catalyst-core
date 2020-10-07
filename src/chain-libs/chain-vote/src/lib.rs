cc015741bcbf31ba2eda486f0921b0eaae6382ff
danielsanchezq <daniel.sanchez@iohk.io>
2020-10-07 17:38:21 +0200

    Implemented state for Tally (private)

--- src/chain-libs/chain-vote/src/lib.rs
+++ src/chain-libs/chain-vote/src/lib.rs
@@ -155,6 +155,12 @@ impl Tally {
         (TallyState { r2s }, TallyDecryptShare { r1s: dshares })
     }
 
+    pub fn state(&self) -> TallyState {
+        TallyState {
+            r2s: self.r.iter().map(|r| r.elements().1.clone()).collect(),
+        }
+    }
+
     pub fn to_bytes(&self) -> Vec<u8> {
         use std::io::Write;
         let mut bytes: Vec<u8> = Vec::with_capacity(CIPHERTEXT_BYTES_LEN * self.r.len());

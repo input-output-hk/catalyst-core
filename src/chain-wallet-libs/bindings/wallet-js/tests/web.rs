7dda89eb280dea6458bfbff10b2f0449b24a5603
Enzo Cioppettini <ecioppettini@atixlabs.com>
2020-10-15 13:19:59 -0300

    wallet-js generate key from seed

--- src/chain-wallet-libs/bindings/wallet-js/tests/web.rs
+++ src/chain-wallet-libs/bindings/wallet-js/tests/web.rs
@@ -46,3 +46,24 @@ fn encrypt_decrypt() {
         &data[..]
     );
 }
+
+#[wasm_bindgen_test]
+fn gen_key_from_seed() {
+    let seed1 = [1u8; 32];
+    let key1 = Ed25519ExtendedPrivate::from_seed(seed1.as_ref()).unwrap();
+    let key2 = Ed25519ExtendedPrivate::from_seed(seed1.as_ref()).unwrap();
+
+    assert_eq!(key1.bytes(), key2.bytes());
+
+    let seed2 = [2u8; 32];
+    let key3 = Ed25519ExtendedPrivate::from_seed(seed2.as_ref()).unwrap();
+
+    assert_ne!(key3.bytes(), key1.bytes());
+}
+
+#[wasm_bindgen_test]
+fn gen_key_from_invalid_seed_fails() {
+    const INVALID_SEED_SIZE: usize = 32 + 1;
+    let bad_seed = [2u8; INVALID_SEED_SIZE];
+    assert!(Ed25519ExtendedPrivate::from_seed(bad_seed.as_ref()).is_err())
+}

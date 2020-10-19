a79d4943ba4d05a9e50806653e0f1237300a6b31
dkijania <dariusz.kijania@iohk.io>
2020-10-19 10:47:37 +0200

    added der cert to output generate_keys method
    update tests

--- src/jortestkit/tests/openssl.rs
+++ src/jortestkit/tests/openssl.rs
@@ -3,5 +3,5 @@ use assert_fs::TempDir;
 #[test]
 pub fn openssl_e2e() {
     let temp_dir = TempDir::new().unwrap();
-    let (_, _) = jortestkit::openssl::generate_keys(&temp_dir);
+    let (_, _, _) = jortestkit::openssl::generate_keys(&temp_dir);
 }

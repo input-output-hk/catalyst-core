ded27d3757acbafbd9923b0a657354993d6302e8
Michael Bishop <cleverca22@gmail.com>
2021-01-12 11:16:32 -0400

    move the migrations folder to fix nix builds

--- src/vit-servicing-station/vit-servicing-station-lib/src/db/migrations.rs
+++ src/vit-servicing-station/vit-servicing-station-lib/src/db/migrations.rs
@@ -1,6 +1,6 @@
 use crate::db::DBConnection;
 
-embed_migrations!("../migrations");
+embed_migrations!("./migrations");
 
 pub fn initialize_db_with_migration(db_conn: &DBConnection) {
     embedded_migrations::run(db_conn).unwrap();

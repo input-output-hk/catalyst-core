bdcdc38cc2f7c4c527565cd874e8971a8ded5a15
dkijania <dariusz.kijania@iohk.io>
2021-01-15 09:44:13 +0100

    move cli args to quick submodule

--- src/vit-testing/vitup/src/setup/quick/args.rs
+++ src/vit-testing/vitup/src/setup/quick/args.rs
@@ -1,5 +1,8 @@
-use crate::scenario::network::vote_backend;
-use crate::setup::QuickVitBackendSettingsBuilder;
+use super::QuickVitBackendSettingsBuilder;
+use crate::scenario::network::service_mode;
+use crate::scenario::network::{endless_mode, interactive_mode, setup_network};
+use crate::setup::quick::mode::parse_mode_from_str;
+use crate::setup::quick::Mode;
 use crate::Result;
 use jormungandr_scenario_tests::programs::prepare_command;
 use jormungandr_scenario_tests::{Context, Seed};
@@ -9,49 +12,21 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-pub enum VitCliCommand {
-    /// start backend
-    Start(StartCommand),
-}
-
-impl VitCliCommand {
-    pub fn exec(self) -> Result<()> {
-        match self {
-            Self::Start(start_command) => start_command.exec(),
-        }
-    }
-}
-
-#[derive(StructOpt, Debug)]
-pub enum StartCommand {
-    /// start backend from scratch
-    Quick(QuickStartCommandArgs),
-}
-
-impl StartCommand {
-    pub fn exec(self) -> Result<()> {
-        match self {
-            Self::Quick(quick_start_command) => quick_start_command.exec(),
-        }
-    }
-}
-
-#[derive(StructOpt, Debug)]
 #[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
 pub struct QuickStartCommandArgs {
     /// path or name of the jormungandr node to test
     #[structopt(long = "jormungandr", default_value = "jormungandr")]
-    jormungandr: PathBuf,
+    pub jormungandr: PathBuf,
 
     /// path or name of the jcli to test
     #[structopt(long = "jcli", default_value = "jcli")]
-    jcli: PathBuf,
+    pub jcli: PathBuf,
 
     /// set a directory in which the tests will be run, allowing every details
     /// to be save persistently. By default it will create temporary directories
     /// and will delete the files and documents
     #[structopt(long = "root-dir", default_value = ".")]
-    testing_directory: PathBuf,
+    pub testing_directory: PathBuf,
 
     /// in some circumstances progress bar can spoil test logs (e.g. on test build job)
     /// if this parametrer value is true, then no progress bar is visible,
@@ -63,19 +38,19 @@ pub struct QuickStartCommandArgs {
         default_value = "Monitor",
         parse(from_str = parse_progress_bar_mode_from_str)
     )]
-    progress_bar_mode: ProgressBarMode,
+    pub progress_bar_mode: ProgressBarMode,
 
     /// to set if to reproduce an existing test
     #[structopt(long = "seed")]
-    seed: Option<Seed>,
+    pub seed: Option<Seed>,
 
     /// level for all nodes
     #[structopt(long = "log-level", default_value = "info")]
-    log_level: String,
+    pub log_level: String,
 
     /// how many addresses to generate
     #[structopt(long = "initials")]
-    initials: Option<usize>,
+    pub initials: Option<usize>,
 
     /// json file which define funds for each account
     /// example:
@@ -84,63 +59,67 @@ pub struct QuickStartCommandArgs {
     ///   "10000",
     /// }
     #[structopt(long = "initials-mapping")]
-    initials_mapping: Option<PathBuf>,
+    pub initials_mapping: Option<PathBuf>,
 
     /// vote start epoch of vote plan
     #[structopt(long = "vote-start-epoch", default_value = "1")]
-    vote_start_epoch: u32,
+    pub vote_start_epoch: u32,
 
     /// vote start epoch of vote plan
     #[structopt(long = "tally-start-epoch", default_value = "2")]
-    tally_start_epoch: u32,
+    pub tally_start_epoch: u32,
 
     /// vote tally end epoch of vote plan
     #[structopt(long = "tally-end-epoch", default_value = "3")]
-    tally_end_epoch: u32,
+    pub tally_end_epoch: u32,
 
     #[structopt(long = "vote-start-timestamp")]
-    vote_start_timestamp: Option<String>,
+    pub vote_start_timestamp: Option<String>,
 
     /// vote start epoch of vote plan
     #[structopt(long = "tally-start-timestamp")]
-    tally_start_timestamp: Option<String>,
+    pub tally_start_timestamp: Option<String>,
 
     /// vote tally end epoch of vote plan
     #[structopt(long = "tally-end-timestamp")]
-    tally_end_timestamp: Option<String>,
+    pub tally_end_timestamp: Option<String>,
 
     /// vote tally end epoch of vote plan
     #[structopt(long = "next-vote-timestamp")]
-    next_vote_timestamp: Option<String>,
+    pub next_vote_timestamp: Option<String>,
 
     /// slot duration
     #[structopt(long = "slot-duration", default_value = "20")]
-    slot_duration: u8,
+    pub slot_duration: u8,
 
     /// slots in epoch
     #[structopt(long = "slots-in-epoch", default_value = "60")]
-    slots_in_epoch: u32,
+    pub slots_in_epoch: u32,
 
     /// proposals number
     #[structopt(long = "proposals", default_value = "10")]
-    proposals: u32,
+    pub proposals: u32,
 
     /// voting power threshold for participating in voting
     #[structopt(long = "voting-power", default_value = "8000000000")]
-    voting_power: u64,
+    pub voting_power: u64,
 
     /// interactive mode introduce easy way to interact with backend
     /// is capable of quering logs, sending transactions (e.g. tallying), etc.,
-    #[structopt(long = "interactive")]
-    interactive: bool,
+    #[structopt(
+        long = "mode",
+        default_value = "Endless",
+        parse(from_str = parse_mode_from_str)
+    )]
+    pub mode: Mode,
 
     /// endopint in format: 127.0.0.1:80
     #[structopt(long = "endpoint", default_value = "0.0.0.0:80")]
-    endpoint: String,
+    pub endpoint: String,
 
     /// switch to private voting type
     #[structopt(long = "private")]
-    private: bool,
+    pub private: bool,
 }
 
 impl QuickStartCommandArgs {
@@ -153,13 +132,13 @@ impl QuickStartCommandArgs {
         let seed = self
             .seed
             .unwrap_or_else(|| Seed::generate(rand::rngs::OsRng));
-        let testing_directory = self.testing_directory;
+        let mut testing_directory = self.testing_directory;
         let generate_documentation = true;
         let log_level = self.log_level;
-        let interactive = self.interactive;
+        let mode = self.mode;
         let endpoint = self.endpoint;
 
-        if interactive {
+        if mode == Mode::Interactive {
             progress_bar_mode = ProgressBarMode::None;
         }
 
@@ -167,7 +146,7 @@ impl QuickStartCommandArgs {
             seed,
             jormungandr,
             jcli,
-            Some(testing_directory),
+            Some(testing_directory.clone()),
             generate_documentation,
             progress_bar_mode,
             log_level,
@@ -210,6 +189,36 @@ impl QuickStartCommandArgs {
             .private(self.private);
 
         jormungandr_scenario_tests::introduction::print(&context, "VOTING BACKEND");
-        Ok(vote_backend(context, quick_setup, interactive, endpoint).map(|_| ())?)
+
+        testing_directory.push(quick_setup.title());
+        if testing_directory.exists() {
+            std::fs::remove_dir_all(&testing_directory)?;
+        }
+        match mode {
+            Mode::Service => service_mode(context, testing_directory, quick_setup, endpoint)?,
+            Mode::Endless => {
+                let (mut vit_controller, mut controller, vit_parameters) =
+                    quick_setup.build(context)?;
+                let (_nodes_list, _vit_station, _wallet_proxy) = setup_network(
+                    &mut controller,
+                    &mut vit_controller,
+                    vit_parameters,
+                    endpoint,
+                )?;
+                endless_mode()?;
+            }
+            Mode::Interactive => {
+                let (mut vit_controller, mut controller, vit_parameters) =
+                    quick_setup.build(context)?;
+                let (nodes_list, vit_station, wallet_proxy) = setup_network(
+                    &mut controller,
+                    &mut vit_controller,
+                    vit_parameters,
+                    endpoint,
+                )?;
+                interactive_mode(controller, nodes_list, vit_station, wallet_proxy)?;
+            }
+        }
+        Ok(())
     }
 }

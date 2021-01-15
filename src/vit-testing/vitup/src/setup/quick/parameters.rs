419826999dc5ccd88f878fcf7847d01f1ed251e8
dkijania <dariusz.kijania@iohk.io>
2021-01-15 09:41:34 +0100

    Quick backend parameters

---
+++ src/vit-testing/vitup/src/setup/quick/parameters.rs
@@ -0,0 +1,41 @@
+use chrono::NaiveDateTime;
+
+#[derive(Clone, Debug)]
+pub struct QuickVitBackendParameters {
+    pub initials: Vec<u64>,
+    pub vote_start: u64,
+    pub vote_tally: u64,
+    pub tally_end: u64,
+    pub vote_start_timestamp: Option<NaiveDateTime>,
+    pub tally_start_timestamp: Option<NaiveDateTime>,
+    pub tally_end_timestamp: Option<NaiveDateTime>,
+    pub next_vote_start_time: Option<NaiveDateTime>,
+    pub proposals: u32,
+    pub slot_duration: u8,
+    pub slots_per_epoch: u32,
+    pub voting_power: u64,
+    pub fund_name: String,
+    pub private: bool,
+}
+
+impl Default for QuickVitBackendParameters {
+    fn default() -> Self {
+        let initials: Vec<u64> = std::iter::from_fn(|| Some(10_000)).take(10).collect();
+        Self {
+            initials,
+            vote_start: 1,
+            vote_tally: 2,
+            tally_end: 3,
+            proposals: 100,
+            slot_duration: 20,
+            slots_per_epoch: 30,
+            voting_power: 8000_000000,
+            vote_start_timestamp: None,
+            tally_start_timestamp: None,
+            tally_end_timestamp: None,
+            next_vote_start_time: None,
+            fund_name: "fund_3".to_owned(),
+            private: false,
+        }
+    }
+}

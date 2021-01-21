e74d28fde29975bc5cb27d8b463f7f5dd0550b5f
dkijania <dariusz.kijania@iohk.io>
2021-01-22 00:26:25 +0100

    remove test

--- src/vit-testing/vitup/src/lib.rs
+++ src/vit-testing/vitup/src/lib.rs
@@ -11,23 +11,3 @@ use error::Result;
 
 use scenario::vit_station;
 use scenario::wallet;
-
-#[cfg(test)]
-pub mod tests {
-    use glob::{glob, glob_with};
-    use json;
-    use walkdir::WalkDir;
-    #[test]
-    pub fn test() {
-        let mut data = json::JsonValue::new_array();
-        let root = "vit_backend";
-        for entry in WalkDir::new(root) {
-            entry.path().display();
-
-            let mut data = json::JsonValue::new_array();
-
-            let entry = entry.unwrap();
-            println!("{}", entry.path().display());
-        }
-    }
-}

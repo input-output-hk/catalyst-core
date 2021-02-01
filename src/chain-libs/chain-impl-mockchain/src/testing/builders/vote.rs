9aff92e17274c00b9ddb8212418bc733339cd4ff
Yevhenii Babichenko <eugene.babichenko@iohk.io>
2021-02-01 15:53:32 +0200

    optimize private vote tally tests by reusing the optimization table across iterations

--- src/chain-libs/chain-impl-mockchain/src/testing/builders/vote.rs
+++ src/chain-libs/chain-impl-mockchain/src/testing/builders/vote.rs
@@ -1,25 +1,33 @@
 use crate::{
-    certificate::{PrivateTallyDecrypted, PrivateTallyDecryptedProposal}, testing::data::CommitteeMembersManager, vote::VotePlanStatus,
+    certificate::{PrivateTallyDecrypted, PrivateTallyDecryptedProposal},
+    testing::data::CommitteeMembersManager,
+    vote::VotePlanStatus,
 };
-pub fn build_tally_decrypt_share(
+
+pub fn decrypt_tally(
     vote_plan_status: &VotePlanStatus,
     members: &CommitteeMembersManager,
 ) -> PrivateTallyDecrypted {
-    let shares = vote_plan_status
+    let encrypted_tally = vote_plan_status
         .proposals
         .iter()
         .map(|proposal| {
-            let tally_state = proposal
-                .tally
-                .as_ref()
-                .unwrap();
-            let encrypted_tally = tally_state.private_encrypted()
-                .unwrap()
-                .0
-                .clone();
+            let tally_state = proposal.tally.as_ref().unwrap();
+            let encrypted_tally = tally_state.private_encrypted().unwrap().0.clone();
             let max_votes = tally_state.private_total_power().unwrap();
             (encrypted_tally, max_votes)
         })
+        .collect::<Vec<_>>();
+
+    let absolute_max_votes = encrypted_tally
+        .iter()
+        .map(|(_encrypted_tally, max_votes)| *max_votes)
+        .max()
+        .unwrap();
+    let table = chain_vote::TallyOptimizationTable::generate_with_balance(absolute_max_votes, 1);
+
+    let proposals = encrypted_tally
+        .into_iter()
         .map(|(encrypted_tally, max_votes)| {
             let decrypt_shares = members
                 .members()
@@ -27,15 +35,15 @@ pub fn build_tally_decrypt_share(
                 .map(|member| member.secret_key())
                 .map(|secret_key| encrypted_tally.finish(secret_key).1)
                 .collect::<Vec<_>>();
-            let tally_state= encrypted_tally.state();
-            let table = chain_vote::TallyOptimizationTable::generate_with_balance(max_votes, 1);
-            let tally = chain_vote::tally(max_votes, &tally_state, &decrypt_shares, &table).unwrap();
+            let tally_state = encrypted_tally.state();
+            let tally =
+                chain_vote::tally(max_votes, &tally_state, &decrypt_shares, &table).unwrap();
             PrivateTallyDecryptedProposal {
-                shares: decrypt_shares.into_boxed_slice(), 
+                shares: decrypt_shares.into_boxed_slice(),
                 decrypted: tally.votes.into_boxed_slice(),
             }
         })
         .collect::<Vec<_>>();
 
-    PrivateTallyDecrypted::new(shares)
+    PrivateTallyDecrypted::new(proposals)
 }

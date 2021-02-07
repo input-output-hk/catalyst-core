9ad9c758dfff0e6fbd31bd4e5a6eae413f656a50
dkijania <dariusz.kijania@iohk.io>
2021-02-07 01:12:55 +0100

    added api_token module

--- src/vit-testing/vitup/src/manager/mod.rs
+++ src/vit-testing/vitup/src/manager/mod.rs
@@ -1,8 +1,10 @@
+mod api_token;
 mod context;
 pub mod file_lister;
 mod rest;
 mod service;
 
+pub use api_token::*;
 pub use context::{ControlContext, ControlContextLock, State};
 pub use rest::{start_rest_server, ServerStopper};
 pub use service::ManagerService;

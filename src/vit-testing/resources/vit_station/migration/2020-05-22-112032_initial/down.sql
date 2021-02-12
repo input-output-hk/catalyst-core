713682ded476fdcb9e4ee811b137520295c2f118
dkijania <dariusz.kijania@iohk.io>
2021-02-12 16:58:26 +0100

    updated mock after challenges changes

--- src/vit-testing/resources/vit_station/migration/2020-05-22-112032_initial/down.sql
+++ src/vit-testing/resources/vit_station/migration/2020-05-22-112032_initial/down.sql
@@ -4,5 +4,4 @@ DROP TABLE IF EXISTS funds;
 DROP TABLE IF EXISTS voteplans;
 DROP TABLE IF EXISTS api_tokens;
 DROP TABLE IF EXISTS challenges;
-DROP TABLE IF EXISTS proposals_challenge_info;
 DROP VIEW IF EXISTS full_proposals_info;

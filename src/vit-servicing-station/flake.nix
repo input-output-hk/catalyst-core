d83f8d286d4610134f7f651999c11346c713e412
Michael Fellinger <michael.fellinger@iohk.io>
2021-02-19 15:49:03 +0100

    use rust.nix so we never have to update hashes again

--- src/vit-servicing-station/flake.nix
+++ src/vit-servicing-station/flake.nix
@@ -1,33 +1,39 @@
 {
   inputs = {
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:kreisys/flake-utils";
+    rust-nix.url = "github:input-output-hk/rust.nix/work";
+    rust-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, utils }:
-  utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
-  let
-    overlay = self: super: {
-      vit-servicing-station = self.callPackage (
-        { lib, rustPlatform, fetchFromGitHub, pkg-config, openssl, protobuf, rustfmt }:
-        rustPlatform.buildRustPackage rec {
-          pname = "vit-servicing-station";
-          version = "HEAD";
-          src = ./.;
-          cargoSha256 = "sha256-Y8bh+m3AzkCzL3ZfFDUXjnX6lFiR4f0xHVGDiojjbCw=";
-          nativeBuildInputs = [ pkg-config protobuf rustfmt ];
-          buildInputs = [ openssl ];
-          configurePhase =''
-            cc=$CC
-          '';
-          doCheck = false;
-          doInstallCheck = false;
-          PROTOC="${protobuf}/bin/protoc";
-          PROTOC_INCLUDE="${protobuf}/include";
-        }
-      ) {};
+  outputs = { self, nixpkgs, utils, rust-nix }:
+    let
+      workspaceCargo = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      inherit (workspaceCargo.workspace) members;
+    in utils.lib.simpleFlake {
+      inherit nixpkgs;
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      preOverlays = [ rust-nix ];
+      overlay = final: prev:
+        let lib = prev.lib;
+        in lib.listToAttrs (lib.forEach members (member:
+          lib.nameValuePair member (final.rust-nix.buildPackage {
+            inherit ((builtins.fromTOML
+              (builtins.readFile (./. + "/${member}/Cargo.toml"))).package)
+              name version;
+            root = ./.;
+            nativeBuildInputs = with final; [ pkg-config protobuf rustfmt ];
+            buildInputs = with final; [ openssl ];
+            PROTOC = "${final.protobuf}/bin/protoc";
+            PROTOC_INCLUDE = "${final.protobuf}/include";
+          })));
+      packages = { vit-servicing-station-cli, vit-servicing-station-lib
+        , vit-servicing-station-server, vit-servicing-station-tests }@pkgs:
+        pkgs;
+      devShell = { mkShell, rustc, cargo, pkg-config, openssl, protobuf }:
+        mkShell {
+          PROTOC = "${protobuf}/bin/protoc";
+          PROTOC_INCLUDE = "${protobuf}/include";
+          buildInputs = [ rustc cargo pkg-config openssl protobuf ];
+        };
     };
-    pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
-  in {
-    packages.vit-servicing-station = pkgs.vit-servicing-station;
-    inherit overlay;
-  });
 }

b0b468c4daab6a2ea07b541ddf27c6e407008206
dkijania <dariusz.kijania@iohk.io>
2021-02-22 09:44:33 +0100

    cargo clippy

--- src/vit-testing/vitup/src/setup/generate/snapshot.rs
+++ src/vit-testing/vitup/src/setup/generate/snapshot.rs
@@ -98,7 +98,7 @@ impl SnapshotCommandArgs {
 
             let wallet = controller.wallet(&file_name)?;
             let new_file_name = format!("{}_{}_{}.png", i, wallet.address(), self.global_pin);
-            i = i + 1;
+            i += 1;
             std::fs::rename(
                 path.clone(),
                 std::path::Path::new(path.parent().unwrap()).join(&new_file_name),

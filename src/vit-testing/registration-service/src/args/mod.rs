4e37a4a72d4a6ee691250ba4dde9562ebe9e1ccb
dkijania <dariusz.kijania@iohk.io>
2021-03-04 08:14:23 +0100

    registration service

---
+++ src/vit-testing/registration-service/src/args/mod.rs
@@ -0,0 +1,64 @@
+use crate::{
+    config::{read_config, Configuration},
+    service::ManagerService,
+    Context,
+};
+use std::sync::Mutex;
+use std::{path::PathBuf, sync::Arc};
+use structopt::StructOpt;
+use thiserror::Error;
+
+#[derive(StructOpt, Debug)]
+pub struct RegistrationServiceCommand {
+    #[structopt(long = "token")]
+    pub token: Option<String>,
+
+    #[structopt(long = "config")]
+    pub config: PathBuf,
+}
+
+impl RegistrationServiceCommand {
+    pub fn exec(self) -> Result<(), Error> {
+        let mut configuration: Configuration = read_config(&self.config)?;
+
+        if self.token.is_some() {
+            configuration.token = self.token;
+        }
+
+        let control_context = Arc::new(Mutex::new(Context::new(
+            configuration.clone(),
+            &configuration.result_dir,
+        )));
+
+        let mut manager = ManagerService::new(control_context.clone());
+        manager.spawn();
+
+        loop {
+            if let Some(job_id) = manager.request_to_start() {
+                let mut job_result_dir = configuration.result_dir.clone();
+                job_result_dir.push(job_id.to_string());
+                std::fs::create_dir_all(job_result_dir.clone())?;
+
+                let params_to_override = control_context.lock().unwrap().params_to_override();
+
+                let mut child = configuration.spawn_command(job_id,params_to_override).unwrap();
+
+                control_context.lock().unwrap().run_started().unwrap();
+
+                child.wait().unwrap();
+                control_context.lock().unwrap().run_finished().unwrap();
+            }
+            std::thread::sleep(std::time::Duration::from_secs(5));
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("cannot spawn configuration")]
+    CannotSpawnCommand(#[from] std::io::Error),
+    #[error("cannot read configuration")]
+    CannotReadConfiguration(#[from] crate::config::Error),
+    #[error("context error")]
+    Context(#[from] crate::context::Error),
+}
---
+++ src/vit-testing/registration-service/src/main.rs
@@ -0,0 +1,17 @@
+mod args;
+mod config;
+mod context;
+mod file_lister;
+mod rest;
+mod service;
+
+pub use args::{Error, RegistrationServiceCommand};
+pub use context::Context;
+use structopt::StructOpt;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Error> {
+    std::env::set_var("RUST_BACKTRACE", "full");
+
+    RegistrationServiceCommand::from_args().exec()
+}

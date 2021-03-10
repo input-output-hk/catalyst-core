8879697e31ad4b142a3bfaca495474fc00f01871
dkijania <dariusz.kijania@iohk.io>
2021-03-10 08:46:50 +0100

    remove proposals from wallet

--- src/vit-testing/iapyx/src/wallet.rs
+++ src/vit-testing/iapyx/src/wallet.rs
@@ -31,7 +31,6 @@ pub enum Error {
 }
 
 pub struct Wallet {
-    proposals: Vec<Proposal>,
     inner: Inner,
 }
 
@@ -45,8 +44,7 @@ impl Wallet {
     pub fn recover(mnemonics: &str, password: &[u8]) -> Result<Self, Error> {
         Ok(Self {
             inner: Inner::recover(mnemonics, password)
-                .map_err(|e| Error::CannotRecover(e.to_string()))?,
-            proposals: vec![],
+                .map_err(|e| Error::CannotRecover(e.to_string()))?
         })
     }
 
@@ -54,7 +52,6 @@ impl Wallet {
         Ok(Self {
             inner: Inner::recover_free_keys(secret_key, &[])
                 .map_err(|e| Error::CannotRecover(e.to_string()))?,
-            proposals: vec![],
         })
     }
 
@@ -62,7 +59,6 @@ impl Wallet {
         Ok(Self {
             inner: Inner::recover_free_keys(secret_key, &[*secret_key])
                 .map_err(|e| Error::CannotRecover(e.to_string()))?,
-            proposals: vec![],
         })
     }
 
@@ -132,10 +128,6 @@ impl Wallet {
         self.inner
             .vote(settings, proposal, choice)
             .map_err(|e| Error::CannotSendVote(e.to_string()))
-    }
-
-    pub fn set_proposals(&mut self, proposals: Vec<Proposal>) {
-        self.proposals = proposals;
     }
 
     pub fn identifier(&self) -> AccountIdentifier {

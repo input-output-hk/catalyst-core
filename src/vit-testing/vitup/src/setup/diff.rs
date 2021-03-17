706f77f066f33a1b1597290c1dd51a6f8b4508a5
dkijania <dariusz.kijania@iohk.io>
2021-03-17 13:30:07 +0100

    finish up functionality

--- src/vit-testing/vitup/src/setup/diff.rs
+++ src/vit-testing/vitup/src/setup/diff.rs
@@ -7,19 +7,30 @@ use diffy::PatchFormatter;
 use iapyx::WalletBackend;
 use jormungandr_lib::interfaces::Block0Configuration;
 use jortestkit::prelude::read_file;
+use std::fs::File;
+use std::io::Write;
 use std::path::{Path, PathBuf};
-use std::str::FromStr;
 use structopt::StructOpt;
 use vit_servicing_station_tests::common::startup::server::ServerBootstrapper;
 
 #[derive(StructOpt, Debug)]
 #[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
 pub struct DiffCommand {
-    #[structopt(short = "l", long = "local", default_value = "./data")]
+    #[structopt(short = "l", long = "local", default_value = "./data/vit_backend/")]
     pub local: PathBuf,
+
+    #[structopt(long = "vit-station", default_value = "vit-servicing-station-server")]
+    pub vit_station: PathBuf,
 
     #[structopt(short = "t", long = "target")]
     pub target: String,
+
+    /// output file. If not it will output to stdout
+    #[structopt(short = "o", long = "output")]
+    pub output: Option<PathBuf>,
+
+    #[structopt(short = "c", long = "color")]
+    pub color: bool,
 }
 
 impl DiffCommand {
@@ -29,29 +40,87 @@ impl DiffCommand {
 
         let local_genesis_yaml = Path::new(&self.local).join("genesis.yaml");
         let local_vit_db = Path::new(&self.local).join("vit_station/storage.db");
-        let remote_client = WalletBackend::new(self.target, Default::default());
+        let remote_client = WalletBackend::new(self.target.clone(), Default::default());
 
         let remote_genesis_yaml = remote.path().join("genesis_remote.yaml");
 
         decode_block0(remote_client.block0()?, remote_genesis_yaml.clone())?;
 
-        let local_genesis_content = read_file(local_genesis_yaml);
+        let local_genesis_content = read_file(&local_genesis_yaml);
         let remote_genesis_content = read_file(remote_genesis_yaml);
 
         let server = ServerBootstrapper::new()
             .with_db_path(local_vit_db.to_str().unwrap())
-            .start(&remote)?;
-        //       .start_with_exe(&remote, PathBuf::from_str("vit-servicing-station-server"))?;
+            .start_with_exe(&remote, self.vit_station.clone())?;
 
         let local_funds = server.rest_client().funds()?;
         let remote_funds = remote_client.funds()?;
 
+        let local_funds_content = serde_json::to_string_pretty(&local_funds)?;
+        let remote_funds_content = serde_json::to_string_pretty(&remote_funds)?;
+
+        let mut local_proposals = server.rest_client().proposals()?;
+        let mut remote_proposals = remote_client.proposals()?;
+
+        let local_proposals_content = serde_json::to_string_pretty(
+            &local_proposals.sort_by(|a, b| b.internal_id.cmp(&a.internal_id)),
+        )?;
+        let remote_proposals_content = serde_json::to_string_pretty(
+            &remote_proposals.sort_by(|a, b| b.internal_id.cmp(&a.internal_id)),
+        )?;
+
+        let address = server.settings().address;
+
         let patch = create_patch(&remote_genesis_content, &local_genesis_content);
-        let f = PatchFormatter::new().with_color();
-        println!("**** GENESIS YAML DIFF ****");
-        print!("{}", f.fmt_patch(&patch));
+        let f = get_formatter(self.color);
+        let block0_diff_header = format!(
+            "diff http://{}/api/v0/block0 {:?}\n",
+            self.target, local_genesis_yaml
+        );
+        let block0_diff = format!("{}\n", f.fmt_patch(&patch));
+
+        let patch = create_patch(&local_funds_content, &remote_funds_content);
+        let f = get_formatter(self.color);
+        let fund_diff_header = format!(
+            "diff http://{}/api/v0/fund {}/api/v0/fund\n",
+            self.target, address
+        );
+        let fund_diff = format!("{}\n", f.fmt_patch(&patch));
+
+        let patch = create_patch(&local_proposals_content, &remote_proposals_content);
+        let f = get_formatter(self.color);
+        let proposals_diff_header = format!(
+            "diff http://{}/api/v0/block0 {}/api/v0/proposals\n",
+            self.target, address
+        );
+        let proposals_diff = format!("{}\n", f.fmt_patch(&patch));
+
+        if let Some(output) = self.output {
+            let mut file = File::create(output)?;
+            file.write_all(block0_diff_header.as_bytes())?;
+            file.write_all(block0_diff.as_bytes())?;
+            file.write_all(fund_diff_header.as_bytes())?;
+            file.write_all(fund_diff.as_bytes())?;
+            file.write_all(proposals_diff_header.as_bytes())?;
+            file.write_all(proposals_diff.as_bytes())?;
+        } else {
+            print!("{}", block0_diff_header);
+            print!("{}", block0_diff);
+            print!("{}", fund_diff_header);
+            print!("{}", fund_diff);
+            print!("{}", proposals_diff_header);
+            print!("{}", proposals_diff);
+        }
         Ok(())
     }
+}
+
+fn get_formatter(color: bool) -> PatchFormatter {
+    let mut f = PatchFormatter::new();
+    if color {
+        f = f.with_color();
+    }
+    f
 }
 
 fn decode_block0<Q: AsRef<Path>>(block0: Vec<u8>, genesis_yaml: Q) -> Result<()> {

86efb5b643bad877056fe080147de1fc729fb844
Enzo Cioppettini <ecioppettini@atixlabs.com>
2021-03-18 17:47:26 -0300

    keep only used fields in Settings

--- src/chain-wallet-libs/wallet/src/blockchain.rs
+++ src/chain-wallet-libs/wallet/src/blockchain.rs
@@ -1,19 +1,17 @@
 use chain_addr::Discrimination;
 use chain_impl_mockchain::{
     block::Block,
-    fee::FeeAlgorithm as _,
-    ledger::{Error, Ledger, LedgerParameters, LedgerStaticParameters},
+    fee::{FeeAlgorithm as _, LinearFee},
+    header::HeaderId,
+    ledger::{Error, Ledger},
     transaction::Input,
-    vote::CommitteeId,
 };
-use chain_time::TimeEra;
-use std::time::{Duration, SystemTime};
 
 #[derive(Clone)]
 pub struct Settings {
-    pub static_parameters: LedgerStaticParameters,
-    pub parameters: LedgerParameters,
-    pub time_era: TimeEra,
+    pub fees: LinearFee,
+    pub discrimination: Discrimination,
+    pub block0_initial_hash: HeaderId,
 }
 
 impl Settings {
@@ -23,12 +21,11 @@ impl Settings {
 
         let static_parameters = ledger.get_static_parameters().clone();
         let parameters = ledger.get_ledger_parameters();
-        let time_era = ledger.era().clone();
 
         Ok(Self {
-            static_parameters,
-            parameters,
-            time_era,
+            fees: parameters.fees,
+            discrimination: static_parameters.discrimination,
+            block0_initial_hash: static_parameters.block0_initial_hash,
         })
     }
 
@@ -36,26 +33,12 @@ impl Settings {
     /// is covering at least its own input fees for a given transaction
     pub fn is_input_worth(&self, input: &Input) -> bool {
         let value = input.value();
-        let minimal_value = self.parameters.fees.fees_for_inputs_outputs(1, 0);
+        let minimal_value = self.fees.fees_for_inputs_outputs(1, 0);
 
         value > minimal_value
     }
 
-    // extract the block0 date-time
-    // seconds in unix time (seconds elapsed since 1-Jan-1970)
-    pub fn start_date_time(&self) -> SystemTime {
-        SystemTime::UNIX_EPOCH + Duration::from_secs(self.static_parameters.block0_start_time.0)
-    }
-
     pub fn discrimination(&self) -> Discrimination {
-        self.static_parameters.discrimination
-    }
-
-    pub fn time_era(&self) -> &TimeEra {
-        &self.time_era
-    }
-
-    pub fn committee(&self) -> &[CommitteeId] {
-        &self.parameters.committees
+        self.discrimination
     }
 }

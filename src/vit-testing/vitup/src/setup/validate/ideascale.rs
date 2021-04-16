02a627996fe135c1872505a6a944aa2c3b1e9b13
dkijania <dariusz.kijania@iohk.io>
2021-04-16 09:26:34 +0200

    expand validation for proposals

--- src/vit-testing/vitup/src/setup/validate/ideascale.rs
+++ src/vit-testing/vitup/src/setup/validate/ideascale.rs
@@ -1,4 +1,5 @@
 use crate::Result;
+use serde_json::Value;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
@@ -40,28 +41,100 @@ impl IdeascaleValidateCommand {
         std::env::set_var("RUST_BACKTRACE", "full");
         std::fs::create_dir_all(&self.output)?;
 
-        match parse_proposals(self.proposals.clone()) {
-            Ok(_) => (),
+        let mut proposals = match parse_proposals(self.proposals.clone()) {
+            Ok(_) => self.parse_proposals_as_value()?,
             Err(err) => {
-                if self.fix {
-                    println!("Attempt to fix {:?}..", self.proposals);
-                    self.try_to_fix_proposals()?;
-                } else {
+                if !self.fix {
                     panic!("Error while parsing {:?}: {:?}", self.proposals, err);
                 }
+                println!("Attempt to fix {:?}..", self.proposals);
+                self.try_to_fix_proposals_missing_type()?
             }
         };
+        self.check_proposals_wrong_syntax(&mut proposals);
+        if self.fix {
+            self.save_proposals(&mut proposals)?;
+        }
         parse_challenges(self.challenges)?;
         parse_funds(self.funds)?;
 
         Ok(())
     }
 
-    pub fn try_to_fix_proposals(&self) -> Result<()> {
+    pub fn save_proposals(&self, data: &mut Vec<Value>) -> Result<()> {
+        let content = serde_json::to_string_pretty(&data)?;
+        let output =
+            Path::new(&self.output).join(self.proposals.to_path_buf().file_name().unwrap());
+        println!("Corrected proposals: {:?}..", output);
+        let mut file = File::create(output)?;
+        file.write_all(content.as_bytes()).map_err(Into::into)
+    }
+
+    pub fn check_proposals_wrong_syntax(&self, data: &mut Vec<Value>) {
+        for proposal in data.iter_mut() {
+            if let Some(proposal_solution) = proposal.get_mut("proposal_solution") {
+                self.check_and_eventually_fix_proposal_syntax(proposal_solution);
+            }
+
+            if let Some(proposal_summary) = proposal.get_mut("proposal_summary") {
+                self.check_and_eventually_fix_proposal_syntax(proposal_summary);
+            }
+
+            if let Some(proposer_relevant_experience) =
+                proposal.get_mut("proposer_relevant_experience")
+            {
+                self.check_and_eventually_fix_proposal_syntax(proposer_relevant_experience);
+            }
+
+            if let Some(proposal_brief) = proposal.get_mut("proposal_brief") {
+                self.check_and_eventually_fix_proposal_syntax(proposal_brief);
+            }
+
+            if let Some(proposal_goal) = proposal.get_mut("proposal_goal") {
+                self.check_and_eventually_fix_proposal_syntax(proposal_goal);
+            }
+
+            if let Some(proposal_importance) = proposal.get_mut("proposal_importance") {
+                self.check_and_eventually_fix_proposal_syntax(proposal_importance);
+            }
+
+            if let Some(proposal_metrics) = proposal.get_mut("proposal_metrics") {
+                self.check_and_eventually_fix_proposal_syntax(proposal_metrics);
+            }
+
+            if let Some(proposal_summary) = proposal.get_mut("proposal_summary") {
+                self.check_and_eventually_fix_proposal_syntax(proposal_summary);
+            }
+        }
+    }
+
+    fn check_and_eventually_fix_proposal_syntax(&self, value: &mut Value) {
+        let illegal_chars_vec = vec!["**", "\\\\*", "\\\\", "\\*\\*", "\\-"];
+        for illegal_chars in illegal_chars_vec {
+            if value.as_str().as_ref().unwrap().contains(illegal_chars) {
+                if self.fix {
+                    let before = value.as_str().unwrap();
+                    let after = value.as_str().as_mut().unwrap().replace(illegal_chars, "");
+                    println!("Fixing illegal chars {}-{}", before, after);
+                    *value = Value::String(after);
+                } else {
+                    panic!(
+                        "illegal chars detected: {}",
+                        value.as_str().as_ref().unwrap()
+                    );
+                }
+            }
+        }
+    }
+
+    fn parse_proposals_as_value(&self) -> Result<Vec<Value>> {
         let mut data: serde_json::Value =
             serde_json::from_str(&jortestkit::file::read_file(&self.proposals))?;
+        Ok(data.as_array_mut().unwrap().to_vec())
+    }
 
-        let proposals = data.as_array_mut().unwrap();
+    pub fn try_to_fix_proposals_missing_type(&self) -> Result<Vec<Value>> {
+        let mut proposals = self.parse_proposals_as_value()?;
         for proposal in proposals.iter_mut() {
             let proposal_object = proposal.as_object_mut().unwrap();
             if !proposal_object.contains_key(&"challenge_type".to_string()) {
@@ -78,13 +151,6 @@ impl IdeascaleValidateCommand {
                 }
             }
         }
-
-        let content = serde_json::to_string_pretty(&data)?;
-        let output =
-            Path::new(&self.output).join(self.proposals.to_path_buf().file_name().unwrap());
-        println!("Corrected proposals: {:?}..", output);
-        let mut file = File::create(output)?;
-        file.write_all(content.as_bytes())?;
-        Ok(())
+        Ok(proposals.to_vec())
     }
 }

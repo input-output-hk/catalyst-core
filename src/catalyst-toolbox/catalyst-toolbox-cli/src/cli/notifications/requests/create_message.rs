ddf7a3553fa31fd0578a318988338ebfd2a8b387
danielsanchezq <daniel.sanchez@iohk.io>
2021-04-26 19:23:41 +0200

    Add serde and reqwests

--- src/catalyst-toolbox/catalyst-toolbox-cli/src/cli/notifications/requests/create_message.rs
+++ src/catalyst-toolbox/catalyst-toolbox-cli/src/cli/notifications/requests/create_message.rs
@@ -1,31 +1,42 @@
+use serde::Serialize;
+
 use std::collections::HashMap;
 
+#[derive(Serialize)]
 pub enum MessageParameters {
     ContentSettings(ContentSettings),
 }
 
+#[derive(Serialize)]
 pub enum Content {
     Plain(String),
     MultiLanguage(HashMap<String, String>),
 }
 
+#[derive(Serialize)]
 pub struct Geozone {
     lat: f32,
     lng: f32,
     range: usize,
 }
 
+#[derive(Serialize)]
 pub struct ContentSettings {
     send_date: String,
-    ignore_user_timezones: bool,
     content: Content,
+    ignore_user_timezones: bool,
     timezone: Option<String>,
     campaign: Option<String>,
     geozone: Option<Geozone>,
+    filter: Option<String>,
 }
 
+#[derive(Serialize)]
 pub struct CreateMessage {
+    /// API access token from Pushwoosh Control Panel
     auth: String,
+    /// Pushwoosh application code
     application: String,
+    ///
     notifications: Vec<MessageParameters>,
 }
--- src/catalyst-toolbox/catalyst-toolbox-cli/src/cli/notifications/requests/mod.rs
+++ src/catalyst-toolbox/catalyst-toolbox-cli/src/cli/notifications/requests/mod.rs
@@ -1,10 +1,11 @@
-pub mod common;
 pub mod create_message;
 
+#[derive(Serialize)]
 pub enum RequestData {
     CreateMessageRequest(create_message::CreateMessage),
 }
 
+#[derive(Serialize)]
 pub struct Request {
     request: RequestData,
 }

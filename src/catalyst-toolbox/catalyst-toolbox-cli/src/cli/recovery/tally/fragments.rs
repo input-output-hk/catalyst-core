2c7733c0fa5c22c4c3178c964ec0a32b30a0e85f
danielsanchezq <daniel.sanchez@iohk.io>
2021-05-06 12:23:00 +0200

    Improve interfaces for fragments iterators (gonna be migrated to jormungandr repository)

--- src/catalyst-toolbox/catalyst-toolbox-cli/src/cli/recovery/tally/fragments.rs
+++ src/catalyst-toolbox/catalyst-toolbox-cli/src/cli/recovery/tally/fragments.rs
@@ -4,43 +4,65 @@ use std::io::BufReader;
 use std::path::{Path, PathBuf};
 
 use bincode::Options;
+
+use crate::cli::recovery::tally::Error;
 use jormungandr_lib::interfaces::PersistentFragmentLog;
 
 struct FileFragments {
     reader: BufReader<fs::File>,
+    file_path: PathBuf,
 }
 
 struct FileFragmentsIterator {
     reader: BufReader<fs::File>,
+    file_path: PathBuf,
+    counter: usize,
 }
 
 impl FileFragments {
     pub fn from_path(file_path: PathBuf) -> std::io::Result<Self> {
-        fs::File::open(file_path).map(|file| Self {
+        fs::File::open(file_path.clone()).map(|file| Self {
             reader: BufReader::new(file),
+            file_path,
         })
     }
 }
 
 impl IntoIterator for FileFragments {
-    type Item = PersistentFragmentLog;
+    type Item = Result<PersistentFragmentLog, Error>;
     type IntoIter = FileFragmentsIterator;
 
     fn into_iter(self) -> Self::IntoIter {
         FileFragmentsIterator {
             reader: self.reader,
+            file_path: self.file_path,
+            counter: 0,
         }
     }
 }
 
 impl Iterator for FileFragmentsIterator {
-    type Item = PersistentFragmentLog;
+    type Item = Result<PersistentFragmentLog, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        // EOF reached when buffer is empty after reading successfully at least one.
+        // Then we stop the iterator.
+        if self.reader.buffer().is_empty() && self.counter != 0 {
+            return None;
+        }
         let codec = bincode::DefaultOptions::new()
             .with_fixint_encoding()
             .allow_trailing_bytes();
-        codec.deserialize_from(&mut self.reader).ok()
+        let current = self.counter;
+        self.counter += 1;
+        Some(
+            codec
+                .deserialize_from(&mut self.reader)
+                .map_err(|_| Error::DeserializeError {
+                    file: self.file_path.to_string_lossy().to_string(),
+                    entry: current,
+                }),
+        )
     }
 }
 
@@ -57,20 +79,18 @@ pub fn get_fragments_log_files_path(folder: &Path) -> io::Result<impl Iterator<I
 
 pub fn read_entries_from_files_path(
     entries: impl Iterator<Item = PathBuf>,
-) -> impl Iterator<Item = PersistentFragmentLog> {
-    let handles = entries.into_iter().map(|path| {
-        FileFragments::from_path(path.clone())
-            .expect(&format!("Could not open: {}", path.to_string_lossy()))
-    });
-    handles.flat_map(|handle| handle.into_iter())
+) -> io::Result<impl Iterator<Item = Result<PersistentFragmentLog, Error>>> {
+    let mut handles = Vec::new();
+    for entry in entries {
+        handles.push(FileFragments::from_path(entry)?);
+    }
+    Ok(handles.into_iter().flat_map(|handle| handle.into_iter()))
 }
 
 pub fn load_fragments_from_folder_path(
     folder: &Path,
-) -> io::Result<impl Iterator<Item = PersistentFragmentLog>> {
-    Ok(read_entries_from_files_path(get_fragments_log_files_path(
-        folder,
-    )?))
+) -> io::Result<impl Iterator<Item = Result<PersistentFragmentLog, Error>>> {
+    read_entries_from_files_path(get_fragments_log_files_path(folder)?)
 }
 
 #[cfg(test)]

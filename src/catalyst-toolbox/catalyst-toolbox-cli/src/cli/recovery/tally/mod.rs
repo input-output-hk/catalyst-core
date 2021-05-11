ae59e049f2200ac36a42d57437300f2f178f49d1
Daniel Sanchez Quiros <daniel.sanchez@iohk.io>
2021-05-11 15:53:42 +0200

    Implemented cli command for tally recovery

--- src/catalyst-toolbox/catalyst-toolbox-cli/src/cli/recovery/tally/mod.rs
+++ src/catalyst-toolbox/catalyst-toolbox-cli/src/cli/recovery/tally/mod.rs
@@ -1,16 +1,29 @@
 pub(crate) mod mockchain;
 
-use chain_core::property::Deserialize as _;
+use chain_core::property::{Deserialize, Fragment};
 use chain_impl_mockchain::block::Block;
-use jormungandr_lib::interfaces::load_persistent_fragments_logs_from_folder_path;
+use jcli_lib::utils::{
+    output_file::{Error as OutputFileError, OutputFile},
+    output_format::{Error as OutputFormatError, OutputFormat},
+};
+use jormungandr_lib::interfaces::{
+    load_persistent_fragments_logs_from_folder_path, VotePlanStatus,
+};
 
-use std::io::BufReader;
+use std::io::{BufReader, Write};
 use std::path::PathBuf;
 
 use structopt::StructOpt;
 
+#[allow(clippy::large_enum_variant)]
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+
+    #[error(transparent)]
+    SerializationError(#[from] serde_json::Error),
+
     #[error(transparent)]
     DeserializeError(#[from] jormungandr_lib::interfaces::FragmentLogDeserializeError),
 
@@ -23,7 +36,7 @@ pub enum Error {
     #[error(transparent)]
     Block0ConfigurationError(#[from] jormungandr_lib::interfaces::Block0ConfigurationError),
 
-    #[error("block0 do not contain any voteplan")]
+    #[error("Block0 do not contain any voteplan")]
     MissingVoteplanError,
 
     #[error("Could not verify transaction {id} signature with range {range:?}")]
@@ -31,13 +44,28 @@ pub enum Error {
         id: String,
         range: std::ops::Range<i32>,
     },
+
+    #[error(transparent)]
+    OutputFileError(#[from] OutputFileError),
+
+    #[error(transparent)]
+    OutputFormatError(#[from] OutputFormatError),
 }
 
 #[derive(StructOpt)]
 #[structopt(rename_all = "kebab")]
 pub struct Replay {
+    #[structopt(long)]
     block0_path: PathBuf,
+
+    #[structopt(long)]
     logs_path: PathBuf,
+
+    #[structopt(flatten)]
+    output: OutputFile,
+
+    #[structopt(flatten)]
+    output_format: OutputFormat,
 }
 
 fn read_block0(path: PathBuf) -> std::io::Result<Block> {
@@ -47,15 +75,28 @@ fn read_block0(path: PathBuf) -> std::io::Result<Block> {
 
 impl Replay {
     pub fn exec(self) -> Result<(), Error> {
-        // let Replay {
-        //     block0_path,
-        //     logs_path,
-        // } = self;
-        // let block0 = read_block0(block0_path)?;
-        // let fragments = load_persistent_fragments_logs_from_folder_path(&logs_path)?;
-        //
-        // let (ledger, failed) = mockchain::recover_ledger_from_logs(&block0, fragments)?;
+        let Replay {
+            block0_path,
+            logs_path,
+            output,
+            output_format,
+        } = self;
+        let block0 = read_block0(block0_path)?;
+        let fragments = load_persistent_fragments_logs_from_folder_path(&logs_path)?;
 
+        let (ledger, failed) = mockchain::recover_ledger_from_logs(&block0, fragments)?;
+        if !failed.is_empty() {
+            println!("{} fragments couldn't be properly processed", failed.len());
+            for failed_fragment in failed {
+                println!("{}", failed_fragment.id());
+            }
+        }
+        let voteplans = ledger.active_vote_plans();
+        let voteplan_status: Vec<VotePlanStatus> =
+            voteplans.into_iter().map(VotePlanStatus::from).collect();
+        let mut out_writter = output.open()?;
+        let content = output_format.format_json(serde_json::to_value(&voteplan_status)?)?;
+        out_writter.write_all(content.as_bytes())?;
         Ok(())
     }
 }

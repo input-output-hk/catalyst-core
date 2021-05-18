9b3228f539b714828e7892ab2be23cba4342bbf4
Enzo Cioppettini <ecioppettini@atixlabs.com>
2021-05-18 12:49:12 -0300

    cordova: add python script for test setup
    Running the tests right now involves multiple steps, and not only because of
    cordova. These steps are only documented in TESTING.md, but this is
    unwelcoming and error prone.
    
    This script tries to automatize that process, in a similar way to
    cordova-paramedic, but with some differences:
    
    - This doesn't force you to use a temporary directory and setup
    everything each time. This matters because you can save time when
    making changes by just re-installing the plugin or the tests plugin.
    
    - This doesn't do the whole thing that cordova paramedic does for
      collecting the test results, although that's not working for me with
      paramedic anyway.
    
    - This, of course, does the things that paramedic wouldn't be able to
      do, that is:
    
      - Build the native libraries.
      - Copy them to the right place.
      - Run the tests bundling command.
    
      This is important because it's easy to forget to do a step in an
      edit-compile-run cycle.

---
+++ src/chain-wallet-libs/bindings/wallet-cordova/tests/setup.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+# cordova create hello com.example.hello HelloWorld
+# cd hello
+# cordova platform add android
+# cordova plugin add cordova-plugin-test-framework
+# cordova plugin add this-plugin-path
+# cordova plugin add path-to-wallet-cordova/tests
+# sed 's/<content src="index.html" \/>/<content src="cdvtests\/index.html" \/>/' config.xml -i
+# cordova build
+# cordova run android
+
+import subprocess
+
+from pathlib import Path
+import subprocess
+import sys
+import shutil
+import argparse
+import os
+import re
+
+sys.path.append(str(Path(__file__).parent.parent))
+from build_jni import run as build_jni
+from build_ios import run as build_ios
+
+root_directory = Path("../../../")
+
+
+def sed(original: str, replacement: str, file: Path):
+    # TODO: this may have some problems, but I'm also not sure if I want to use
+    # `sed`, mostly for Windows compatibility
+    with open(file, "r") as config:
+        lines = config.readlines()
+
+    with open(file, "w") as config:
+        for line in lines:
+            config.write(re.sub(original, replacement, line))
+
+
+def create_hello_world(build_dir: Path):
+    os.makedirs(build_dir, exist_ok=True)
+
+    subprocess.call(
+        ["cordova", "create", "hello", "com.example.hello", "HelloWorld"],
+        cwd=build_dir,
+    )
+
+
+def install_test_framework(app_dir: Path):
+    subprocess.call(
+        ["cordova", "plugin", "add", "cordova-plugin-test-framework"], cwd=app_dir
+    )
+
+    sed(
+        '<content src="index.html" />',
+        '<content src="cdvtests/index.html" />',
+        app_dir / "config.xml",
+    )
+
+
+def install_platforms(app_dir: Path, android=True, ios=True):
+    if android:
+        subprocess.call(["cordova", "platform", "add", "android"], cwd=app_dir)
+
+    if ios:
+        subprocess.call(["cordova", "platform", "add", "ios"], cwd=app_dir)
+
+
+def install_main_plugin(app_dir: Path, reinstall=True, android=False, ios=False):
+    plugin_path = Path(__file__).parent.parent
+
+    print(f"plugin_path: {plugin_path}")
+
+    if reinstall:
+        subprocess.call(
+            ["cordova", "plugin", "rm", "wallet-cordova-plugin"], cwd=app_dir
+        )
+
+    if android:
+        build_jni(release=False)
+
+    if ios:
+        build_ios()
+
+    subprocess.call(["cordova", "plugin", "add", str(plugin_path)], cwd=app_dir)
+
+
+def install_test_plugin(app_dir: Path, reinstall=True):
+    tests_path = Path(__file__).parent
+
+    print(f"tests_path: {tests_path}")
+
+    subprocess.call(["npm", "run", "build"], cwd=tests_path)
+
+    if reinstall:
+        subprocess.call(
+            ["cordova", "plugin", "rm", "wallet-cordova-plugin-tests"], cwd=app_dir
+        )
+
+    subprocess.call(["cordova", "plugin", "add", str(tests_path)], cwd=app_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create test harness")
+
+    platform_choices = ["android", "ios"]
+
+    parser.add_argument(
+        "--platform", required=True, nargs="+", choices=platform_choices
+    )
+    parser.add_argument("command", choices=["setup", "plugin", "tests"])
+    parser.add_argument("-d", "--directory", type=Path)
+    parser.add_argument("-r", "--run", choices=platform_choices)
+
+    args = parser.parse_args()
+
+    android = "android" in args.platform
+    ios = "ios" in args.platform
+
+    build_dir = args.directory
+
+    app_dir = build_dir / "hello"
+
+    if args.command == "setup":
+        create_hello_world(build_dir)
+        install_platforms(app_dir, android=android, ios=ios)
+        install_test_framework(app_dir)
+        install_main_plugin(app_dir, reinstall=False, android=android, ios=ios)
+        install_test_plugin(app_dir, reinstall=False)
+
+    if args.command == "plugin":
+        install_main_plugin(app_dir, reinstall=True, android=android, ios=ios)
+
+    if args.command == "tests":
+        install_test_plugin(app_dir, reinstall=True)
+
+    subprocess.call(["cordova", "build"], cwd=app_dir)
+    if args.run:
+        subprocess.call(["cordova", "run", args.run], cwd=app_dir)

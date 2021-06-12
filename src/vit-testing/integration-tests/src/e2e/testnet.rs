f3e1ff52142c7f9ad4e679c441d5e6a4d122c557
dkijania <dariusz.kijania@iohk.io>
2021-06-12 13:08:39 +0200

    refactor e2e testnet test

--- src/vit-testing/integration-tests/src/e2e/testnet.rs
+++ src/vit-testing/integration-tests/src/e2e/testnet.rs
@@ -1,73 +1,121 @@
-use jortestkit::prelude::WaitBuilder;
-use registration_service::{
-    client::rest::RegistrationRestClient, context::State, request::Request,
+use crate::common::registration::do_registration;
+use crate::common::snapshot::do_snapshot;
+use crate::common::snapshot::wait_for_db_sync;
+use crate::common::{
+    asserts::VotePlanStatusAssert, vitup_setup, wait_until_folder_contains_all_qrs, Error, Vote,
+    VoteTiming,
 };
+use assert_fs::TempDir;
+use chain_impl_mockchain::header::BlockDate;
+use iapyx::Protocol;
+use jormungandr_testing_utils::testing::node::time;
+use jortestkit::prelude::WaitBuilder;
+use registration_service::context::State;
 use snapshot_trigger_service::{client::rest::SnapshotRestClient, config::JobParameters};
+use std::path::Path;
+use vit_servicing_station_tests::common::data::ArbitraryValidVotingTemplateGenerator;
+use vitup::scenario::network::setup_network;
+use vitup::setup::start::QuickVitBackendSettingsBuilder;
+const GRACE_PERIOD_FOR_SNAPSHOT: u64 = 300;
 
 #[test]
 pub fn e2e_flow_using_voter_registration_local_vitup_and_iapyx() {
-    let registration_token = std::env::var("REGISTRATION_TOKEN")
-        .unwrap_or_else(|_| "REGISTRATION_TOKEN not defined".to_owned());
-    let registration_address = std::env::var("REGISTRATION_ADDRESS")
-        .unwrap_or_else(|_| "REGISTRATION_ADDRESS not defined".to_owned());
-    let snapshot_token =
-        std::env::var("SNAPSHOT_TOKEN").unwrap_or_else(|_| "SNAPSHOT_TOKEN not defined".to_owned());
-    let snapshot_address = std::env::var("SNAPSHOT_ADDRESS")
-        .unwrap_or_else(|_| "SNAPSHOT_ADDRESS not defined".to_owned());
-    let payment_skey =
-        std::env::var("PAYMENT_SKEY").unwrap_or_else(|_| "PAYMENT_SKEY not defined".to_owned());
-    let payment_vkey =
-        std::env::var("PAYMENT_VKEY").unwrap_or_else(|_| "PAYMENT_VKEY not defined".to_owned());
-    let stake_skey =
-        std::env::var("STAKE_SKEY").unwrap_or_else(|_| "STAKE_SKEY not defined".to_owned());
-    let stake_vkey =
-        std::env::var("STAKE_VKEY").unwrap_or_else(|_| "STAKE_VKEY not defined".to_owned());
+    let temp_dir = TempDir::new().unwrap();
+    let result = do_registration(&temp_dir);
 
-    let registration_client = RegistrationRestClient::new_with_token(
-        registration_token.to_string(),
-        registration_address.to_string(),
-    );
+    result.assert_status_is_finished();
+    result.assert_qr_equals_to_sk();
 
-    let registration_request = Request {
-        payment_skey: payment_skey.to_string(),
-        payment_vkey: payment_vkey.to_string(),
-        stake_skey: stake_skey.to_string(),
-        stake_vkey: stake_vkey.to_string(),
+    let job_param = JobParameters {
+        slot_no: Some(result.slot_no().unwrap() + GRACE_PERIOD_FOR_SNAPSHOT),
+        threshold: 1_000_000,
     };
 
-    let registration_job_id = registration_client.job_new(registration_request).unwrap();
+    wait_for_db_sync();
+    let snapshot_result = do_snapshot(&temp_dir, job_param).unwrap();
 
-    let wait = WaitBuilder::new().tries(10).sleep_between_tries(10).build();
-    println!("waiting for registration job");
-    let registration_jobs_status = registration_client
-        .wait_for_job_finish(registration_job_id.clone(), wait)
-        .unwrap();
-    println!("{:?}", registration_jobs_status);
-
-    registration_client
-        .download_qr(registration_job_id.clone(), "C:\\tmp\\qr_1234.png")
+    let entry = snapshot_result
+        .by_address(&result.address())
+        .unwrap()
         .unwrap();
 
-    let snapshot_client = SnapshotRestClient::new_with_token(
-        snapshot_token.to_string(),
-        snapshot_address.to_string(),
-    );
+    let endpoint = "127.0.0.1:8080";
+    let vote_timing = VoteTiming::new(0, 1, 2);
+    let testing_directory = TempDir::new().unwrap().into_persistent();
+    let mut quick_setup = QuickVitBackendSettingsBuilder::new();
+    quick_setup
+        .vote_start_epoch(vote_timing.vote_start)
+        .tally_start_epoch(vote_timing.tally_start)
+        .tally_end_epoch(vote_timing.tally_end)
+        .slot_duration_in_seconds(2)
+        .slots_in_epoch_count(30)
+        .proposals_count(300)
+        .voting_power(1)
+        .private(false);
 
-    let job_param = match registration_jobs_status {
-        State::Finished { info, .. } => JobParameters {
-            slot_no: Some(info.slot_no),
-            threshold: 1_000_000,
-        },
-        _ => panic!("registration job should be already finished"),
+    println!("{:?}", testing_directory.path().to_path_buf());
+
+    let mut template_generator = ArbitraryValidVotingTemplateGenerator::new();
+    let (mut vit_controller, mut controller, vit_parameters, _) =
+        vitup_setup(quick_setup, testing_directory.path().to_path_buf());
+    let (nodes, vit_station, wallet_proxy) = setup_network(
+        &mut controller,
+        &mut vit_controller,
+        vit_parameters,
+        &mut template_generator,
+        endpoint.to_string(),
+        &Protocol::Http,
+        "2.0".to_owned(),
+    )
+    .unwrap();
+
+    let leader_1 = &nodes[0];
+    let wallet_node = &nodes[4];
+    let mut committee = controller.wallet("committee_1").unwrap();
+
+    // start mainnet wallets
+    let mut alice = vit_controller
+        .iapyx_wallet_from_qr(&result.qr_code(), &result.pin(), &wallet_proxy)
+        .unwrap();
+
+    let fund1_vote_plan = &controller.vote_plans()[0];
+    let fund2_vote_plan = &controller.vote_plans()[1];
+
+    alice
+        .vote_for(fund1_vote_plan.id(), 0, Vote::Yes as u8)
+        .unwrap();
+
+    alice
+        .vote_for(fund2_vote_plan.id(), 0, Vote::Yes as u8)
+        .unwrap();
+
+    let target_date = BlockDate {
+        epoch: 1,
+        slot_id: 5,
     };
+    time::wait_for_date(target_date.into(), leader_1.rest());
 
-    let snapshot_job_id = snapshot_client.job_new(job_param).unwrap();
+    controller
+        .fragment_sender()
+        .send_public_vote_tally(&mut committee, &fund1_vote_plan.clone().into(), wallet_node)
+        .unwrap();
 
-    let wait = WaitBuilder::new().tries(10).sleep_between_tries(10).build();
+    controller
+        .fragment_sender()
+        .send_public_vote_tally(&mut committee, &fund2_vote_plan.clone().into(), wallet_node)
+        .unwrap();
 
-    println!("waiting for snapshot job");
-    let snapshot_jobs_status = snapshot_client.wait_for_job_finish(snapshot_job_id.clone(), wait);
-    println!("{:?}", snapshot_jobs_status);
+    vote_timing.wait_for_tally_end(leader_1.rest());
 
-    let snapshot_file = snapshot_client.download_snapshot(snapshot_job_id, "C:\\tmp");
+    let vote_plans = leader_1.vote_plans().unwrap();
+    vote_plans.assert_all_proposals_are_tallied();
+    vote_plans.assert_proposal_tally(fund1_vote_plan.id(), 0, vec![0, u64::from(entry.value), 0]);
+    vote_plans.assert_proposal_tally(fund2_vote_plan.id(), 0, vec![0, u64::from(entry.value), 0]);
+
+    vit_station.shutdown();
+    wallet_proxy.shutdown();
+    for node in nodes {
+        node.shutdown().unwrap();
+    }
+    controller.finalize();
 }

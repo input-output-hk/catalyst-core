efa7336446b8e5e7e86f009eb0b48716d3d919fe
dkijania <dariusz.kijania@iohk.io>
2021-06-15 14:14:24 +0200

    update iapyx-stats app

--- src/vit-testing/iapyx/src/bin/iapyx-stats.rs
+++ src/vit-testing/iapyx/src/bin/iapyx-stats.rs
@@ -1,1 +1,11 @@
+use futures::future::FutureExt;
+use iapyx::cli::args::stats::IapyxStatsCommand;
+use structopt::StructOpt;
 
+#[tokio::main]
+pub async fn main() -> Result<(), iapyx::cli::args::stats::IapyxStatsCommandError> {
+    std::env::set_var("RUST_BACKTRACE", "full");
+    let cli_future = IapyxStatsCommand::from_args().exec().fuse();
+    tokio::pin!(cli_future);
+    signals_handler::with_signal_handler(cli_future).await
+}

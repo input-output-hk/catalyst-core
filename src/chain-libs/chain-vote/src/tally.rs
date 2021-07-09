7f87577f95f9d42683818b5905d2e9663fca92c8
iquerejeta <querejeta.azurmendi@iohk.io>
2021-07-09 14:22:36 +0100

    serialisation function

--- src/chain-libs/chain-vote/src/tally.rs
+++ src/chain-libs/chain-vote/src/tally.rs
@@ -265,6 +265,13 @@ impl ProvenDecryptShare {
         let proof = CorrectShareGenerationZkp::from_bytes(&bytes[GroupElement::BYTES_LEN..])?;
         Some(ProvenDecryptShare { r1, pi: proof })
     }
+
+    pub fn to_bytes(&self) -> [u8; Self::SIZE] {
+        let mut output = [0u8; Self::SIZE];
+        output[0..GroupElement::BYTES_LEN].copy_from_slice(&self.r1.to_bytes());
+        output[GroupElement::BYTES_LEN..].copy_from_slice(&self.pi.to_bytes());
+        output
+    }
 }
 
 impl TallyDecryptShare {
@@ -296,8 +303,7 @@ impl TallyDecryptShare {
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut out = Vec::new();
         for element in self.elements.iter() {
-            out.extend_from_slice(element.r1.to_bytes().as_ref());
-            out.extend_from_slice(&element.pi.to_bytes());
+            out.extend_from_slice(&element.to_bytes());
         }
         out
     }

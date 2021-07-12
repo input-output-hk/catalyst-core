585b9a952298b5d605c462874d3e2ace93dfcb47
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2021-07-12 15:25:39 +0200

    Refactored cli error system

--- src/vit-servicing-station/vit-servicing-station-cli/src/api_token.rs
+++ src/vit-servicing-station/vit-servicing-station-cli/src/api_token.rs
@@ -5,13 +5,37 @@ use rand::Rng;
 use std::collections::HashSet;
 use std::io;
 use structopt::StructOpt;
+use thiserror::Error;
 use vit_servicing_station_lib::{
     db::{
+        Error as DbPoolError,
         load_db_connection_pool, models::api_tokens::ApiTokenData,
         queries::api_tokens::insert_token_data, DbConnection,
     },
     v0::api_token::ApiToken,
 };
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("base64 encoded token `{token}` is not valid")]
+    Base64DecodeError{
+        #[source]
+        source: base64::DecodeError,
+        token: String
+    },
+
+    #[error("Error with database")]
+    DbError(#[from] diesel::result::Error),
+
+    #[error("Error connecting db pool")]
+    DbPoolError (#[from] DbPoolError),
+
+    #[error("Error connecting to db")]
+    DbConnectionError(#[from] r2d2::Error),
+
+    #[error(transparent)]
+    IoError(#[from] std::io::Error)
+}
 
 #[derive(Debug, PartialEq, StructOpt)]
 pub enum ApiTokenCmd {
@@ -49,7 +73,7 @@ impl ApiTokenCmd {
             .collect()
     }
 
-    fn add_tokens_from_stream(db_conn: &DbConnection) -> io::Result<()> {
+    fn add_tokens_from_stream(db_conn: &DbConnection) -> Result<(), Error> {
         let mut base64_tokens: Vec<String> = Vec::new();
         let mut input = String::new();
         while let Ok(n) = io::stdin().read_line(&mut input) {
@@ -63,40 +87,34 @@ impl ApiTokenCmd {
         ApiTokenCmd::add_tokens(&base64_tokens, db_conn)
     }
 
-    fn add_tokens(base64_tokens: &[String], db_conn: &DbConnection) -> io::Result<()> {
+    fn add_tokens(base64_tokens: &[String], db_conn: &DbConnection) -> Result<(), Error> {
         // filter duplicated tokens
         let base64_tokens: HashSet<String> = base64_tokens.iter().cloned().collect();
         for base64_token in base64_tokens {
             let token =
                 base64::decode_config(&base64_token, base64::URL_SAFE_NO_PAD).map_err(|e| {
-                    io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        format!(
-                            "base64 encoded token `{}` is not valid due to:\n {}",
-                            base64_token, e
-                        ),
-                    )
+                    Error::Base64DecodeError {
+                        source: e,
+                        token: base64_token
+                    }
                 })?;
             let api_token_data = ApiTokenData {
                 token: ApiToken::new(token),
                 creation_time: Utc::now().timestamp(),
                 expire_time: (Utc::now() + Duration::days(365)).timestamp(),
             };
-            insert_token_data(api_token_data, db_conn)
-                .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{}", e)))?;
+            insert_token_data(api_token_data, db_conn).map_err(Error::DbError)?;
         }
         Ok(())
     }
 
-    fn handle_api_token_add(tokens: &Option<Vec<String>>, db_url: &str) -> io::Result<()> {
+    fn handle_api_token_add(tokens: &Option<Vec<String>>, db_url: &str) -> Result<(), Error> {
         // check if db file exists
         db_file_exists(db_url)?;
 
-        let pool = load_db_connection_pool(db_url)
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, format!("{}", e)))?;
+        let pool = load_db_connection_pool(db_url).map_err(Error::DbPoolError)?;
         let db_conn = pool
-            .get()
-            .map_err(|e| io::Error::new(io::ErrorKind::NotConnected, format!("{}", e)))?;
+            .get()?;
 
         match tokens {
             // if not tokens are provided then listen to stdin for input ones
@@ -109,7 +127,7 @@ impl ApiTokenCmd {
     fn handle_api_token_add_whith_db_backup(
         tokens: &Option<Vec<String>>,
         db_url: &str,
-    ) -> io::Result<()> {
+    ) -> Result<(), Error> {
         let backup_file = backup_db_file(db_url)?;
         if let Err(e) = Self::handle_api_token_add(tokens, db_url) {
             restore_db_file(backup_file, db_url)?;
@@ -129,8 +147,9 @@ impl ApiTokenCmd {
 
 impl ExecTask for ApiTokenCmd {
     type ResultValue = ();
+    type Error = Error;
 
-    fn exec(&self) -> std::io::Result<()> {
+    fn exec(&self) -> Result<(), Error> {
         match self {
             ApiTokenCmd::Add { tokens, db_url } => {
                 ApiTokenCmd::handle_api_token_add_whith_db_backup(tokens, db_url)

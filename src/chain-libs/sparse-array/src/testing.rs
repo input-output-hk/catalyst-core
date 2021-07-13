60675a432c4be42c27780750df46538902b442e7
Yevhenii Babichenko <eugene.babichenko@iohk.io>
2021-07-13 16:40:30 +0300

    sparse-array: address comments in proptest

--- src/chain-libs/sparse-array/src/testing.rs
+++ src/chain-libs/sparse-array/src/testing.rs
@@ -5,7 +5,7 @@ const MAX_INDEX: usize = 255;
 /// Generates a list of indexes for testing sparse arrays and bitmaps.
 pub(crate) fn test_indexes() -> impl Strategy<Value = Vec<u8>> {
     // generate the list of free & occupied positions in a sparse aray
-    prop::bits::bool_vec::between(0, MAX_INDEX).prop_map(|occupied_entries| {
+    prop::bits::bool_vec::between(0, MAX_INDEX + 1).prop_map(|occupied_entries| {
         occupied_entries
             .into_iter()
             .enumerate()
@@ -20,7 +20,7 @@ pub(crate) fn test_indexes() -> impl Strategy<Value = Vec<u8>> {
 pub(crate) fn sparse_array_test_data() -> impl Strategy<Value = Vec<(u8, u8)>> {
     test_indexes().prop_flat_map(|indexes: Vec<u8>| {
         // populate an array with the given indexes
-        prop::collection::vec(0..=u8::MAX, indexes.len()).prop_map(move |values| {
+        prop::collection::vec(prop::arbitrary::any::<u8>(), indexes.len()).prop_map(move |values| {
             let indexes = indexes.clone();
             indexes.into_iter().zip(values.into_iter()).collect()
         })

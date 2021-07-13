9c7dfc001d7453458191b66302aad8216b943cc7
dkijania <dariusz.kijania@iohk.io>
2021-07-13 13:08:03 +0200

    refactor config module

---
+++ src/vit-testing/iapyx/src/load/config/artificial_user.rs
@@ -0,0 +1,29 @@
+use crate::NodeLoadConfig;
+use jortestkit::load::Configuration;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub enum RequestType {
+    #[serde(alias = "vote")]
+    Vote,
+    #[serde(alias = "account")]
+    Account,
+    #[serde(alias = "fund")]
+    Fund,
+    #[serde(alias = "challenges")]
+    Challenges,
+    #[serde(alias = "proposal")]
+    Proposal,
+    #[serde(alias = "proposals")]
+    Proposals,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub account: Configuration,
+    pub vote: NodeLoadConfig,
+    pub fund: Configuration,
+    pub challenges: Configuration,
+    pub proposal: Configuration,
+    pub proposals: Configuration,
+}
---
+++ src/vit-testing/iapyx/src/load/config/servicing_station.rs
@@ -0,0 +1,39 @@
+use jortestkit::load::Configuration;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use thiserror::Error;
+
+#[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub enum RequestType {
+    #[serde(alias = "fund")]
+    Fund,
+    #[serde(alias = "challenges")]
+    Challenges,
+    #[serde(alias = "proposal")]
+    Proposal,
+    #[serde(alias = "proposals")]
+    Proposals,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub config: HashMap<RequestType, Configuration>,
+    pub criterion: Option<u8>,
+    pub address: String,
+    pub use_https: bool,
+}
+
+impl Config {
+    pub fn get(&self, request_type: RequestType) -> Result<Configuration, Error> {
+        self.config
+            .get(&request_type)
+            .map(|x| x.clone())
+            .ok_or(Error::CannotFindConfigurationFor(request_type))
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("cannot find configuration for request type: {0:?}")]
+    CannotFindConfigurationFor(RequestType),
+}

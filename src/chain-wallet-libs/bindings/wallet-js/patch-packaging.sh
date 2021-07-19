1825d164e5bf4d9f44ad501c3f6ff0acc2b5802d
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2021-07-19 11:53:15 +0300

    Remove the packaging patch
    No longer needed with wasm-pack 0.10.

--- src/chain-wallet-libs/bindings/wallet-js/patch-packaging.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-# package.json needs to be fixed up due to a known issue in wasm-pack:
-# https://github.com/rustwasm/wasm-pack/issues/837
-
-cat pkg/package.json | jq '.files |= (.+ ["wallet_bg.js"] | unique)' > tmp.json
-mv tmp.json pkg/package.json

f336a651c3a5fa37869e81e85d0237185361c741
dkijania <dariusz.kijania@iohk.io>
2021-07-22 10:12:11 +0200

    add flake.nix

---
+++ src/catalyst-toolbox/flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:kreisys/flake-utils";
+    rust-nix.url = "github:input-output-hk/rust.nix/work";
+    rust-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, utils, rust-nix }:
+
+    utils.lib.simpleFlake rec {
+      inherit nixpkgs;
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      preOverlays = [ rust-nix overlay ];
+      overlay = final: prev: {
+         vit-kedqr = prev.rust-nix.buildPackage {
+            inherit ((builtins.fromTOML
+              (builtins.readFile (./Cargo.toml))).package)
+              name version;
+            root = ./.;
+          };
+      };
+      packages =
+        { vit-kedqr }@pkgs:
+        pkgs;
+      devShell = { mkShell, rustc, cargo, pkg-config, openssl }: mkShell {
+        buildInputs = [ rustc cargo pkg-config openssl ];
+      };
+    };
+}

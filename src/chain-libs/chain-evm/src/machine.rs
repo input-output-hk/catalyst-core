8a015fa7766c283fbf1c0dc3a6e114e7cef320b7
Joaquín Rosales <joaquin.rosales@iohk.io>
2021-07-28 22:12:58 -0500

    stub function that will execute evm runtime in machine module

---
+++ src/chain-libs/chain-evm/src/machine.rs
@@ -0,0 +1,8 @@
+//! Abstractions for the EVM runtime.
+//!
+//! This module contains types that abstract away implementation details from the `evm` crate,
+//! specially those involving node and stack configurations, and runtime execution.
+
+pub fn code_to_execute_evm_runtime() -> Result<(), ()> {
+    todo!("put together the puzzle of types needed to run evm code");
+}

29dcc9be15d168a55f5a04d293374400af36cc36
Joaquín Rosales <joaquin.rosales@iohk.io>
2021-07-30 14:39:53 -0500

    add types needed for VirtualMachine to impl Backend
    Basically brings in type aliases for required EVM types,
    and hooks up the AccountTrie as the state.
    
    This commit implements the evm::backend::Backend trait,
    which is needed to implement the StackState trait needed
    for execution.

--- src/chain-libs/chain-evm/src/machine.rs
+++ src/chain-libs/chain-evm/src/machine.rs
@@ -1,60 +1,104 @@
+//! # The Virtual Machine
+//!
 //! Abstractions for the EVM runtime.
 //!
 //! This module contains types that abstract away implementation details from the `evm` crate,
 //! specially those involving node and stack configurations, and runtime execution.
+//!
+//! ## Handler <- EVM Context Handler
+//! ## StackState<'config>
+//!
 
 use evm::{
-    backend::{ApplyBackend, Backend},
+    backend::{ApplyBackend, Backend, Basic, MemoryVicinity},
+    executor::StackState,
+    Config, Context,
 };
 use primitive_types::{H160, H256, U256};
 
+use crate::state::AccountTrie;
 
-pub struct Machine {
-    //
+pub type Environment = MemoryVicinity;
+
+/// The context of the EVM runtime
+pub type RuntimeContext = Context;
+
+/// Top-level abstraction for the EVM with the
+/// necessary types used to get the runtime going.
+pub struct VirtualMachine {
+    _context: RuntimeContext,
+    config: MachineConfig,
+    state: AccountTrie,
 }
 
-impl Backend for Machine {
+/// EVM configuration
+pub struct MachineConfig {
+    _evm_config: Config,
+    environment: Environment,
+}
+
+impl Backend for VirtualMachine {
     fn gas_price(&self) -> U256 {
-        todo!();
+        self.config.environment.gas_price
     }
     fn origin(&self) -> H160 {
-        todo!();
+        self.config.environment.origin
     }
     fn block_hash(&self, number: U256) -> H256 {
-        todo!();
+        if number >= self.config.environment.block_number
+            || self.config.environment.block_number - number - U256::one()
+                >= U256::from(self.config.environment.block_hashes.len())
+        {
+            H256::default()
+        } else {
+            let index = (self.config.environment.block_number - number - U256::one()).as_usize();
+            self.config.environment.block_hashes[index]
+        }
     }
     fn block_number(&self) -> U256 {
-        todo!();
+        self.config.environment.block_number
     }
     fn block_coinbase(&self) -> H160 {
-        todo!();
+        self.config.environment.block_coinbase
     }
     fn block_timestamp(&self) -> U256 {
-        todo!();
+        self.config.environment.block_timestamp
     }
     fn block_difficulty(&self) -> U256 {
-        todo!();
+        self.config.environment.block_difficulty
     }
     fn block_gas_limit(&self) -> U256 {
-        todo!();
+        self.config.environment.block_gas_limit
     }
     fn chain_id(&self) -> U256 {
-        todo!();
+        self.config.environment.chain_id
     }
     fn exists(&self, address: H160) -> bool {
-        todo!();
+        self.state.contains(&address)
     }
     fn basic(&self, address: H160) -> evm::backend::Basic {
-        todo!();
+        self.state
+            .get(&address)
+            .map(|a| Basic {
+                balance: a.balance,
+                nonce: a.nonce,
+            })
+            .unwrap_or_default()
     }
     fn code(&self, address: H160) -> Vec<u8> {
-        todo!();
+        self.state
+            .get(&address)
+            .map(|val| val.code.clone())
+            .unwrap_or_default()
     }
     fn storage(&self, address: H160, index: H256) -> H256 {
-        todo!();
+        self.state
+            .get(&address)
+            .map(|val| val.storage.get(&index).cloned().unwrap_or_default())
+            .unwrap_or_default()
     }
     fn original_storage(&self, address: H160, index: H256) -> Option<H256> {
-        todo!();
+        Some(self.storage(address, index))
     }
 }
 

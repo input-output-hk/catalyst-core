a07e04cf4b577942405213f4de120103a515c38c
Enzo Cioppettini <ecioppettini@atixlabs.com>
2021-08-10 11:54:11 -0300

    extract necessary setting fields

--- src/chain-wallet-libs/wallet/src/blockchain.rs
+++ src/chain-wallet-libs/wallet/src/blockchain.rs
@@ -1,23 +1,29 @@
 use chain_addr::Discrimination;
 use chain_impl_mockchain::{
     block::Block,
+    config::Block0Date,
     fee::{FeeAlgorithm as _, LinearFee},
     header::HeaderId,
     ledger::{Error, Ledger},
     transaction::Input,
 };
+use chain_time::TimeEra;
 
 #[derive(Clone)]
 pub struct Settings {
     pub fees: LinearFee,
     pub discrimination: Discrimination,
     pub block0_initial_hash: HeaderId,
+    pub block0_date: Block0Date,
+    pub slot_duration: u8,
+    pub time_era: TimeEra,
+    pub transaction_max_expiry_epochs: u8,
 }
 
 impl Settings {
     pub fn new(block: &Block) -> Result<Self, Error> {
         let header_id = block.header.id();
-        let ledger = Ledger::new(header_id, block.contents.iter())?;
+        let mut ledger = Ledger::new(header_id, block.contents.iter())?;
 
         let static_parameters = ledger.get_static_parameters().clone();
         let parameters = ledger.get_ledger_parameters();
@@ -26,6 +32,10 @@ impl Settings {
             fees: parameters.fees,
             discrimination: static_parameters.discrimination,
             block0_initial_hash: static_parameters.block0_initial_hash,
+            block0_date: static_parameters.block0_start_time,
+            slot_duration: ledger.settings().slot_duration,
+            time_era: ledger.era().clone(),
+            transaction_max_expiry_epochs: ledger.settings().transaction_max_expiry_epochs,
         })
     }
 

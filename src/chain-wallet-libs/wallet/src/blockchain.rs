a80e78693d727d02d99cc0b7868e8fe7542bc7a0
Enzo Cioppettini <ecioppettini@atixlabs.com>
2021-08-11 20:33:24 -0300

    extract slot duration directly from block0

--- src/chain-wallet-libs/wallet/src/blockchain.rs
+++ src/chain-wallet-libs/wallet/src/blockchain.rs
@@ -1,10 +1,11 @@
 use chain_addr::Discrimination;
 use chain_impl_mockchain::{
     block::Block,
-    config::Block0Date,
+    config::{Block0Date, ConfigParam},
     fee::{FeeAlgorithm as _, LinearFee},
+    fragment::Fragment,
     header::HeaderId,
-    ledger::{Error, Ledger},
+    ledger::{Block0Error, Error, Ledger},
     transaction::Input,
 };
 use chain_time::TimeEra;
@@ -28,12 +29,58 @@ impl Settings {
         let static_parameters = ledger.get_static_parameters().clone();
         let parameters = ledger.get_ledger_parameters();
 
+        // TODO: I think there is a bug in Ledger::new(), as it doesn't set the slot_duration in
+        // the Settings.
+        // This doesn't seem to matter for jormungandr anyway, because it gets the initial value
+        // directly from the block0, and then just doesn't use the field in the settings anymore.
+        // For now, just get the setting directly from the block0 here too, but should probably be
+        // fixed in Ledger::new (or at least checked).
+        let mut slot_duration = None;
+
+        for fragment in block.contents.iter() {
+            if let Fragment::Initial(initials) = fragment {
+                for initial in initials.iter() {
+                    match initial {
+                        ConfigParam::Block0Date(_) => {}
+                        ConfigParam::Discrimination(_) => {}
+                        ConfigParam::ConsensusVersion(_) => {}
+                        ConfigParam::SlotsPerEpoch(_) => {}
+                        ConfigParam::SlotDuration(sd) => {
+                            slot_duration.replace(sd);
+                        }
+                        ConfigParam::EpochStabilityDepth(_) => {}
+                        ConfigParam::ConsensusGenesisPraosActiveSlotsCoeff(_) => {}
+                        ConfigParam::BlockContentMaxSize(_) => {}
+                        ConfigParam::AddBftLeader(_) => {}
+                        ConfigParam::RemoveBftLeader(_) => {}
+                        ConfigParam::LinearFee(_) => {}
+                        ConfigParam::ProposalExpiration(_) => {}
+                        ConfigParam::KesUpdateSpeed(_) => {}
+                        ConfigParam::TreasuryAdd(_) => {}
+                        ConfigParam::TreasuryParams(_) => {}
+                        ConfigParam::RewardPot(_) => {}
+                        ConfigParam::RewardParams(_) => {}
+                        ConfigParam::PerCertificateFees(_) => {}
+                        ConfigParam::FeesInTreasury(_) => {}
+                        ConfigParam::RewardLimitNone => {}
+                        ConfigParam::RewardLimitByAbsoluteStake(_) => {}
+                        ConfigParam::PoolRewardParticipationCapping(_) => {}
+                        ConfigParam::AddCommitteeId(_) => {}
+                        ConfigParam::RemoveCommitteeId(_) => {}
+                        ConfigParam::PerVoteCertificateFees(_) => {}
+                        ConfigParam::TransactionMaxExpiryEpochs(_) => {}
+                    }
+                }
+            }
+        }
+
         Ok(Self {
             fees: parameters.fees,
             discrimination: static_parameters.discrimination,
             block0_initial_hash: static_parameters.block0_initial_hash,
             block0_date: static_parameters.block0_start_time,
-            slot_duration: ledger.settings().slot_duration,
+            slot_duration: *slot_duration
+                .ok_or(Error::Block0(Block0Error::InitialMessageNoSlotDuration))?,
             time_era: ledger.era().clone(),
             transaction_max_expiry_epochs: ledger.settings().transaction_max_expiry_epochs,
         })

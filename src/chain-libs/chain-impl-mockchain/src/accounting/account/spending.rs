ed93847fe27013793f9455ca5f262e9b08ba3c25
Vincent Hanquez <vincent.hanquez@iohk.io>
2021-09-05 15:18:19 +0800

    cleanup lanes arithmetics and mask, add some documentation

--- src/chain-libs/chain-impl-mockchain/src/accounting/account/spending.rs
+++ src/chain-libs/chain-impl-mockchain/src/accounting/account/spending.rs
@@ -1,15 +1,20 @@
 //! Spending strategies
 use super::LedgerError;
 
-/// Simple strategy to spend from an increasing counter
+/// Simple strategy to spend from multiple increasing counters
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SpendingCounterIncreasing {
     nexts: Vec<SpendingCounter>,
 }
 
+// number of bits reserved for lanes
+const LANES_BITS: usize = 3;
+// number of bits reserved for counter (unrespective of the lane)
+const UNLANES_BITS: usize = 32 - LANES_BITS;
+
 impl SpendingCounterIncreasing {
     /// number of parallel lanes of increasing counters
-    pub const LANES: usize = 8;
+    pub const LANES: usize = 1 << LANES_BITS;
 
     pub fn new_from_counter(set: SpendingCounter) -> Self {
         let mut x = Self::default();
@@ -38,6 +43,10 @@ impl SpendingCounterIncreasing {
         self.nexts.clone()
     }
 
+    /// try to match the lane of the counter in argument, if it doesn't match
+    /// a ledger error reported.
+    ///
+    /// If the counter match succesfully, then the counter at this lane is incremented by one.
     pub fn next_verify(&mut self, counter: SpendingCounter) -> Result<(), LedgerError> {
         let actual_counter = self.nexts[counter.lane()];
 
@@ -76,15 +85,21 @@ impl Default for SpendingCounterIncreasing {
 /// the counter is incremented. A matching counter
 /// needs to be used in the spending phase to make
 /// sure we have non-replayability of a transaction.
+///
+/// Note that the leading LANES_BITS bits are used to codify the
+/// implicit lane.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct SpendingCounter(pub(crate) u32);
 
 impl SpendingCounter {
-    const UNLANED_MASK: u32 = 0x1fff_ffff;
-    const LANED_MASK: u32 = 0xe000_0000;
+    // on 32 bits: 0x1fff_ffff;
+    const UNLANED_MASK: u32 = (1 << UNLANES_BITS) - 1;
+
+    // LANES_BITS on the MSB, on 32 bits: 0xe000_0000
+    const LANED_MASK: u32 = !Self::UNLANED_MASK;
 
     fn lane(self) -> usize {
-        (self.0 >> 29) as usize
+        (self.0 >> UNLANES_BITS) as usize
     }
 
     fn unlaned_counter(self) -> u32 {
@@ -92,15 +107,20 @@ impl SpendingCounter {
     }
 
     pub(crate) fn new(lane: usize, counter: u32) -> Self {
-        assert!(lane < 0b1000);
-        assert!(counter < 0x2000_0000);
-        SpendingCounter((lane << 29) as u32 | (counter & Self::UNLANED_MASK))
+        assert!(lane < (1 << LANES_BITS));
+        assert!(counter < (1 << UNLANES_BITS));
+        SpendingCounter((lane << UNLANES_BITS) as u32 | (counter & Self::UNLANED_MASK))
     }
 
     pub fn zero() -> Self {
         SpendingCounter(0)
     }
 
+    /// Increment the counter within it own lane. the lane of where this counter apply, cannot change
+    /// through the incrementation procedure
+    ///
+    /// if the counter bits overflow, it will automatically be wrapped, so
+    /// that the lane remains identical
     #[must_use = "this function does not modify the state"]
     pub fn increment(self) -> Self {
         let inc = (self.unlaned_counter() + 1) & Self::UNLANED_MASK;

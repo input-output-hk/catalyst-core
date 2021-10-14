b1abd60c9fdd20b2d308fba7d4730c505d62c65d
Daniel Sanchez Quiros <daniel.sanchez@iohk.io>
2021-10-14 12:39:25 +0200

    Base cli command structure

---
+++ src/catalyst-toolbox/src/bin/cli/rewards/veterans.rs
@@ -0,0 +1,23 @@
+use super::Error;
+use catalyst_toolbox::rewards::veterans;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(rename_all = "kebab-case")]
+pub struct VotersRewards {
+    #[structopt(flatten)]
+    reviews: PathBuf,
+    /// Reward (in LOVELACE) to be distributed
+    #[structopt(long = "total-rewards")]
+    total_rewards: u64,
+}
+
+impl VotersRewards {
+    pub fn exec(self) -> Result<(), Error> {
+        let Self {
+            reviews,
+            total_rewards,
+        };
+    }
+}

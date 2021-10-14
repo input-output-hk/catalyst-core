acfbef8ab984114c142778e59a32e4c6072961ae
Daniel Sanchez Quiros <daniel.sanchez@iohk.io>
2021-10-14 12:43:33 +0200

    Fix pathbuf in command input

--- src/catalyst-toolbox/src/bin/cli/rewards/veterans.rs
+++ src/catalyst-toolbox/src/bin/cli/rewards/veterans.rs
@@ -5,19 +5,20 @@ use structopt::StructOpt;
 
 #[derive(StructOpt)]
 #[structopt(rename_all = "kebab-case")]
-pub struct VotersRewards {
-    #[structopt(flatten)]
+pub struct VeteransRewards {
     reviews: PathBuf,
     /// Reward (in LOVELACE) to be distributed
     #[structopt(long = "total-rewards")]
     total_rewards: u64,
 }
 
-impl VotersRewards {
+impl VeteransRewards {
     pub fn exec(self) -> Result<(), Error> {
         let Self {
             reviews,
             total_rewards,
         };
+
+        Ok(())
     }
 }

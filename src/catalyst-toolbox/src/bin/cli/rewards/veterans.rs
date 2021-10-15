6092577842acdc0ad430790d22e1c5dba69127e8
Daniel Sanchez Quiros <daniel.sanchez@iohk.io>
2021-10-15 12:24:54 +0200

    Added simple test

--- src/catalyst-toolbox/src/bin/cli/rewards/veterans.rs
+++ src/catalyst-toolbox/src/bin/cli/rewards/veterans.rs
@@ -9,10 +9,10 @@ use structopt::StructOpt;
 #[structopt(rename_all = "kebab-case")]
 pub struct VeteransRewards {
     /// Reviews csv file path
-    reviews: PathBuf,
+    from: PathBuf,
 
     /// Results file output path
-    output: PathBuf,
+    to: PathBuf,
 
     /// Reward to be distributed
     #[structopt(long = "total-rewards")]
@@ -22,14 +22,45 @@ pub struct VeteransRewards {
 impl VeteransRewards {
     pub fn exec(self) -> Result<(), Error> {
         let Self {
-            reviews,
-            output,
+            from,
+            to,
             total_rewards,
         } = self;
-        let reviews: Vec<veterans::VeteranReviewsCount> = csv::load_data_from_csv(&reviews)?;
+        let reviews: Vec<veterans::VeteranReviewsCount> = csv::load_data_from_csv(&from)?;
         let results = veterans::calculate_veteran_advisors_rewards(&reviews, total_rewards);
-        csv::dump_data_to_csv(&results, &output)?;
+        csv::dump_data_to_csv(&results, &to)?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::cli::rewards::veterans::VeteransRewards;
+    use catalyst_toolbox::rewards::veterans::VeteranAdvisorReward;
+    use jcli_lib::utils::io;
+    use rust_decimal::prelude::FromStr;
+    use std::io::BufRead;
+
+    #[test]
+    fn test_output_csv() {
+        let resource_input = "./resources/testing/veteran_reviews_count.csv";
+        let tmp_file = assert_fs::NamedTempFile::new("outfile.csv").unwrap();
+
+        let export = VeteransRewards {
+            from: resource_input.into(),
+            to: tmp_file.path().into(),
+            total_rewards: 1000.into(),
+        };
+
+        export.exec().unwrap();
+        let reader = io::open_file_read(&Some(tmp_file.path())).unwrap();
+        let expected_reward = VeteranAdvisorReward::from(200);
+        for line in reader.lines() {
+            let line = line.unwrap();
+            let res: Vec<&str> = line.split(',').collect();
+            let reward = VeteranAdvisorReward::from_str(res[1]).unwrap();
+            assert_eq!(reward, expected_reward);
+        }
     }
 }

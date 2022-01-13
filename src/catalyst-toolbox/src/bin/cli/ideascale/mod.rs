90e92211286beb2dde1c7b6b755dc2d859c375ec
dkijania <dariusz.kijania@iohk.io>
2022-01-13 08:20:37 +0100

    adjust scores.csv file

--- src/catalyst-toolbox/src/bin/cli/ideascale/mod.rs
+++ src/catalyst-toolbox/src/bin/cli/ideascale/mod.rs
@@ -197,23 +197,19 @@ fn parse_from_csv(s: &str) -> Filters {
 
 fn read_scores_file(path: &Option<PathBuf>) -> Result<Scores, Error> {
     let mut scores = Scores::new();
-
-    if let Some(path) = path {
-        let mut reader = csv::Reader::from_path(path)?;
-        for record in reader.records() {
-            let record = record?;
-            let proposal_id: u32 = record
-                .get(1)
-                .expect("Proposal ids should be present in scores file second column")
-                .parse()
-                .expect("Proposal ids should be integers");
-            let rating_given: f32 = record
-                .get(2)
-                .expect("Ratings should be present in scores file third column")
-                .parse()
-                .expect("Ratings should be floats [0, 5]");
-            scores.insert(proposal_id, rating_given);
-        }
+    for record in reader.records() {
+        let record = record?;
+        let proposal_id: u32 = record
+            .get(0)
+            .expect("Proposal ids should be present in scores file second column")
+            .parse()
+            .expect("Proposal ids should be integers");
+        let rating_given: f32 = record
+            .get(1)
+            .expect("Ratings should be present in scores file third column")
+            .parse()
+            .expect("Ratings should be floats [0, 5]");
+        scores.insert(proposal_id, rating_given);
     }
     Ok(scores)
 }

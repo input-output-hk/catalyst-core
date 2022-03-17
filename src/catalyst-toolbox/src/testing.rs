f6da7fd62d2db49a6333b6ae1e760f8c3f75e1ac
dkijania <dariusz.kijania@iohk.io>
2022-03-17 13:10:39 +0100

    migrate from py03 to standard cmd for proposers_rewards

--- src/catalyst-toolbox/src/testing.rs
+++ src/catalyst-toolbox/src/testing.rs
@@ -1,8 +1,12 @@
-use pyo3::prelude::*;
+use assert_fs::fixture::PathChild;
+use assert_fs::TempDir;
+use std::process::Command;
 use std::{path::PathBuf, str::FromStr};
+use thiserror::Error;
 
 #[derive(Debug)]
-pub struct ProposerRewardsExecutor {
+pub struct ProposerRewardsCommand {
+    python_exec: String,
     output_file: PathBuf,
     block0_path: PathBuf,
     total_stake_threshold: f64,
@@ -13,12 +17,19 @@ pub struct ProposerRewardsExecutor {
     excluded_proposals_path: Option<String>,
     active_voteplan_path: Option<String>,
     challenges_path: Option<String>,
-    vit_station_url: String,
+    vit_station_url: Option<String>,
 }
 
-impl Default for ProposerRewardsExecutor {
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
+
+impl Default for ProposerRewardsCommand {
     fn default() -> Self {
         Self {
+            python_exec: "python3".to_string(),
             output_file: PathBuf::from_str("./output").unwrap(),
             block0_path: PathBuf::from_str("./block0.bin").unwrap(),
             total_stake_threshold: 0.01,
@@ -29,12 +40,12 @@ impl Default for ProposerRewardsExecutor {
             excluded_proposals_path: None,
             active_voteplan_path: None,
             challenges_path: None,
-            vit_station_url: "https://servicing-station.vit.iohk.io".to_string(),
+            vit_station_url: None,
         }
     }
 }
 
-impl ProposerRewardsExecutor {
+impl ProposerRewardsCommand {
     pub fn output_file(mut self, output_file: PathBuf) -> Self {
         self.output_file = output_file;
         self
@@ -78,36 +89,59 @@ impl ProposerRewardsExecutor {
         self
     }
     pub fn vit_station_url(mut self, vit_station_url: String) -> Self {
-        self.vit_station_url = vit_station_url;
+        self.vit_station_url = Some(vit_station_url);
         self
     }
 
-    pub fn proposers_rewards(self) -> PyResult<()> {
-        Python::with_gil(|py| {
-            let fun: Py<PyAny> = PyModule::from_code(
-                py,
-                include_str!("../scripts/python/proposers_rewards.py"),
-                "",
-                "",
-            )?
-            .getattr("calculate_rewards")?
-            .into();
+    pub fn cmd(self, temp_dir: &TempDir) -> Result<Command, Error> {
+        let script_content = include_str!("../scripts/python/proposers_rewards.py");
+        let script_file = temp_dir.child("proposers_rewards.py");
 
-            let args = (
-                self.output_file.to_str(),
-                self.block0_path.to_str(),
-                self.total_stake_threshold.to_string(),
-                self.approval_threshold.to_string(),
-                self.output_format.to_string(),
-                self.proposals_path,
-                self.excluded_proposals_path,
-                self.active_voteplan_path,
-                self.challenges_path,
-                self.vit_station_url,
-                self.committee_keys_path,
-            );
-            fun.call1(py, args)
-        })?;
-        Ok(())
+        std::fs::write(script_file.path(), script_content)?;
+
+        let mut command = Command::new(self.python_exec);
+
+        command
+            .arg(script_file.path())
+            .arg("--output-file")
+            .arg(self.output_file)
+            .arg("--block0-path")
+            .arg(self.block0_path)
+            .arg("--output-format")
+            .arg(self.output_format)
+            .arg("--total-stake-threshold")
+            .arg(self.total_stake_threshold.to_string())
+            .arg("--approval-threshold")
+            .arg(self.approval_threshold.to_string());
+
+        if let Some(proposals_path) = self.proposals_path {
+            command.arg("--proposals-path").arg(proposals_path);
+        }
+
+        if let Some(active_voteplan_path) = self.active_voteplan_path {
+            command
+                .arg("--active-voteplan-path")
+                .arg(active_voteplan_path);
+        }
+
+        if let Some(challenges_path) = self.challenges_path {
+            command.arg("--challenges-path").arg(challenges_path);
+        }
+
+        if let Some(excluded_proposals_path) = self.excluded_proposals_path {
+            command
+                .arg("--excluded-proposals-path")
+                .arg(excluded_proposals_path);
+        }
+        if let Some(committee_keys_path) = self.committee_keys_path {
+            command
+                .arg("--committee-keys-path")
+                .arg(committee_keys_path);
+        }
+        if let Some(vit_station_url) = self.vit_station_url {
+            command.arg("--vit-station-url").arg(vit_station_url);
+        }
+
+        Ok(command)
     }
 }

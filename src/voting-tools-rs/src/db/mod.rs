ee1a10167c8e3e4077739ef923a9c895d497b0ef
Cameron <cameron.studdstreet@gmail.com>
2022-08-08 12:30:36 +0100

    nix working

--- src/voting-tools-rs/src/db/mod.rs
+++ src/voting-tools-rs/src/db/mod.rs
@@ -1,20 +1,45 @@
 use color_eyre::Result;
+use diesel::{
+    r2d2::{ConnectionManager, Pool, PooledConnection},
+    PgConnection,
+};
 use microtype::secrecy::ExposeSecret;
 
 use crate::config::DbConfig;
 
-pub async fn connect(
-    DbConfig {
-        name,
-        user,
-        host,
-        password,
-    }: &DbConfig,
-) -> Result<()> {
-    let connection_str = format!(
-        "postgres://{user}:{password}@{host}/{name}",
-        password = password.expose_secret()
-    );
+pub struct Db(Pool<ConnectionManager<PgConnection>>);
 
-    Ok(())
+type PoolConn = PooledConnection<ConnectionManager<PgConnection>>;
+
+impl Db {
+    pub fn connect(
+        DbConfig {
+            name,
+            user,
+            host,
+            password,
+        }: &DbConfig,
+    ) -> Result<Self> {
+        let url = format!(
+            "postgres://{user}:{password}@{host}/{name}",
+            password = password.expose_secret()
+        );
+
+        let manager = ConnectionManager::new(&url);
+        let pool = Pool::builder().build(manager)?;
+
+        Ok(Db(pool))
+    }
+
+    async fn exec<F, T>(&self, f: F) -> Result<T>
+    where
+        F: FnOnce(&PoolConn) -> diesel::result::QueryResult<T> + Send + 'static,
+        T: Send + 'static,
+    {
+        let conn = self.0.get()?;
+        let result = tokio::task::spawn_blocking(move || f(&conn)).await?;
+        let value = result?;
+
+        Ok(value)
+    }
 }

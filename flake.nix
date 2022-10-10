1a421f75e47821b5f4999216586b8a4ee73ebac7
Joaquín Rosales <joaquin.rosales@iohk.io>
2022-10-10 07:53:59 -0500

    add top-level nix flake for monorepo

--- flake.nix
+++ flake.nix
@@ -1,35 +1,63 @@
 {
-  description = "Catalyst Core";
-  inputs =
-    {
-      nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-      flake-utils.url = "github:numtide/flake-utils";
-      rust-overlay.url = "github:oxalica/rust-overlay";
-      rust-overlay.inputs.flake-utils.follows = "flake-utils";
-      rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    };
+  nixConfig.extra-substituters = [
+    "https://iog.cachix.org"
+    "https://hydra.iohk.io"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "iog.cachix.org-1:nYO0M9xTk/s5t1Bs9asZ/Sww/1Kt/hRhkLP0Hhv/ctY="
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+  ];
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }: flake-utils.lib.eachDefaultSystem (system:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-    let pkgs = import nixpkgs {
-      inherit system;
-      overlays = [ (import rust-overlay) ];
-    };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-compat,
+    flake-utils,
+    pre-commit-hooks,
+    rust-overlay,
+    naersk,
+  }:
+    flake-utils.lib.eachSystem
+    [
+      flake-utils.lib.system.x86_64-linux
+      flake-utils.lib.system.aarch64-linux
+    ]
+    (
+      system: let
+        readTOML = file: builtins.fromTOML (builtins.readFile file);
+        workspaceCargo = readTOML ./Cargo.toml;
 
-    in
-
-    {
-
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          rust-bin.stable.latest.default
-
-          pkg-config
-          openssl
-          protobuf
-          uniffi-bindgen
-        ];
-      };
-
-    });
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
+      in {
+        devShells.default =
+          pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              rust-bin.stable.latest.default
+              pkg-config
+              openssl
+              protobuf
+              uniffi-bindgen
+            ];
+          };
+      }
+    );
 }

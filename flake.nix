31b78777b7d317402d1fd99e88c2a3faaf7c4df7
Joaquín Rosales <joaquin.rosales@iohk.io>
2022-10-17 09:46:41 -0500

    Update nix files for monorepo (#22)
    * add top-level nix flake for monorepo
    
    * refactor jormungandr flake into top-level
    
    * transfers the entire flake from  into root directory
    * removes
    * adds
    * refactors  function to handle  and  variables
    
    * add jortestkit and wallet-core crates to the workspace
    
    * move proptest to workspace.dependencies
    
    make all crates use the workspace deps, removes version discrepancy from chain-evm
    
    * remove flake files from src/chain-libs
    
    * add .envrc file
    
    * move default.nix and shell.nix to top-level, remove duplicates
    
    * update .gitignore to include files created by nix
    
    * update name for util-linux dep in nixpkgs
    
    * actually remove flake.lock from src/
    
    * cleaner way to get nix package name; fix extra nightly- prefix
    
    * set rust version to 1.64.0 in flake.nix
    
    * add voting-tools-rs to workspace.members
    
    * use package name instead of path to build with cargo in flake
    
    * move profile settings into voting-tools-rs/config.toml
    
    * update flake.lock
    
    also adds version variable to mkRust in flake.nix
    
    * move profile.bench settings into config.toml for chain-libs crates
    
    * move profile.release settings into config.toml for chain-wallet-libs crates
    
    * add shell packages from voting-tools-rs, remove flake in crate
    
    * run cargo fmt in voting-tools-rs to pass flake check
    
    * specify branch for evm-test-suite, update lock files
    
    fixes issue with git not finding the main branch as defined in Cargo
    
    * copy .gitignore from jormungandr
    
    * remove inner .envrc files
    
    * migrate missing parts from catalyst-toolbox/flake
    
    * finish migrating flakes

--- flake.nix
+++ flake.nix
@@ -1,35 +1,337 @@
 {
-  description = "Catalyst Core";
-  inputs =
-    {
-      nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-      flake-utils.url = "github:numtide/flake-utils";
-      rust-overlay.url = "github:oxalica/rust-overlay";
-      rust-overlay.inputs.flake-utils.follows = "flake-utils";
-      rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    };
+  nixConfig.extra-substituters = [
+    "https://iog.cachix.org"
+    "https://hydra.iohk.io"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "iog.cachix.org-1:nYO0M9xTk/s5t1Bs9asZ/Sww/1Kt/hRhkLP0Hhv/ctY="
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+  ];
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }: flake-utils.lib.eachDefaultSystem (system:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    cardano-node.url = "github:input-output-hk/cardano-node/1.33.0";
+  };
 
-    let pkgs = import nixpkgs {
-      inherit system;
-      overlays = [ (import rust-overlay) ];
-    };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-compat,
+    flake-utils,
+    pre-commit-hooks,
+    rust-overlay,
+    naersk,
+    cardano-node,
+  }:
+    flake-utils.lib.eachSystem
+    [
+      flake-utils.lib.system.x86_64-linux
+      flake-utils.lib.system.aarch64-linux
+    ]
+    (
+      system: let
+        readTOML = file: builtins.fromTOML (builtins.readFile file);
+        workspaceCargo = readTOML ./Cargo.toml;
 
-    in
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
 
-    {
+        inherit (cardano-node.packages.${system}) cardano-cli;
 
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          rust-bin.stable.latest.default
+        mkRust = {
+          channel ? "stable",
+          version ? "latest",
+        }: let
+          _rust = pkgs.rust-bin.${channel}.${version}.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analysis"
+              "rls-preview"
+              "rustfmt-preview"
+              "clippy-preview"
+            ];
+          };
+        in
+          pkgs.buildEnv {
+            name = _rust.name;
+            inherit (_rust) meta;
+            buildInputs = [pkgs.makeWrapper pkgs.openssl];
+            paths = [_rust];
+            pathsToLink = ["/" "/bin"];
+            # XXX: This is needed because cargo and clippy commands need to
+            # also be aware of other binaries in order to work properly.
+            # https://github.com/cachix/pre-commit-hooks.nix/issues/126
+            postBuild = ''
+              for i in $out/bin/*; do
+                wrapProgram "$i" --prefix PATH : "$out/bin"
+              done
+            '';
+          };
 
-          pkg-config
-          openssl
-          protobuf
-          uniffi-bindgen
-        ];
-      };
+        rust-stable = mkRust {
+          channel = "stable";
+          version = "1.64.0";
+        };
+        rust-nightly = mkRust {channel = "nightly";};
 
-    });
+        naersk-lib-stable = naersk.lib."${system}".override {
+          cargo = rust-stable;
+          rustc = rust-stable;
+        };
+
+        naersk-lib-nighlty = naersk.lib."${system}".override {
+          cargo = rust-nightly;
+          rustc = rust-nightly;
+        };
+
+        mkPackage = {
+          naersk-lib ? naersk-lib-stable,
+          pkgPath,
+          pkgCargo,
+        }: let
+          name = pkgCargo.package.name;
+          cargoOptions =
+            [
+              "--package"
+              "${name}"
+            ]
+            ++ (pkgs.lib.optionals (name == "jormungandr") [
+              "--features"
+              "prometheus-metrics"
+            ]);
+          nativeBuildInputs = with pkgs;
+            [
+              pkg-config
+              protobuf
+              rustfmt
+            ]
+            ++ (pkgs.lib.optionals (name == "voting_tools_rs") [
+              postgresql
+            ]);
+
+          unwrapped = naersk-lib.buildPackage {
+            inherit (pkgCargo.package) name version;
+            inherit nativeBuildInputs;
+
+            root = self;
+
+            cargoBuildOptions = x: x ++ cargoOptions;
+            cargoTestOptions = x: x ++ cargoOptions;
+
+            PROTOC = "${pkgs.protobuf}/bin/protoc";
+            PROTOC_INCLUDE = "${pkgs.protobuf}/include";
+
+            buildInputs = with pkgs; [
+              openssl
+            ];
+          };
+
+          extraBinPath = {
+            snapshot-trigger-service = with workspace; [voting-tools];
+            registration-service = with workspace; [catalyst-toolbox jcli cardano-cli voting-tools];
+            registration-verify-service = with workspace; [jcli];
+          };
+        in
+          if builtins.elem name (builtins.attrNames extraBinPath)
+          then
+            pkgs.runCommand "wrapped-${unwrapped.name}" {nativeBuildInputs = [pkgs.makeWrapper];} ''
+              mkdir -p $out/bin
+              ln -s ${unwrapped}/bin/${name} $out/bin/${name}
+              wrapProgram $out/bin/${name} --prefix PATH : ${pkgs.lib.makeBinPath extraBinPath.${name}}
+            ''
+          else unwrapped;
+
+        workspace =
+          builtins.listToAttrs
+          (
+            builtins.map
+            (pkgPath: let
+              inherit pkgPath;
+              pkgCargo = readTOML ./${pkgPath}/Cargo.toml;
+            in {
+              name = pkgCargo.package.name;
+              value = mkPackage {inherit pkgPath pkgCargo;};
+            })
+            workspaceCargo.workspace.members
+          );
+
+        workspace-nightly =
+          builtins.listToAttrs
+          (
+            builtins.map
+            (pkgPath: let
+              inherit pkgPath;
+              pkgCargo = readTOML ./${pkgPath}/Cargo.toml;
+            in {
+              name = "nightly-${pkgCargo.package.name}";
+              value = mkPackage {
+                inherit pkgPath pkgCargo;
+                naersk-lib = naersk-lib-nighlty;
+              };
+            })
+            workspaceCargo.workspace.members
+          );
+
+        jormungandr-entrypoint = let
+          script =
+            pkgs.writeShellScriptBin "entrypoint"
+            ''
+              set -exuo pipefail
+
+              ulimit -n 1024
+
+              nodeConfig="$NOMAD_TASK_DIR/node-config.json"
+              runConfig="$NOMAD_TASK_DIR/running.json"
+              runYaml="$NOMAD_TASK_DIR/running.yaml"
+              name="jormungandr"
+
+              chmod u+rwx -R "$NOMAD_TASK_DIR" || true
+
+              function convert () {
+                chmod u+rwx -R "$NOMAD_TASK_DIR" || true
+                cp "$nodeConfig" "$runConfig"
+                remarshal --if json --of yaml "$runConfig" > "$runYaml"
+              }
+
+              if [ "$RESET" = "true" ]; then
+                echo "RESET is given, will start from scratch..."
+                rm -rf "$STORAGE_DIR"
+              elif [ -d "$STORAGE_DIR" ]; then
+                echo "$STORAGE_DIR found, not restoring from backup..."
+              else
+                echo "$STORAGE_DIR not found, restoring backup..."
+
+                restic restore latest \
+                  --verbose=5 \
+                  --no-lock \
+                  --tag "$NAMESPACE" \
+                  --target / \
+                || echo "couldn't restore backup, continue startup procedure..."
+              fi
+
+              set +x
+              echo "waiting for $REQUIRED_PEER_COUNT peers"
+              until [ "$(jq -e -r '.p2p.trusted_peers | length' < "$nodeConfig" || echo 0)" -ge $REQUIRED_PEER_COUNT ]; do
+                sleep 1
+              done
+              set -x
+
+              convert
+
+              if [ -n "$PRIVATE" ]; then
+                echo "Running with node with secrets..."
+                exec jormungandr \
+                  --storage "$STORAGE_DIR" \
+                  --config "$NOMAD_TASK_DIR/running.yaml" \
+                  --genesis-block $NOMAD_TASK_DIR/block0.bin/block0.bin \
+                  --secret $NOMAD_SECRETS_DIR/bft-secret.yaml \
+                  "$@" || true
+              else
+                echo "Running with follower node..."
+                exec jormungandr \
+                  --storage "$STORAGE_DIR" \
+                  --config "$NOMAD_TASK_DIR/running.yaml" \
+                  --genesis-block $NOMAD_TASK_DIR/block0.bin/block0.bin \
+                  "$@" || true
+              fi
+            '';
+        in
+          pkgs.symlinkJoin {
+            name = "entrypoint";
+            paths =
+              [script workspace.jormungandr]
+              ++ (with pkgs; [
+                bashInteractive
+                coreutils
+                curl
+                diffutils
+                fd
+                findutils
+                gnugrep
+                gnused
+                htop
+                jq
+                lsof
+                netcat
+                procps
+                remarshal
+                restic
+                ripgrep
+                strace
+                tcpdump
+                tmux
+                tree
+                util-linux
+                vim
+                yq
+              ]);
+          };
+
+        pre-commit = pre-commit-hooks.lib.${system}.run {
+          src = self;
+          hooks = {
+            alejandra = {
+              enable = true;
+            };
+            rustfmt = {
+              enable = true;
+              entry = pkgs.lib.mkForce "${rust-nightly}/bin/cargo-fmt fmt -- --check --color always";
+            };
+          };
+        };
+
+        warnToUpdateNix = pkgs.lib.warn "Consider updating to Nix > 2.7 to remove this warning!";
+      in rec {
+        packages =
+          workspace
+          // workspace-nightly
+          // {
+            inherit jormungandr-entrypoint pre-commit;
+            default = pre-commit;
+          };
+
+        devShells.default = pkgs.mkShell {
+          PROTOC = "${pkgs.protobuf}/bin/protoc";
+          PROTOC_INCLUDE = "${pkgs.protobuf}/include";
+          buildInputs =
+            [rust-stable]
+            ++ (with pkgs; [
+              pkg-config
+              openssl
+              protobuf
+              uniffi-bindgen
+              postgresql
+              diesel-cli
+              cargo-insta # snapshot testing lib
+            ]);
+          shellHook =
+            pre-commit.shellHook
+            + ''
+              echo "=== Catalyst Core development shell ==="
+              echo "Info: Git hooks can be installed using \`pre-commit install\`"
+            '';
+          # TODO: is this needed for vit-testing development
+          # export PATH="${jormungandr}/bin:$PATH"
+          # export PATH="${vit-servicing-station-server}/bin:$PATH"
+        };
+
+        checks.pre-commit = pre-commit;
+
+        defaultPackage = warnToUpdateNix packages.default;
+        devShell = warnToUpdateNix devShells.default;
+      }
+    );
 }

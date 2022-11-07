13129f05daa1e33efa2d09d995375fd46f668829
Cameron <cameron.studdstreet@gmail.com>
2022-11-07 11:40:20 +0000

    du

--- .github/jormungandr_tests.sh
+++ .github/jormungandr_tests.sh
@@ -5,8 +5,10 @@ RUST_BACKTRACE=1
 CARGO_FLAGS="--verbose --locked"
 CARGO_INCREMENTAL=0
 
+du
 cat Cargo.lock
 uniffi-bindgen --version
 cargo build --all-targets --locked
+du
 cargo nextest run --no-fail-fast --partition hash:$1/$2 
 

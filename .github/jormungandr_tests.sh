9e8f49d9d221f2707ececab8fba6de765764e37f
Cameron <cameron.studdstreet@gmail.com>
2022-11-07 10:52:43 +0000

    remove debugging stuff

--- .github/jormungandr_tests.sh
+++ .github/jormungandr_tests.sh
@@ -7,9 +7,6 @@ CARGO_INCREMENTAL=0
 
 cat Cargo.lock
 uniffi-bindgen --version
-which uniffi-bindgen
-cargo tree -i uniffi_build
-cargo tree -i uniffi
 cargo build --all-targets --locked
 cargo nextest run --no-fail-fast --partition hash:$1/$2 
 

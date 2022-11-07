cbda6680f1f6f3b0dfd1a99e0a3ec28061a7bf57
Cameron <cameron.studdstreet@gmail.com>
2022-11-07 12:03:39 +0000

    remove everything

--- .github/jormungandr_tests.sh
+++ .github/jormungandr_tests.sh
@@ -5,9 +5,5 @@ RUST_BACKTRACE=1
 CARGO_FLAGS="--verbose --locked"
 CARGO_INCREMENTAL=0
 
-du
-cat Cargo.lock
-uniffi-bindgen --version
-du
 cargo nextest run --no-fail-fast --partition hash:$1/$2 
 

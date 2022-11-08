dde30e3b47a3f113bbe93c1a3305e3fd9d00bad2
Cameron <cameron.studdstreet@gmail.com>
2022-11-08 10:38:49 +0000

    copy paste commit

---
+++ .github/jormungandr_tests.sh
@@ -0,0 +1,10 @@
+set -ex
+
+RUSTFLAGS="-D warnings" 
+RUST_BACKTRACE=1
+CARGO_FLAGS="--verbose --locked"
+CARGO_INCREMENTAL=0
+
+curl -LsSf https://get.nexte.st/latest/linux | tar zxf - -C ${CARGO_HOME:-~/.cargo}/bin
+cargo nextest run --no-fail-fast --partition hash:$1/$2 
+

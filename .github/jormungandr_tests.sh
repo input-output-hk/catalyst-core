bf1bee027535946c3669c88b3e004325704c5cb2
Cameron <cameron.studdstreet@gmail.com>
2022-11-08 19:37:54 +0000

    build before

--- .github/jormungandr_tests.sh
+++ .github/jormungandr_tests.sh
@@ -6,5 +6,6 @@ CARGO_FLAGS="--verbose --locked"
 CARGO_INCREMENTAL=0
 
 curl -LsSf https://get.nexte.st/latest/linux | tar zxf - -C ${CARGO_HOME:-~/.cargo}/bin
+cargo build --all-targets
 cargo nextest run --no-fail-fast --partition hash:$1/$2 
 

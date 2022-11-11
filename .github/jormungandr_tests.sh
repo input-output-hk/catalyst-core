aeba92a8ceeb16e94a2a5cbdb1ddd54776b833ca
Cameron <cameron.studdstreet@gmail.com>
2022-11-11 00:04:13 +0000

    test more

--- .github/jormungandr_tests.sh
+++
@@ -1,7 +0,0 @@
-set -ex
-
-curl -LsSf https://get.nexte.st/latest/linux | tar zxf - -C ${CARGO_HOME:-~/.cargo}/bin
-cargo build -p jormungandr
-cargo build -p jcli
-cargo nextest run --no-fail-fast --partition hash:$1/$2 
-

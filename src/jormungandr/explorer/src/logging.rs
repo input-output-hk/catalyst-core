9bb212a78a244abc51d3d72996430f1f37a50689
Cameron <cameron.studdstreet@gmail.com>
2022-11-11 13:53:25 +0000

    fix clippy

--- src/jormungandr/explorer/src/logging.rs
+++ src/jormungandr/explorer/src/logging.rs
@@ -59,8 +59,8 @@ impl FromStr for LogOutput {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.starts_with('@') {
-            return Ok(Self::File(PathBuf::from(&s[1..])));
+        if let Some(stripped) = s.strip_prefix('@') {
+            return Ok(Self::File(PathBuf::from(stripped)));
         }
 
         match s.trim().to_lowercase().as_str() {

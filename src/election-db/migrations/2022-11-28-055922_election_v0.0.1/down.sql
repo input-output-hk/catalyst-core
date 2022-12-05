587f4eabd10878517af9e2d8cc1480724d744626
Steven Johnson <sakurainds@gmail.com>
2022-12-05 19:56:50 +0700

    drop everything in a single drop so that inter-related tables cleanly drop.

--- src/election-db/migrations/2022-11-28-055922_election_v0.0.1/down.sql
+++ src/election-db/migrations/2022-11-28-055922_election_v0.0.1/down.sql
@@ -1,20 +1,23 @@
 -- This file should undo anything in `up.sql`
+DROP VIEW IF EXISTS
+    schema_version;
+    
+DROP TABLE IF EXISTS 
+    config,
+    voteplan_types,
+    voting_group,
+    voteplan,
+    proposal_voteplan,
+    community_advisors_review,
+    goal,
+    voter_registration_type,
+    voter_registration,
+    stake_address_balance,
+    voting_power,
+    election,
+    challenge_category,
+    currency,
+    vote_options,
+    challenge,
+    proposal;
 
-DROP TABLE IF EXISTS config;
-DROP TABLE IF EXISTS proposals_voteplans;
-DROP TABLE IF EXISTS proposal_simple_challenge;
-DROP TABLE IF EXISTS proposal_community_choice_challenge;
-DROP TABLE IF EXISTS community_advisors_reviews;
-DROP TABLE IF EXISTS goals;
-DROP TABLE IF EXISTS groups;
-DROP TABLE IF EXISTS votes;
-DROP TABLE IF EXISTS voters;
-DROP TABLE IF EXISTS contributions;
-DROP TABLE IF EXISTS snapshots;
-DROP TABLE IF EXISTS voteplans;
-
-DROP TABLE IF EXISTS proposal;
-DROP TABLE IF EXISTS challenge;
-DROP TABLE IF EXISTS election;
-DROP TABLE IF EXISTS challenge_type;
-DROP TABLE IF EXISTS currency;

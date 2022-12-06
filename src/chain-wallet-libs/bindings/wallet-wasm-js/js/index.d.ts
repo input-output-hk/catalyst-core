098ba9c0d7a268dbcef5fd283a499a0b2d4e4df5
Mr-Leshiy <leshiy12345678@gmail.com>
2022-12-06 19:39:16 +0200

    update typings

--- src/chain-wallet-libs/bindings/wallet-wasm-js/js/index.d.ts
+++ src/chain-wallet-libs/bindings/wallet-wasm-js/js/index.d.ts
@@ -1,22 +1,42 @@
-import { BlockDate, Fragment } from "wallet_js";
+import * as wallet_wasm from "wallet-wasm-js";
 
 export class Settings {
-    constructor(json: string): Settings;
+  settings: wallet_wasm.Settings;
 
-    to_json(): string;
+  constructor(json: string): Settings;
+
+  to_json(): string;
 }
 
 export class Vote {
+  vote_cast: wallet_wasm.VoteCast;
 
-    static public(vote_plan_bytes: Uint8Array, proposal_index: number, choice: number): Vote;
+  static public(
+    vote_plan_bytes: Uint8Array,
+    proposal_index: number,
+    choice: number
+  ): Vote;
 
-    static private(vote_plan_bytes: Uint8Array, proposal_index: number, options: number, choice: number, public_key: Uint8Array): Vote;
+  static private(
+    vote_plan_bytes: Uint8Array,
+    proposal_index: number,
+    options: number,
+    choice: number,
+    public_key: Uint8Array
+  ): Vote;
 }
 
 export class Wallet {
-    constructor(private_key: Uint8Array, init_value: bigint): Wallet;
+  wallet: wallet_wasm.Wallet;
 
-    signVotes(votes: [Vote], settings: Settings, valid_until: BlockDate, lane: number): Fragment;
+  constructor(private_key: Uint8Array, init_value: bigint): Wallet;
 
-    total_value(): bigint;
+  signVotes(
+    votes: [Vote],
+    settings: Settings,
+    valid_until: wallet_wasm.BlockDate,
+    lane: number
+  ): wallet_wasm.Fragment;
+
+  total_value(): bigint;
 }

0e592d7465ae3ced53b73b3ed9f4645b6c8b95c5
nicopado <nico.pado@hotmail.it>
2022-12-16 16:56:23 +0100

    cleaned up and corrected key saturation calculation formula

--- src/vit-servicing-station/vit-servicing-station-tests/src/tests/rest/voting_key_saturation.rs
+++ src/vit-servicing-station/vit-servicing-station-tests/src/tests/rest/voting_key_saturation.rs
@@ -1,53 +1,52 @@
-use crate::common::{
-    data::{self},
-    startup::{quick_start},
-};
-use assert_fs::TempDir;
+use crate::common::startup::quick_start;
 use crate::common::snapshot::SnapshotBuilder;
+use assert_fs::TempDir;
 
-#[test] // api/v0/snapshot/voter/{tag}/{voting_key}
-pub fn get_voting_key_saturation() { // 2 snapshots required: 1 from SnapshotBuilder, the other from ArbitrarySnapshotGenerator
+const DIRECT_VOTING_GROUP_NAME: &str = "direct";
+const DREP_VOTING_GROUP_NAME: &str = "dreps";
+
+#[test]
+pub fn get_voting_key_saturation() {
     let temp_dir = TempDir::new().unwrap();
+
     let (server, _snapshot) = quick_start(&temp_dir).unwrap();
 
     let snapshot = SnapshotBuilder::default().build();
 
-    let (hash, _token) = data::token();
-
-    let client = server.rest_client_with_token(&hash);
+    let client = server.rest_client();
 
     let snapshot_tag = snapshot.clone().tag;
 
     client.put_snapshot_info(&snapshot).unwrap();
 
-    let total_voting_power = snapshot.content.snapshot.iter().map(|x| u64::from(x.hir.voting_power)).sum::<u64>();
+    let total_direct_voting_power = snapshot.content.snapshot.iter()
+        .filter(|x| x.hir.voting_group == DIRECT_VOTING_GROUP_NAME).map(|x| u64::from(x.hir.voting_power)).sum::<u64>() as f64;
 
-    println!("total voting power before cast: {:#?}", total_voting_power);
-
-    let total_voting_power_f = total_voting_power as f64;
-
-    println!("total voting power after cast: {:#?}", total_voting_power_f);
+    let total_drep_voting_power = snapshot.content.snapshot.iter()
+        .filter(|x| x.hir.voting_group == DREP_VOTING_GROUP_NAME).map(|x| u64::from(x.hir.voting_power)).sum::<u64>() as f64;
 
     for i in 0..snapshot.content.snapshot.len() {
         let key = snapshot.content.snapshot[i].hir.clone().voting_key.to_hex();
 
         let voter_info = client.voter_info(&snapshot_tag, &key).unwrap();
 
-        assert!(voter_info.voter_info.len() > 0, "Voter info is empty");
+        let voting_group = snapshot.content.snapshot[i].clone().hir.voting_group;
 
-        println!("voting power before cast: {:#?}", snapshot.content.snapshot[i].hir.clone().voting_power);
+        assert!(voter_info.voter_info.len() > 0, "Voter info is empty");
 
         let voting_power = u64::from(snapshot.content.snapshot[i].hir.clone().voting_power) as f64;
 
-        println!("voting power after cast: {:#?}", voting_power);
+        let mut expected_voting_key_saturation:f64 = 0.0;
 
-        let expected_voting_key_saturation = voting_power / total_voting_power_f;
-
-        println!("expected_voting_key_saturation: {:#?}", expected_voting_key_saturation);
+        if voting_group == DIRECT_VOTING_GROUP_NAME {
+            expected_voting_key_saturation = voting_power / total_direct_voting_power;
+        } else if voting_group == DREP_VOTING_GROUP_NAME {
+            expected_voting_key_saturation = voting_power / total_drep_voting_power;
+        } else {
+            assert!(false, "Voting group not recognized");
+        }
 
         let voting_key_saturation = client.voter_info(&snapshot_tag, &key).unwrap().voter_info.first().unwrap().voting_power_saturation;
-
-        println!("voting_key_saturation: {:#?}", voting_key_saturation);
 
         assert_eq!(expected_voting_key_saturation, voting_key_saturation);
     }

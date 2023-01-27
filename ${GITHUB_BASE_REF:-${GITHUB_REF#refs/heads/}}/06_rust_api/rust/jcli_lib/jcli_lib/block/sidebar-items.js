4f7c6aec584851cd6340d65de85224d3579c5fca
dkijania <dkijania@users.noreply.github.com>
2023-01-27 06:28:36 +0000

    deploy: 496d3aedca12ad097c8238c744a3150ad9f20a71

--- ${GITHUB_BASE_REF:-${GITHUB_REF#refs/heads/}}/06_rust_api/rust/jcli_lib/jcli_lib/block/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":[["Error",""],["Genesis","create block 0 of the blockchain (i.e. the genesis block)"]],"fn":[["load_block",""],["open_block_file",""],["open_output",""]],"struct":[["Common",""],["Input",""]]};

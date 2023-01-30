7472401adb6b950bcaa69ea8f288fba532522905
stevenj <stevenj@users.noreply.github.com>
2023-01-30 08:12:50 +0000

    deploy: 0a9c4e2ea34b790a1ed92aa6e0f1a2fa5816d0dc

---
+++ main/06_rust_api/rust/implementors/rlp/traits/trait.Decodable.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"chain_evm":[["impl Decodable for <a class=\"enum\" href=\"chain_evm/transaction/enum.EthereumUnsignedTransaction.html\" title=\"enum chain_evm::transaction::EthereumUnsignedTransaction\">EthereumUnsignedTransaction</a>"],["impl Decodable for <a class=\"struct\" href=\"chain_evm/transaction/struct.EthereumSignedTransaction.html\" title=\"struct chain_evm::transaction::EthereumSignedTransaction\">EthereumSignedTransaction</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

1ec987249e8af38ed69849bd7c9fb92d20d2b499
minikin <minikin@users.noreply.github.com>
2023-01-31 10:42:18 +0000

    Removed Draft Docs for build-all-features-in-ci

--- build-all-features-in-ci/06_rust_api/rust/chain_crypto/hash/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":[["HASH_SIZE_256",""]],"enum":[["Error",""]],"struct":[["Blake2b256","Blake2b 256 bits"]],"type":[["Result",""]]};
--- build-all-features-in-ci/06_rust_api/rust/chain_impl_mockchain/leadership/genesis/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":[["VrfEvalFailure",""]],"struct":[["LeadershipData","Genesis Praos leadership data for a specific epoch"],["Threshold","Threshold between 0.0 and 1.0"]],"type":[["Witness","Witness"],["WitnessOutput",""]]};

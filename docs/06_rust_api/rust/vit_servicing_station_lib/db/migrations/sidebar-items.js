09b75d17a92e43ac8111b254d581528617c5b45a
stevenj <stevenj@users.noreply.github.com>
2023-02-01 15:46:39 +0000

    deploy: 7e7a642b19b133b119f46a4ddd321bec2b6afb37

--- docs/06_rust_api/rust/vit_servicing_station_lib/db/migrations/sidebar-items.js
+++ docs/06_rust_api/rust/vit_servicing_station_lib/db/migrations/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["InitializeDbWithMigrationError",""]],"fn":[["initialize_db_with_migration",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["initialize_db_with_migration",""]],"type":[["InitializeDbWithMigrationError",""]]};

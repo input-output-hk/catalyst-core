6328bc1b4a3925c6a0d219fcbef92a87dfb6109d
saibatizoku <saibatizoku@users.noreply.github.com>
2023-02-07 05:42:42 +0000

    deploy: 30d054d220e780dba6318eb841292332d6830d27

---
+++ feature/voting-node-service/06_rust_api/rust/vit_servicing_station_lib/db/queries/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":[["api_tokens",""],["challenges",""],["community_advisors_reviews",""],["funds",""],["goals",""],["groups",""],["proposals",""],["search",""],["snapshot",""],["voteplans",""],["votes",""]]};

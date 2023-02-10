1c40b19cd20d6ea91d1ee20dc3ed84380b5c4c4d
jmgilman <jmgilman@users.noreply.github.com>
2023-02-10 22:39:14 +0000

    deploy: a8e86aecf589a1741e8533aab86ca0e2d7199f20

---
+++ remove-dot-std/06_rust_api/rust/implementors/zeroize/trait.ZeroizeOnDrop.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"hdkeygen":[["impl ZeroizeOnDrop for <a class=\"struct\" href=\"hdkeygen/rindex/struct.HdKey.html\" title=\"struct hdkeygen::rindex::HdKey\">HdKey</a>"]],
+"wallet":[["impl ZeroizeOnDrop for <a class=\"struct\" href=\"wallet/struct.ScrubbedBytes.html\" title=\"struct wallet::ScrubbedBytes\">ScrubbedBytes</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ remove-dot-std/06_rust_api/rust/vit_servicing_station_tests/common/snapshot/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["Snapshot",""],["SnapshotBuilder",""],["SnapshotUpdater",""],["VoterInfo",""],["VotingPower",""]]};

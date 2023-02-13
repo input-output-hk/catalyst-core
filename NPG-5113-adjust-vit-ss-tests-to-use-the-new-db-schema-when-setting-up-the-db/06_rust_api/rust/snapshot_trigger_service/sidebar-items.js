f9f6f59b9a4ce7c96f16d381f16a764a7eb8b06e
FelipeRosa <FelipeRosa@users.noreply.github.com>
2023-02-13 17:40:37 +0000

    deploy: 4e54ed299300947239354980a04d4b155bf9944f

---
+++ NPG-5113-adjust-vit-ss-tests-to-use-the-new-db-schema-when-setting-up-the-db/06_rust_api/rust/snapshot_trigger_service/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["Error",""]],"mod":[["client",""],["config",""],["rest",""]],"struct":[["Context",""],["TriggerServiceCommand",""]],"type":[["ContextState",""]]};
---
+++ NPG-5113-adjust-vit-ss-tests-to-use-the-new-db-schema-when-setting-up-the-db/06_rust_api/rust/vit_servicing_station_lib/v0/endpoints/search/requests/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["Column",""],["Constraint",""],["OrderBy",""],["SearchResponse",""],["Table",""]],"struct":[["SearchCountQuery",""],["SearchQuery",""]]};

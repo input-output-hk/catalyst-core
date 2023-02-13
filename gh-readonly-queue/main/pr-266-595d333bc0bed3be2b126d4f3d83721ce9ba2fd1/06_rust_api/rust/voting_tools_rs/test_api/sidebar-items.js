266e58e1cbdbbb48acc25ef6ea3c2b08349823b8
stevenj <stevenj@users.noreply.github.com>
2023-02-13 07:12:55 +0000

    deploy: c14286d6d6185e759b3bfde86612142c7399d95a

---
+++ gh-readonly-queue/main/pr-266-595d333bc0bed3be2b126d4f3d83721ce9ba2fd1/06_rust_api/rust/voting_tools_rs/test_api/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["MockDbProvider","Mock db provider based on [`InMemoryDbSync`] struct from [`mainnet_lib`] project."],["SnapshotOutputAssert","Fluent api for [`SnapshotEntry`] assertions"]],"trait":[["VerifiableSnapshotOutput","Allows [`SnapshotEntry`] struct to be assertable"]]};

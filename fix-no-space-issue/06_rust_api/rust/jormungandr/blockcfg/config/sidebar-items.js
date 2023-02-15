e7c8b0d9ef764384a7cde4bf7d2c7f9df0e9f355
kukkok3 <kukkok3@users.noreply.github.com>
2023-02-15 14:23:47 +0000

    Removed Draft Docs for fix-no-space-issue

--- fix-no-space-issue/06_rust_api/rust/jormungandr/blockcfg/config/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":[["ConfigParam",""],["Error","Possible errors"],["RewardParams",""],["Tag",""]],"struct":[["Block0Date","Seconds elapsed since 1-Jan-1970 (unix time)"],["TagIter","An iterator over the variants of [Self]"]]};

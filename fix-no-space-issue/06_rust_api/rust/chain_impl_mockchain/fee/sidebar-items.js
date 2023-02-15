b2cfef855c8b2b18cb12b460bb199bcf0f426d0b
kukkok3 <kukkok3@users.noreply.github.com>
2023-02-15 09:40:51 +0000

    deploy: 951d836322c6b3ae1d3338284e478d89b0be04ef

---
+++ fix-no-space-issue/06_rust_api/rust/chain_impl_mockchain/fee/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["LinearFee","Linear fee using the basic affine formula `COEFFICIENT * bytes(COUNT(tx.inputs) + COUNT(tx.outputs)) + CONSTANT + CERTIFICATE*COUNT(certificates)`."],["PerCertificateFee",""],["PerVoteCertificateFee",""]],"trait":[["FeeAlgorithm",""]]};

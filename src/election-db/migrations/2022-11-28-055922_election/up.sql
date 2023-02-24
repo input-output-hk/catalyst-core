4ea2f78451e15618b008af9de10872c25b6b5fcd
Joaquín Rosales <joaquin.rosales@iohk.io>
2023-02-23 19:01:35 -0600

    slim down voting_node table

--- src/election-db/migrations/2022-11-28-055922_election/up.sql
+++ src/election-db/migrations/2022-11-28-055922_election/up.sql
@@ -497,15 +497,13 @@ CREATE TABLE voting_power (
 -- This table is looked up by hostname
 CREATE TABLE voting_nodes (
     hostname NAME PRIMARY KEY,
-    pubkey TEXT,
-    seckey TEXT,
-    netkey TEXT,
-    extra JSONB
+    pubkey TEXT NOT NULL,
+    seckey TEXT NOT NULL,
+    netkey TEXT NOT NULL
 );
 
 COMMENT ON TABLE voting_nodes IS 'This table holds information for all nodes in the election, and it is used by nodes to self-bootstrap the blockchain.';
 COMMENT ON COLUMN voting_nodes.hostname IS 'Unique hostname for the voting node.';
-COMMENT ON COLUMN voting_nodes.seckey IS 'Secret key from Ed25519 pair for the node. NULL = not yet defined.';
-COMMENT ON COLUMN voting_nodes.pubkey IS 'Public key from Ed25519 pair for the node. NULL = not yet defined.';
-COMMENT ON COLUMN voting_nodes.netkey IS 'Ed25519 secret key for the node. NULL = not yet defined.';
-COMMENT ON COLUMN voting_nodes.extra IS 'Json Map defining node specific extra data, particularly the "leadership" information. NULL = Not yet defined. "leadership"."role" = one of: "leader0", "leader", or "follower", "leadership"."consensus_id" = a Ed25519Extended public key used only by "leader0" or "leader" nodes.';
+COMMENT ON COLUMN voting_nodes.seckey IS 'Secret key from Ed25519 pair for the node. Used as the node secret.';
+COMMENT ON COLUMN voting_nodes.pubkey IS 'Public key from Ed25519 pair for the node. Used as consensus_leader_id when the node is a leader.';
+COMMENT ON COLUMN voting_nodes.netkey IS 'Ed25519 secret key for the node. Used as the node p2p topology key.';

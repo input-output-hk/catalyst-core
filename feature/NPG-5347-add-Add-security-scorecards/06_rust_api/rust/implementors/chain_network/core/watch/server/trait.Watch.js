9bf44e61b3ad20640590ff6a7294bb7f91eed379
minikin <minikin@users.noreply.github.com>
2023-03-01 19:09:48 +0000

    Removed Draft Docs for feature/NPG-5347-add-Add-security-scorecards

--- feature/NPG-5347-add-Add-security-scorecards/06_rust_api/rust/implementors/chain_network/core/watch/server/trait.Watch.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"jormungandr":[["impl Watch for <a class=\"struct\" href=\"jormungandr/watch_client/struct.WatchClient.html\" title=\"struct jormungandr::watch_client::WatchClient\">WatchClient</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

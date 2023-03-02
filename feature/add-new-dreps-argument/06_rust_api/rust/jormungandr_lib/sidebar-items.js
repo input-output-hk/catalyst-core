c245f101332c35400be46e9fe1adce66818ec173
minikin <minikin@users.noreply.github.com>
2023-03-02 09:09:23 +0000

    Removed Draft Docs for feature/add-new-dreps-argument

--- feature/add-new-dreps-argument/06_rust_api/rust/jormungandr_lib/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":[["crypto","This module provides wrapper around the different cryptographic objects used in jormungandr, the jcli and the tests"],["interfaces",""],["multiaddr",""],["time","Time and related data structures"]]};

7f65cd947d02fec7c9271993b9c772d1d6a56b20
saibatizoku <saibatizoku@users.noreply.github.com>
2023-03-08 04:07:42 +0000

    Removed Draft Docs for fix-migrations

--- fix-migrations/06_rust_api/rust/chain_crypto/digest/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":[["Error",""]],"struct":[["Context","A Digest Context for the H digest algorithm"],["Digest",""],["DigestOf","A typed version of Digest"],["TryFromSliceError",""]],"trait":[["DigestAlg",""]]};
--- fix-migrations/06_rust_api/rust/vit_servicing_station_lib/db/queries/search/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":[["search_count_db",""],["search_db",""]],"struct":[["RANDOM",""]]};

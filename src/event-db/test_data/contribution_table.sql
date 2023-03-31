d1db51a6ad237a4f72c3943a8c4f170751261a0c
Mr-Leshiy <leshiy12345678@gmail.com>
2023-03-31 13:31:08 +0300

    add event-db test data insertion scripts

---
+++ src/event-db/test_data/contribution_table.sql
@@ -0,0 +1,30 @@
+INSERT INTO contribution 
+(row_id, stake_public_key, snapshot_id, voting_key, voting_weight, voting_key_idx, value, voting_group, reward_address)
+VALUES 
+(1, 'stake_public_key_1', 1, 'voting_key_1', 1, 1, 140, 'rep', 'reward_address_1'),
+(2, 'stake_public_key_2', 1, 'voting_key_1', 1, 1, 110, 'rep', 'reward_address_2'),
+(3, 'stake_public_key_3', 1, 'voting_key_2', 1, 1, 100, 'rep', 'reward_address_3'),
+(4, 'stake_public_key_4', 1, 'voting_key_2', 1, 1, 50, 'rep', 'reward_address_4'),
+(5, 'stake_public_key_5', 1, 'voting_key_3', 1, 1, 350, 'direct', 'reward_address_5'),
+(6, 'stake_public_key_6', 1, 'voting_key_5', 1, 1, 550, 'direct', 'reward_address_6'),
+
+(7, 'stake_public_key_1', 2, 'voting_key_1', 1, 1, 140, 'rep', 'reward_address_1'),
+(8, 'stake_public_key_2', 2, 'voting_key_1', 1, 1, 110, 'rep', 'reward_address_2'),
+(9, 'stake_public_key_3', 2, 'voting_key_2', 1, 1, 100, 'rep', 'reward_address_3'),
+(10, 'stake_public_key_4', 2, 'voting_key_2', 1, 1, 50, 'rep', 'reward_address_4'),
+(11, 'stake_public_key_5', 2, 'voting_key_3', 1, 1, 350, 'direct', 'reward_address_5'),
+(12, 'stake_public_key_6', 2, 'voting_key_5', 1, 1, 550, 'direct', 'reward_address_6'),
+
+(13, 'stake_public_key_1', 3, 'voting_key_1', 1, 1, 140, 'rep', 'reward_address_1'),
+(14, 'stake_public_key_2', 3, 'voting_key_1', 1, 1, 110, 'rep', 'reward_address_2'),
+(15, 'stake_public_key_3', 3, 'voting_key_2', 1, 1, 100, 'rep', 'reward_address_3'),
+(16, 'stake_public_key_4', 3, 'voting_key_2', 1, 1, 50, 'rep', 'reward_address_4'),
+(17, 'stake_public_key_5', 3, 'voting_key_3', 1, 1, 350, 'direct', 'reward_address_5'),
+(18, 'stake_public_key_6', 3, 'voting_key_5', 1, 1, 550, 'direct', 'reward_address_6'),
+
+(19, 'stake_public_key_1', 4, 'voting_key_1', 1, 1, 140, 'rep', 'reward_address_1'),
+(20, 'stake_public_key_2', 4, 'voting_key_1', 1, 1, 110, 'rep', 'reward_address_2'),
+(21, 'stake_public_key_3', 4, 'voting_key_2', 1, 1, 100, 'rep', 'reward_address_3'),
+(22, 'stake_public_key_4', 4, 'voting_key_2', 1, 1, 50, 'rep', 'reward_address_4'),
+(23, 'stake_public_key_5', 4, 'voting_key_3', 1, 1, 350, 'direct', 'reward_address_5'),
+(24, 'stake_public_key_6', 4, 'voting_key_5', 1, 1, 550, 'direct', 'reward_address_6');
---
+++ src/event-db/test_data/voter_table.sql
@@ -0,0 +1,22 @@
+INSERT INTO voter 
+(row_id, voting_key, snapshot_id, voting_group, voting_power)
+VALUES 
+(1, 'voting_key_1', 1, 'rep', 250),
+(2, 'voting_key_2', 1, 'rep', 150),
+(3, 'voting_key_3', 1, 'direct', 350),
+(4, 'voting_key_4', 1, 'direct', 550),
+
+(5, 'voting_key_1', 2, 'rep', 250),
+(6, 'voting_key_2', 2, 'rep', 150),
+(7, 'voting_key_3', 2, 'direct', 350),
+(8, 'voting_key_4', 2, 'direct', 550),
+
+(9, 'voting_key_1', 3, 'rep', 250),
+(10, 'voting_key_2', 3, 'rep', 150),
+(11, 'voting_key_3', 3, 'direct', 350),
+(12, 'voting_key_4', 3, 'direct', 550),
+
+(13, 'voting_key_1', 4, 'rep', 250),
+(14, 'voting_key_2', 4, 'rep', 150),
+(15, 'voting_key_3', 4, 'direct', 350),
+(16, 'voting_key_4', 4, 'direct', 550);

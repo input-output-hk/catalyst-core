65db5ccaf750f2bc4b19c80c35296387eed73594
Mr-Leshiy <leshiy12345678@gmail.com>
2023-04-06 12:10:00 +0300

    cleanup

--- src/cat-data-service/src/service/v1/mod.rs
+++ src/cat-data-service/src/service/v1/mod.rs
@@ -2,25 +2,10 @@ use crate::state::State;
 use axum::Router;
 use std::sync::Arc;
 
-mod chalenges;
-mod fund;
-mod proposals;
 mod registration;
-mod reviews;
 
 pub fn v1(state: Arc<State>) -> Router {
     let registration = registration::registration(state);
-    let fund = fund::fund();
-    let chalenges = chalenges::chalenges();
-    let proposals = proposals::proposals();
-    let reviews = reviews::reviews();
 
-    Router::new().nest(
-        "/v1",
-        registration
-            .merge(fund)
-            .merge(chalenges)
-            .merge(proposals)
-            .merge(reviews),
-    )
+    Router::new().nest("/v1", registration)
 }

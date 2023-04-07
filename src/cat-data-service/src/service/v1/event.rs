ce0ecfc8cb7598b548e8716c2ed57398c7e37a58
Mr-Leshiy <leshiy12345678@gmail.com>
2023-04-07 16:01:22 +0300

    add new event endpoint

--- src/cat-data-service/src/service/v1/event.rs
+++ src/cat-data-service/src/service/v1/event.rs
@@ -3,11 +3,18 @@ use crate::{
     state::State,
 };
 use axum::{extract::Path, routing::get, Router};
-use event_db::types::event::EventSummary;
+use event_db::types::event::{EventId, EventSummary};
 use std::sync::Arc;
 
 pub fn event(state: Arc<State>) -> Router {
     Router::new()
+        .route(
+            "/event/:event",
+            get({
+                let state = state.clone();
+                move |path| async { handle_result(event_exec(path, state).await).await }
+            }),
+        )
         .route(
             "/events",
             get({
@@ -33,6 +40,13 @@ pub fn event(state: Arc<State>) -> Router {
                 }
             }),
         )
+}
+
+async fn event_exec(Path(event): Path<EventId>, state: Arc<State>) -> Result<EventSummary, Error> {
+    tracing::debug!("event_exec, event: {0}", event.0);
+
+    let event = state.event_db.get_event(event).await?;
+    Ok(event)
 }
 
 async fn events_exec(state: Arc<State>) -> Result<Vec<EventSummary>, Error> {
@@ -80,6 +94,44 @@ mod tests {
     use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
     use event_db::types::event::EventId;
     use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn event_test() {
+        let state = Arc::new(State::new(None).await.unwrap());
+        let app = app(state);
+
+        let request = Request::builder()
+            .uri(format!("/api/v1/event/{0}", 1))
+            .body(Body::empty())
+            .unwrap();
+        let response = app.clone().oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(
+            String::from_utf8(response.into_body().data().await.unwrap().unwrap().to_vec())
+                .unwrap(),
+            serde_json::to_string(&EventSummary {
+                id: EventId(1),
+                name: "Test Fund 1".to_string(),
+                starts: DateTime::<Utc>::from_utc(
+                    NaiveDateTime::new(
+                        NaiveDate::from_ymd_opt(2020, 5, 1).unwrap(),
+                        NaiveTime::from_hms_opt(12, 0, 0).unwrap()
+                    ),
+                    Utc
+                ),
+                ends: DateTime::<Utc>::from_utc(
+                    NaiveDateTime::new(
+                        NaiveDate::from_ymd_opt(2020, 6, 1).unwrap(),
+                        NaiveTime::from_hms_opt(12, 0, 0).unwrap()
+                    ),
+                    Utc
+                ),
+                is_final: true,
+                reg_checked: None,
+            },)
+            .unwrap()
+        );
+    }
 
     #[tokio::test]
     async fn events_test() {
@@ -159,6 +211,13 @@ mod tests {
             ])
             .unwrap()
         );
+
+        let request = Request::builder()
+            .uri(format!("/api/v1/event/{0}", 10))
+            .body(Body::empty())
+            .unwrap();
+        let response = app.clone().oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
     }
 
     #[tokio::test]
@@ -167,7 +226,7 @@ mod tests {
         let app = app(state);
 
         let request = Request::builder()
-            .uri(format!("/api/v1/events/1"))
+            .uri(format!("/api/v1/events/{0}", 1))
             .body(Body::empty())
             .unwrap();
         let response = app.clone().oneshot(request).await.unwrap();
@@ -219,6 +278,13 @@ mod tests {
             ])
             .unwrap()
         );
+
+        let request = Request::builder()
+            .uri(format!("/api/v1/events/{0}", 10))
+            .body(Body::empty())
+            .unwrap();
+        let response = app.clone().oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
     }
 
     #[tokio::test]
@@ -227,7 +293,7 @@ mod tests {
         let app = app(state);
 
         let request = Request::builder()
-            .uri(format!("/api/v1/events/1/1"))
+            .uri(format!("/api/v1/events/{0}/{1}", 1, 1))
             .body(Body::empty())
             .unwrap();
         let response = app.clone().oneshot(request).await.unwrap();

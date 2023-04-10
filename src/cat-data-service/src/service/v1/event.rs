0069dabd8cb68f4a1e1dfd9a721df001cbce4b9a
Mr-Leshiy <leshiy12345678@gmail.com>
2023-04-10 10:41:57 +0300

    update events endpoint

--- src/cat-data-service/src/service/v1/event.rs
+++ src/cat-data-service/src/service/v1/event.rs
@@ -2,8 +2,13 @@ use crate::{
     service::{handle_result, Error},
     state::State,
 };
-use axum::{extract::Path, routing::get, Router};
+use axum::{
+    extract::{Path, Query},
+    routing::get,
+    Router,
+};
 use event_db::types::event::{EventId, EventSummary};
+use serde::Deserialize;
 use std::sync::Arc;
 
 pub fn event(state: Arc<State>) -> Router {
@@ -19,24 +24,7 @@ pub fn event(state: Arc<State>) -> Router {
             "/events",
             get({
                 let state = state.clone();
-                move || async { handle_result(events_exec(state).await).await }
-            }),
-        )
-        .route(
-            "/events/:offset",
-            get({
-                let state = state.clone();
-                move |path| async {
-                    handle_result(events_with_offset_exec(path, state).await).await
-                }
-            }),
-        )
-        .route(
-            "/events/:offset/:limit",
-            get({
-                move |path| async {
-                    handle_result(events_with_offset_and_limit_exec(path, state).await).await
-                }
+                move |query| async { handle_result(events_exec(query, state).await).await }
             }),
         )
 }
@@ -48,30 +36,26 @@ async fn event_exec(Path(event): Path<EventId>, state: Arc<State>) -> Result<Eve
     Ok(event)
 }
 
-async fn events_exec(state: Arc<State>) -> Result<Vec<EventSummary>, Error> {
-    tracing::debug!("events_exec");
-
-    let events = state.event_db.get_events(None, None).await?;
-    Ok(events)
+#[derive(Deserialize)]
+struct EventsQuery {
+    limit: Option<i64>,
+    offset: Option<i64>,
 }
 
-async fn events_with_offset_exec(
-    Path(offset): Path<i64>,
+async fn events_exec(
+    events_query: Query<EventsQuery>,
     state: Arc<State>,
 ) -> Result<Vec<EventSummary>, Error> {
-    tracing::debug!("events_with_offset_exec, offset: {offset}");
+    tracing::debug!(
+        "events_exec, limit: {0:?}, offset: {1:?}",
+        events_query.limit,
+        events_query.offset
+    );
 
-    let events = state.event_db.get_events(None, Some(offset)).await?;
-    Ok(events)
-}
-
-async fn events_with_offset_and_limit_exec(
-    Path((limit, offset)): Path<(i64, i64)>,
-    state: Arc<State>,
-) -> Result<Vec<EventSummary>, Error> {
-    tracing::debug!("events_with_offset_and_limit_exec, offset: {offset}, limit: {limit}");
-
-    let events = state.event_db.get_events(Some(limit), Some(offset)).await?;
+    let events = state
+        .event_db
+        .get_events(events_query.limit, events_query.offset)
+        .await?;
     Ok(events)
 }
 
@@ -130,6 +114,13 @@ mod tests {
             },)
             .unwrap()
         );
+
+        let request = Request::builder()
+            .uri(format!("/api/v1/event/{0}", 10))
+            .body(Body::empty())
+            .unwrap();
+        let response = app.clone().oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
     }
 
     #[tokio::test]
@@ -212,20 +203,7 @@ mod tests {
         );
 
         let request = Request::builder()
-            .uri(format!("/api/v1/event/{0}", 10))
-            .body(Body::empty())
-            .unwrap();
-        let response = app.clone().oneshot(request).await.unwrap();
-        assert_eq!(response.status(), StatusCode::NOT_FOUND);
-    }
-
-    #[tokio::test]
-    async fn events_with_offset_test() {
-        let state = Arc::new(State::new(None).await.unwrap());
-        let app = app(state);
-
-        let request = Request::builder()
-            .uri(format!("/api/v1/events/{0}", 1))
+            .uri(format!("/api/v1/events?offset={0}", 1))
             .body(Body::empty())
             .unwrap();
         let response = app.clone().oneshot(request).await.unwrap();
@@ -279,7 +257,71 @@ mod tests {
         );
 
         let request = Request::builder()
-            .uri(format!("/api/v1/events/{0}", 10))
+            .uri(format!("/api/v1/events?limit={0}", 1))
+            .body(Body::empty())
+            .unwrap();
+        let response = app.clone().oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(
+            String::from_utf8(response.into_body().data().await.unwrap().unwrap().to_vec())
+                .unwrap(),
+            serde_json::to_string(&vec![EventSummary {
+                id: EventId(1),
+                name: "Test Fund 1".to_string(),
+                starts: DateTime::<Utc>::from_utc(
+                    NaiveDateTime::new(
+                        NaiveDate::from_ymd_opt(2020, 5, 1).unwrap(),
+                        NaiveTime::from_hms_opt(12, 0, 0).unwrap()
+                    ),
+                    Utc
+                ),
+                ends: DateTime::<Utc>::from_utc(
+                    NaiveDateTime::new(
+                        NaiveDate::from_ymd_opt(2020, 6, 1).unwrap(),
+                        NaiveTime::from_hms_opt(12, 0, 0).unwrap()
+                    ),
+                    Utc
+                ),
+                is_final: true,
+                reg_checked: None,
+            },])
+            .unwrap()
+        );
+
+        let request = Request::builder()
+            .uri(format!("/api/v1/events?limit={0}&offset={1}", 1, 1))
+            .body(Body::empty())
+            .unwrap();
+        let response = app.clone().oneshot(request).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(
+            String::from_utf8(response.into_body().data().await.unwrap().unwrap().to_vec())
+                .unwrap(),
+            serde_json::to_string(&vec![EventSummary {
+                id: EventId(2),
+                name: "Test Fund 2".to_string(),
+                starts: DateTime::<Utc>::from_utc(
+                    NaiveDateTime::new(
+                        NaiveDate::from_ymd_opt(2021, 5, 1).unwrap(),
+                        NaiveTime::from_hms_opt(12, 0, 0).unwrap()
+                    ),
+                    Utc
+                ),
+                ends: DateTime::<Utc>::from_utc(
+                    NaiveDateTime::new(
+                        NaiveDate::from_ymd_opt(2021, 6, 1).unwrap(),
+                        NaiveTime::from_hms_opt(12, 0, 0).unwrap()
+                    ),
+                    Utc
+                ),
+                is_final: true,
+                reg_checked: None,
+            },])
+            .unwrap()
+        );
+
+        let request = Request::builder()
+            .uri(format!("/api/v1/events?offset={0}", 10))
             .body(Body::empty())
             .unwrap();
         let response = app.clone().oneshot(request).await.unwrap();

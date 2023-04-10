4590035fa17d074aa4683ede5a6ef94bd29c1282
Mr-Leshiy <Mr-Leshiy@users.noreply.github.com>
2023-04-10 14:02:52 +0000

    deploy: 98c8b0298607ca255fac6b6536188aba5918be05

--- cat-data-service-event/06_rust_api/rust/doc/event_db/types/event/sidebar-items.js
+++ cat-data-service-event/06_rust_api/rust/doc/event_db/types/event/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Event","EventDetails","EventGoal","EventId","EventRegistration","EventSchedule","EventSummary","VoterGroup","VotingPowerSettings","__SerializeWith","__Visitor"]};
+window.SIDEBAR_ITEMS = {"enum":["VotingPowerAlgorithm"],"struct":["Event","EventDetails","EventGoal","EventId","EventRegistration","EventSchedule","EventSummary","VoterGroup","VotingPowerSettings","__SerializeWith","__Visitor"]};

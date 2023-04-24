5911be25d9a53e8c7b55f6d0f476e699480b6d06
Mr-Leshiy <Mr-Leshiy@users.noreply.github.com>
2023-04-24 13:36:22 +0000

    deploy: 431dd05d478000fab9ee3a65d5d9e305e862cebd

---
+++ cat-data-service-proposal/06_rust_api/rust/doc/chain_impl_mockchain/fee/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["test"],"struct":["LinearFee","PerCertificateFee","PerVoteCertificateFee"],"trait":["FeeAlgorithm"]};
---
+++ cat-data-service-proposal/06_rust_api/rust/doc/event_db/types/event/proposal/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["Proposal","ProposalBallotDetails","ProposalDetails","ProposalSupplementalDetails","ProposerDetails","VotePlan"]};

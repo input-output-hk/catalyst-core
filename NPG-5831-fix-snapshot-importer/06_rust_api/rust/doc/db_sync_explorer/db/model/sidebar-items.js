4bd4782064e41a781a1a985c18da6588cb14ad46
FelipeRosa <FelipeRosa@users.noreply.github.com>
2023-04-24 13:50:20 +0000

    deploy: 0d0bcb8f980bcaa7f650277053f05e8803e2215f

--- NPG-5831-fix-snapshot-importer/06_rust_api/rust/doc/db_sync_explorer/db/model/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":["FIELDS"],"enum":["__Field"],"struct":["BehindDuration","Meta","Progress","TransactionConfirmation","__FieldVisitor","__Visitor"],"type":["TransactionConfirmationRow"]};
--- NPG-5831-fix-snapshot-importer/06_rust_api/rust/doc/db_sync_explorer/rest/v0/endpoints/mock/submit/handlers/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":["submit_tx"],"opaque":[""]};

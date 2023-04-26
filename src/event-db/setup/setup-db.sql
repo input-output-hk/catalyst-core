1b8ed640b6a01c99062fe9caf070977b2d7ce933
Steven Johnson <stevenj@users.noreply.github.com>
2023-04-27 00:38:23 +0700

    fix(event-db): Parameterize Event DB Init SQL scripts to make easier to use in production (#327)
    This adds parameters to the DB init scripts so they can be configured at
    run time with different DB/User and password settings.
    Make the built in dev options use the new parameters.

---
+++ src/event-db/setup/setup-db.sql
@@ -0,0 +1,57 @@
+-- Initialise the Project Catalyst Event Database.
+
+-- This script requires a number of variables to be set.
+-- They will default if not set externally.
+-- These variables can be set on the "psql" command line.
+-- Passwords may optionally be set by ENV Vars.
+-- This script requires "psql" is run inside a POSIX compliant shell.
+
+-- VARIABLES:
+
+-- The name of the Database to connect to.
+\if :{?dbName} \else
+  \set dbName CatalystEventDev
+\endif
+
+-- The description of the database.
+\if :{?dbDescription} \else
+  \set dbDescription 'Catalyst Event DB'
+\endif
+
+
+-- The db user with full permissions on the DB.
+\if :{?dbUser} \else
+  \set dbUser 'catalyst-event-dev'
+\endif
+
+-- The db users password.
+\if :{?dbUserPw} \else
+  \set dbUserPw `echo ${DB_USER_PW:-CHANGE_ME}`
+\endif
+
+-- DISPLAY ALL VARIABLES
+\echo VARIABLES:
+\echo -> dbName ................. = :dbName
+\echo -> dbDescription .......... = :dbDescription
+\echo -> dbUser ................. = :dbUser
+\echo -> dbUserPw / $DB_USER_PW . = :dbUserPw
+
+
+-- Cleanup if we already ran this before.
+DROP DATABASE IF EXISTS :"dbName";
+
+DROP USER IF EXISTS :"dbUser";
+
+-- Create the test user we will use with the local Catalyst-Event dev database.
+CREATE USER :"dbUser" WITH PASSWORD :'dbUserPw';
+
+-- Privileges for this user/role.
+ALTER DEFAULT privileges REVOKE EXECUTE ON functions FROM public;
+
+ALTER DEFAULT privileges IN SCHEMA public REVOKE EXECUTE ON functions FROM :"dbUser";
+
+-- Create the database.
+CREATE DATABASE :"dbName" WITH OWNER :"dbUser";
+
+COMMENT ON DATABASE :"dbName" IS :'dbDescription';
+

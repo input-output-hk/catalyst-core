1661b5ff4f56ef38f494e90469ece986db66fab4
Mr-Leshiy <Mr-Leshiy@users.noreply.github.com>
2023-04-26 14:12:42 +0000

    deploy: c416b2223bffbbf7814e8fd558bf515f0f5784b9

--- cat-data-service-proposal/06_rust_api/rust/doc/event_db/types/event/proposal/sidebar-items.js
+++ cat-data-service-proposal/06_rust_api/rust/doc/event_db/types/event/proposal/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Proposal","ProposalBallotDetails","ProposalDetails","ProposalId","ProposalSupplementalDetails","ProposerDetails","VotePlan"]};
+window.SIDEBAR_ITEMS = {"struct":["Proposal","ProposalBallotDetails","ProposalDetails","ProposalId","ProposalSummary","ProposalSupplementalDetails","ProposerDetails","VotePlan"]};

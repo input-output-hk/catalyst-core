83140d24baed76231601310294640f03acb7c18e
stevenj <stevenj@users.noreply.github.com>
2023-04-26 17:40:14 +0000

    Removed Draft Docs for fix/graphql

--- fix/graphql/06_rust_api/rust/doc/catalyst_toolbox/recovery/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["ReplayError"],"mod":["replay","tally"],"struct":["Replay"]};
--- fix/graphql/06_rust_api/rust/doc/jormungandr_automation/jormungandr/legacy/config/node/data/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":["FIELDS"],"enum":["__Field"],"struct":["LegacyNodeConfig","Log","P2p","TrustedPeer","__FieldVisitor","__Visitor"]};

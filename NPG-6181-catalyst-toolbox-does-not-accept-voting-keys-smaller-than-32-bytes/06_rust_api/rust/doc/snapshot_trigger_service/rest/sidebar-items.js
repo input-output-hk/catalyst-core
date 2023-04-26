6b6a3b04f8fc6b7245b2834a31dac47b57c49e9e
FelipeRosa <FelipeRosa@users.noreply.github.com>
2023-04-26 16:08:17 +0000

    deploy: 6fed3fe7a62e5768d56be7e6b07673468cf84326

---
+++ NPG-6181-catalyst-toolbox-does-not-accept-voting-keys-smaller-than-32-bytes/06_rust_api/rust/doc/snapshot_trigger_service/rest/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Error"],"fn":["authorize_token","job_new_handler","job_prameters_json_body","job_status_handler","report_invalid","start_rest_server"],"opaque":[""],"static":["S"],"struct":["__StaticPath"]};

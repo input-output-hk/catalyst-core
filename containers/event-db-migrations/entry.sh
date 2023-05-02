c965950c0bff11f9e1c8ee8f07636ce7189abff2
Joaquín Rosales <joaquin.rosales@iohk.io>
2023-05-02 13:41:11 -0600

    add eventdb-graphql container, update docker-compose files

--- containers/event-db-migrations/entry.sh
+++ containers/event-db-migrations/entry.sh
@@ -1,47 +1,34 @@
 #!/usr/bin/env sh
 
-# Set default value for initDb flag
-initDb=false
+# Set default value for reinitDb flag
+reinitDb=false
 
-# Check if initDb flag is set in environment variable
-if [ -n "$INIT_DB" ]; then
-  if [ "$INIT_DB" = true ] || [ "$INIT_DB" = false ]; then
-    initDb=$INIT_DB
+# Check if reinitDb flag is set in environment variable
+if [ -n "$REINIT_DB" ]; then
+  if [ "$REINIT_DB" = true ] || [ "$REINIT_DB" = false ]; then
+    reinitDb=$REINIT_DB
   else
-    echo "Invalid value for INIT_DB environment variable. Must be true or false."
+    echo "Invalid value for REINIT_DB environment variable. Must be true or false."
     exit 1
   fi
 fi
 
-# Parse command line arguments
-for arg in "$@"; do
-  case $arg in
-    -i|--initDb)
-      initDb=true
-      ;;
-    *)
-      # unknown option
-      echo "Unknown option: $arg"
-      exit 1
-      ;;
-  esac
-done
-
-# Check if initDb flag is set
-if [ "$initDb" = true ]; then
+# Check if the database has been initialized
+if [ ! -f ./tmp/initialized.txt ] || [ "$reinitDb" = true ]; then
   echo "Initializing database..."
-  psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -f /setup/setup-db.sql
+  psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -f ./setup/setup-db.sql
   echo "Initializing database for graphql..."
-  psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -f /setup/graphql-setup.sql
+  psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -f ./setup/graphql-setup.sql
+  touch ./tmp/initialized.txt
 else
-  echo "initDb flag not set. Skipping database initialization."
+  echo "Database already initialized. Skipping initialization."
 fi
 
 # Run migrations
-./refinery migrate -e DATABASE_URL -c ./refinery.toml -p /migrations
+./refinery migrate -e DATABASE_URL -c ./refinery.toml -p ./migrations
 
 # Add historic data from previous funds
-directory="/historic_data"
+directory="./historic_data"
 
 for file in $(ls "$directory"/*.sql | sort); do
   echo "Adding historic data from $file"

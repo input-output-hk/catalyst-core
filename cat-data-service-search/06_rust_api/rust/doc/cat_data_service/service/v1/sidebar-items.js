ac0ef1285afe1c895040b2d97c7ed6cc04a00387
Mr-Leshiy <Mr-Leshiy@users.noreply.github.com>
2023-05-08 09:15:39 +0000

    deploy: da77b6fe394ce88fbf050472cf478a68ee5c1124

--- cat-data-service-search/06_rust_api/rust/doc/cat_data_service/service/v1/sidebar-items.js
+++ cat-data-service-search/06_rust_api/rust/doc/cat_data_service/service/v1/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["v1"],"mod":["event","registration","search"]};
+window.SIDEBAR_ITEMS = {"constant":["FIELDS"],"enum":["__Field"],"fn":["v1"],"mod":["event","registration","search"],"struct":["LimitOffset","__FieldVisitor","__Visitor"]};

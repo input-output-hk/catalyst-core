a5e769d2a3b1b8cf8e29b77bc4e9ea69a4d38075
saibatizoku <saibatizoku@users.noreply.github.com>
2023-05-15 19:03:27 +0000

    deploy: bea63ab723281f0f059c3de6aed03dd13a762fd0

---
+++ deploy-cat-data-service/06_rust_api/rust/doc/jormungandr_lib/interfaces/committee/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["get_bytes"],"struct":["CommitteeId","CommitteeIdDef","__Visitor"]};
---
+++ deploy-cat-data-service/06_rust_api/rust/doc/mjolnir/mjolnir_lib/fragment/standard/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Standard"],"mod":["adversary","all","tx_only"]};

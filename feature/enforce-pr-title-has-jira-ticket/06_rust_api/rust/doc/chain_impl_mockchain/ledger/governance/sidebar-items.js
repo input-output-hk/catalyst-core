daca6674918d6db219fbd485e655c1ac0b9c598f
minikin <minikin@users.noreply.github.com>
2023-05-16 15:18:33 +0000

    deploy: 67c67079a1ec7a9655572da70b07ebf03943f4a0

---
+++ feature/enforce-pr-title-has-jira-ticket/06_rust_api/rust/doc/chain_impl_mockchain/ledger/governance/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["CENT"],"enum":["ParametersGovernanceAction","ParametersGovernanceActionType","TreasuryGovernanceAction","TreasuryGovernanceActionType"],"mod":["parameters","treasury"],"struct":["Governance","GovernanceAcceptanceCriteria","ParametersGovernance","TreasuryGovernance"]};
---
+++ feature/enforce-pr-title-has-jira-ticket/06_rust_api/rust/doc/chain_impl_mockchain/testing/scenario/controller/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ControllerError"],"struct":["Controller"]};

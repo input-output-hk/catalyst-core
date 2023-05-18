83d9a58c97fbc1671b6eced0730f778f695a86b0
Mr-Leshiy <Mr-Leshiy@users.noreply.github.com>
2023-05-18 14:15:03 +0000

    deploy: 614ff5870c7e014fddeda199f8178c7da090e18f

---
+++ feature/modify-supplemental-fields/06_rust_api/rust/doc/chain_ser/deser/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ReadError","WriteError"],"trait":["Deserialize","DeserializeFromSlice","Serialize"]};
---
+++ feature/modify-supplemental-fields/06_rust_api/rust/doc/scheduler_service_lib/rest/files/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Error"],"fn":["files_handler","filter"],"opaque":[""],"static":["S"],"struct":["__StaticPath"]};

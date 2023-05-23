da663cc74a4bd420d58f5e77759292480a9d1d52
Joaquín Rosales <joaquin.rosales@iohk.io>
2023-05-23 01:22:55 -0600

    build: improve entrypoint output, check envvars for voting-node

--- services/voting-node/entry.sh
+++ services/voting-node/entry.sh
@@ -12,6 +12,66 @@ set -o monitor
 set -o posix
 shopt -s dotglob
 
+check_env_vars() {
+    local env_vars=("$@")
+
+    # Iterate over the array and check if each variable is set
+    for var in "${env_vars[@]}"; do
+        echo "Checking $var"
+        if [ -z "${!var}" ]; then
+            echo ">>> Error: $var is required and not set."
+            exit 1
+        fi
+    done
+}
+
+debug_sleep() {
+    if [ -n "${DEBUG_SLEEP:-}" ]; then
+        echo "DEBUG_SLEEP is set. Sleeping for ${DEBUG_SLEEP} seconds..."
+        sleep "${DEBUG_SLEEP}"
+    fi
+}
+
+echo ">>> Starting entrypoint script..."
+
+# Check if all required environment variables are set
+REQUIRED_ENV=(
+    "IS_NODE_RELOADABLE"
+    "VOTING_HOST"
+    "VOTING_PORT"
+    "VOTING_LOG_LEVEL"
+    "VOTING_NODE_STORAGE"
+    "EVENTDB_URL"
+    "JORM_PATH"
+    "JCLI_PATH"
+)
+echo ">>> Checking required env vars..."
+check_env_vars "${REQUIRED_ENV[@]}"
+
+# Get the hostname
+HOSTNAME=$(hostname)
+
+# Check if the hostname is 'leader0'
+if [ "$HOSTNAME" = "leader0" ]; then
+    echo ">>> 'leader0' found. Checking required env vars..."
+    LEADER0_ENV=(
+        "COMMITTEE_CRS"
+        "SECRET_SECRET"
+        "IDEASCALE_API_TOKEN"
+        "IDEASCALE_CAMPAIGN_GROUP"
+        "IDEASCALE_STAGE_ID"
+        "IDEASCALE_LOG_LEVEL"
+        "IDEASCALE_API_URL"
+        "SNAPSHOT_CONFIG_PATH"
+        "SNAPSHOT_OUTPUT_DIR"
+        "SNAPSHOT_NETWORK_ID"
+    )
+    check_env_vars "${LEADER0_ENV[@]}"
+fi
+
+# Sleep if DEBUG_SLEEP is set
+debug_sleep
+
 # Define the command to be executed
 CMD_TO_RUN="voting-node start"
 
@@ -25,6 +85,7 @@ if [ -n "${DEBUG_SLEEP:-}" ]; then
   sleep "$DEBUG_SLEEP"
 fi
 
+echo ">>> Executing command..."
 # Expand the command with arguments and capture the exit code
 set +e
 eval "$CMD"

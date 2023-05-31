fd31fb0d834108cdf971d5d0dc6fe6c66a3c5f7c
Mr-Leshiy <leshiy12345678@gmail.com>
2023-05-31 13:53:20 +0300

    add new types

--- src/event-db/src/types/event/ballot.rs
+++ src/event-db/src/types/event/ballot.rs
@@ -1,6 +1,8 @@
 use crate::types::registration::VoterGroupId;
 use serde::Serialize;
 
+use super::{objective::ObjectiveId, proposal::ProposalId};
+
 #[derive(Debug, Clone, Serialize, PartialEq, Eq)]
 pub struct ObjectiveChoices(pub Vec<String>);
 
@@ -25,6 +27,24 @@ pub struct Ballot {
     pub choices: ObjectiveChoices,
     pub voteplans: GroupVotePlans,
 }
+
+#[derive(Debug, Clone, Serialize, PartialEq, Eq)]
+pub struct ProposalBallot {
+    pub proposal_id: ProposalId,
+    pub ballot: Ballot,
+}
+
+#[derive(Debug, Clone, Serialize, PartialEq, Eq)]
+pub struct ProposalBallots(pub Vec<ProposalBallot>);
+
+#[derive(Debug, Clone, Serialize, PartialEq, Eq)]
+pub struct ObjectiveBallots {
+    pub objective_id: ObjectiveId,
+    pub ballots: ProposalBallots,
+}
+
+#[derive(Debug, Clone, Serialize, PartialEq, Eq)]
+pub struct ObjectivesBallots(pub Vec<ObjectiveBallots>);
 
 #[cfg(test)]
 mod tests {
@@ -67,6 +87,111 @@ mod tests {
                             "encryption_key": "encryption_key 1"
                         }
                     ]
+                }
+            )
+        )
+    }
+
+    #[test]
+    fn proposal_ballot_json_test() {
+        let proposal_ballot = ProposalBallot {
+            proposal_id: ProposalId(1),
+            ballot: Ballot {
+                choices: ObjectiveChoices(vec![
+                    "Abstain".to_string(),
+                    "Yes".to_string(),
+                    "No".to_string(),
+                ]),
+                voteplans: GroupVotePlans(vec![VotePlan {
+                    chain_proposal_index: 1,
+                    group: VoterGroupId("rep".to_string()),
+                    ballot_type: BallotType("public".to_string()),
+                    chain_voteplan_id: "chain_voteplan_id 1".to_string(),
+                    encryption_key: Some("encryption_key 1".to_string()),
+                }]),
+            },
+        };
+
+        let json = serde_json::to_value(&proposal_ballot).unwrap();
+        assert_eq!(
+            json,
+            json!(
+                {
+                    "proposal_id": 1,
+                    "ballot":
+                        {
+                            "choices": [
+                                "Abstain",
+                                "Yes",
+                                "No"
+                            ],
+                            "voteplans": [
+                                {
+                                    "chain_proposal_index": 1,
+                                    "group": "rep",
+                                    "ballot_type": "public",
+                                    "chain_voteplan_id": "chain_voteplan_id 1",
+                                    "encryption_key": "encryption_key 1"
+                                }
+                            ]
+                        }
+                }
+            )
+        )
+    }
+
+    #[test]
+    fn objective_ballots_json_test() {
+        let objective_ballot = ObjectiveBallots {
+            objective_id: ObjectiveId(1),
+            ballots: ProposalBallots(vec![ProposalBallot {
+                proposal_id: ProposalId(1),
+                ballot: Ballot {
+                    choices: ObjectiveChoices(vec![
+                        "Abstain".to_string(),
+                        "Yes".to_string(),
+                        "No".to_string(),
+                    ]),
+                    voteplans: GroupVotePlans(vec![VotePlan {
+                        chain_proposal_index: 1,
+                        group: VoterGroupId("rep".to_string()),
+                        ballot_type: BallotType("public".to_string()),
+                        chain_voteplan_id: "chain_voteplan_id 1".to_string(),
+                        encryption_key: Some("encryption_key 1".to_string()),
+                    }]),
+                },
+            }]),
+        };
+
+        let json = serde_json::to_value(&objective_ballot).unwrap();
+        assert_eq!(
+            json,
+            json!(
+                {
+                    "objective_id": 1,
+                    "ballots":
+                        [
+                            {
+                                "proposal_id": 1,
+                                "ballot":
+                                    {
+                                        "choices": [
+                                            "Abstain",
+                                            "Yes",
+                                            "No"
+                                        ],
+                                        "voteplans": [
+                                            {
+                                                "chain_proposal_index": 1,
+                                                "group": "rep",
+                                                "ballot_type": "public",
+                                                "chain_voteplan_id": "chain_voteplan_id 1",
+                                                "encryption_key": "encryption_key 1"
+                                            }
+                                        ]
+                                    }
+                            }
+                        ]
                 }
             )
         )

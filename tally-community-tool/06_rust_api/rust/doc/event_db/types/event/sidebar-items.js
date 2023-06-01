6fb2450a95a5384aea6a4ebd971bbacf012cb615
cong-or <cong-or@users.noreply.github.com>
2023-06-01 08:00:01 +0000

    deploy: 05224ceae21eec9d1c22d239cafb06d7d9d64c3a

--- tally-community-tool/06_rust_api/rust/doc/event_db/types/event/sidebar-items.js
+++ tally-community-tool/06_rust_api/rust/doc/event_db/types/event/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["VotingPowerAlgorithm"],"mod":["objective","proposal","review"],"struct":["Event","EventDetails","EventGoal","EventId","EventRegistration","EventSchedule","EventSummary","VotingPowerSettings","__SerializeWith","__Visitor"]};
+window.SIDEBAR_ITEMS = {"enum":["VotingPowerAlgorithm"],"mod":["ballot","objective","proposal","review"],"struct":["Event","EventDetails","EventGoal","EventId","EventRegistration","EventSchedule","EventSummary","VotingPowerSettings","__SerializeWith","__Visitor"]};

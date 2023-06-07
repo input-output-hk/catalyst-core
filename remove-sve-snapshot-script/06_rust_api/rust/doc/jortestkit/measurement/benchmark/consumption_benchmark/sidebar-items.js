0cf32757e4129eaaa95fae557acc59b20ea9bcd8
kukkok3 <kukkok3@users.noreply.github.com>
2023-06-07 20:46:25 +0000

    deploy: 85e100f58010c307ebf0824e6e78155773d5854b

---
+++ remove-sve-snapshot-script/06_rust_api/rust/doc/jortestkit/measurement/benchmark/consumption_benchmark/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ConsumptionBenchmarkError"],"struct":["ConsumptionBenchmarkDef","ConsumptionBenchmarkFinish","ConsumptionBenchmarkRun","ConsumptionBenchmarkRunAsync"]};

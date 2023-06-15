42be95b1eb1824520bbe67f5e900b19b5f1a65c4
Joshua Gilman <joshuagilman@gmail.com>
2023-06-15 11:28:00 -0700

    refactor: migrates CI code to separate repository

--- ci/pkg/executor.go
+++
@@ -1,7 +0,0 @@
-package pkg
-
-// Executor is an interface that can execute programs.
-type Executor interface {
-	// Run executes a program.
-	Run(args ...string) (string, error)
-}
--- ci/pkg/git_client.go
+++
@@ -1,7 +0,0 @@
-package pkg
-
-// GitClient is an interface for interacting with git
-type GitClient interface {
-	// Tags returns a list of tags in the repository
-	Tags() ([]string, error)
-}

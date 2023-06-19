83c3b80e64a8f0bcfb1909a9a8a4acf80dc4ef25
nicopado <nicopado@users.noreply.github.com>
2023-06-19 09:02:54 +0000

    Removed Draft Docs for tests-initial-structure

--- tests-initial-structure/06_rust_api/rust/doc/mainnet_tools/snapshot/wormhole/config/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":["FIELDS"],"enum":["__Field"],"struct":["Config","ServicingStation","SnapshotService","__FieldVisitor","__Visitor"]};
--- tests-initial-structure/06_rust_api/rust/doc/voting_tools_rs/testing/test_api/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["fake","output_assertions"]};

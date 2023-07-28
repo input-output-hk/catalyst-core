f4dc385dabdde8180f240da25adbf579d1e7ed71
Alex Pozhylenkov <leshiy12345678@gmail.com>
2023-07-28 16:12:00 +0300

    feat: Update catalyst-toolbox `proposal-score` command | NPG-7776 (#491)
    # Description
    
    Modified `proposal-score` cli command, so now it is not inserted
    proposal's scores directly into the database.
    It inserts data into the provided `proposals.json` file.
    This update will simplify deployment process so the flow would the
    following:
    1. Run ideascale importer to get `proposals.json` file
    2. Take `reviews.csv` file with all reviews
    3. Run catalyst-toolbox `proposal-score` command and provide
    `reviews.csv` as an input, `proposals.json` as an input and as an output
    of the updated proposals with the added scores data

--- src/catalyst-toolbox/catalyst-toolbox/src/proposal_score/load.rs
+++ src/catalyst-toolbox/catalyst-toolbox/src/proposal_score/load.rs
@@ -1,4 +1,4 @@
-use super::{AligmentReviews, AuditabilityReviews, FeasibilityReviews, ProposalId, Review};
+use super::{AlignmentReviews, AuditabilityReviews, FeasibilityReviews, ProposalId, Review};
 use crate::utils;
 use serde::Deserialize;
 use std::{collections::HashMap, path::Path};
@@ -6,9 +6,15 @@ use std::{collections::HashMap, path::Path};
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error(transparent)]
+    IO(#[from] std::io::Error),
+    #[error(transparent)]
+    Json(#[from] serde_json::Error),
+    #[error(transparent)]
     Csv(#[from] csv::Error),
     #[error("Invalid review type value, should be either 0 or 1, provided: {0}")]
     InvalidReviewType(u32),
+    #[error("Invalid proposal data: {0}")]
+    InvalidProposalData(String),
 }
 
 const ALLOCATED_TYPE: u32 = 1;
@@ -31,7 +37,7 @@ pub struct ReviewsRow {
 
 pub fn load_reviews_from_csv(
     path: &Path,
-) -> Result<HashMap<ProposalId, (AligmentReviews, FeasibilityReviews, AuditabilityReviews)>, Error>
+) -> Result<HashMap<ProposalId, (AlignmentReviews, FeasibilityReviews, AuditabilityReviews)>, Error>
 {
     let rows: Vec<ReviewsRow> = utils::csv::load_data_from_csv::<_, b','>(path)?;
     let mut reviews_per_proposal = HashMap::new();
@@ -46,7 +52,7 @@ pub fn load_reviews_from_csv(
         let reviews = reviews_per_proposal
             .entry(ProposalId(row.proposal_id))
             .or_insert((
-                AligmentReviews(Vec::new()),
+                AlignmentReviews(Vec::new()),
                 FeasibilityReviews(Vec::new()),
                 AuditabilityReviews(Vec::new()),
             ));
@@ -67,16 +73,67 @@ pub fn load_reviews_from_csv(
     Ok(reviews_per_proposal)
 }
 
+fn retrieve_proposal_id(proposal: &serde_json::Value) -> Result<ProposalId, Error> {
+    Ok(ProposalId(
+        proposal
+            .get("proposal_id")
+            .ok_or_else(|| {
+                Error::InvalidProposalData("does not have \"proposal_id\" field".to_string())
+            })?
+            .as_str()
+            .ok_or_else(|| {
+                Error::InvalidProposalData(
+                    "invalid \"proposal_id\" field type, not a string".to_string(),
+                )
+            })?
+            .parse()
+            .map_err(|_| {
+                Error::InvalidProposalData(
+                    "invalid \"proposal_id\" field type, not a string encoded int".to_string(),
+                )
+            })?,
+    ))
+}
+
+pub fn load_proposals_from_json(
+    path: &Path,
+) -> Result<HashMap<ProposalId, serde_json::Value>, Error> {
+    let proposals: Vec<serde_json::Value> = serde_json::from_str(&std::fs::read_to_string(path)?)?;
+    proposals
+        .into_iter()
+        .map(
+            |proposal| -> Result<(ProposalId, serde_json::Value), Error> {
+                Ok((retrieve_proposal_id(&proposal)?, proposal))
+            },
+        )
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::path::PathBuf;
 
     #[test]
-    fn load_test() {
+    fn load_reviews_test() {
         let file = PathBuf::from("src/proposal_score/test_data/reviews-example.csv");
-
         let res = load_reviews_from_csv(&file).unwrap();
+        println!("{:?}", res);
+    }
+
+    #[test]
+    fn retrieve_proposal_id_test() {
+        let proposal = serde_json::json!({
+            "proposal_id": "32",
+        });
+        let proposal_id = retrieve_proposal_id(&proposal).unwrap();
+        assert_eq!(proposal_id, ProposalId(32));
+    }
+
+    #[test]
+    fn load_proposals_test() {
+        let file = PathBuf::from("src/proposal_score/test_data/proposals.json");
+        let res = load_proposals_from_json(&file).unwrap();
         println!("{:?}", res);
     }
 }

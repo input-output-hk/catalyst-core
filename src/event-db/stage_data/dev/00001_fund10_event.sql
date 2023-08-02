a5b2670f0eb758638f9ae31c40f7499a9fb838e6
Steven Johnson <sakurainds@gmail.com>
2023-08-02 12:56:54 +0700

    fix: Fix Committee size and deadlines to F10 parameters where known.

--- src/event-db/stage_data/dev/00001_fund10_event.sql
+++ src/event-db/stage_data/dev/00001_fund10_event.sql
@@ -28,27 +28,27 @@ INSERT INTO event (
 ) VALUES (
     10,
     'Fund 10',
-    'Fund 10 Description',
-    '2023-08-18 00:00:00',
-    '2023-08-23 00:00:00',
-    450000000,
-    1,
-    NULL,
-    '2023-06-16 19:56:00',
-    '2023-09-18 00:00:00',
-    '2023-06-22 00:00:00',
-    '2023-06-22 00:00:00',
-    '2023-06-22 00:00:00',
-    '2023-07-13 00:00:00',
-    '2023-07-20 00:00:00',
-    '2023-08-10 00:00:00',
-    '2023-08-31 00:00:00',
-    '2023-09-14 00:00:00',
-    '2023-09-18 00:00:00',
-    NULL,
-    NULL,
-    10,
-    7,
-    NULL,
-    NULL
+    'Catalyst Dev Environment - Fund 10',
+    '2023-08-18 21:00:00',  -- Registration Snapshot Time
+    '2023-08-23 22:00:00',  -- Snapshot Start.
+    450000000,              -- Voting Power Threshold
+    1,                      -- Max Voting Power PCT
+    NULL,                   -- Review Rewards
+    '2023-06-16 19:56:00',  -- Start Time
+    '2023-09-18 00:00:00',  -- End Time
+    '2023-06-22 00:00:00',  -- Insight Sharing Start
+    '2023-06-22 00:00:00',  -- Proposal Submission Start
+    '2023-06-22 00:00:00',  -- Refine Proposals Start
+    '2023-07-13 00:00:00',  -- Finalize Proposals Start
+    '2023-07-20 00:00:00',  -- Proposal Assessment Start
+    '2023-08-10 00:00:00',  -- Assessment QA Start
+    '2023-08-31 11:00:00',  -- Voting Starts
+    '2023-09-14 11:00:00',  -- Voting Ends
+    '2023-09-18 11:00:00',  -- Tallying Ends
+    NULL,                   -- Block 0 Data
+    NULL,                   -- Block 0 Hash
+    1,                      -- Committee Size
+    1,                      -- Committee Threshold
+    NULL,                   -- Extra
+    NULL                    -- Cast to
 );

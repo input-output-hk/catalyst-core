f6adef243a19b96582b158c7d4e836abceedfd00
Alex Pozhylenkov <leshiy12345678@gmail.com>
2023-08-08 12:00:09 +0300

    feat: Added different logs formatting for cat-data-service | NPG-5779 (#505)
    # Description
    
    Added different logs formatting as:
    - `full`, default human readable format
    - `json`,  full JSON format
    - `compact`, human readable less verbose output format

--- src/cat-data-service/src/logger.rs
+++ src/cat-data-service/src/logger.rs
@@ -1,9 +1,22 @@
 use clap::ValueEnum;
-use tracing::level_filters::LevelFilter;
-use tracing::subscriber::SetGlobalDefaultError;
-use tracing_subscriber::FmtSubscriber;
+use tracing::{level_filters::LevelFilter, subscriber::SetGlobalDefaultError};
+use tracing_subscriber::{
+    fmt::{
+        format::{DefaultFields, Format},
+        FormatEvent,
+    },
+    FmtSubscriber, Registry,
+};
 
+pub const LOG_FORMAT_DEFAULT: &str = "full";
 pub const LOG_LEVEL_DEFAULT: &str = "info";
+
+#[derive(ValueEnum, Clone)]
+pub enum LogFormat {
+    Full,
+    Compact,
+    Json,
+}
 
 #[derive(ValueEnum, Clone)]
 pub enum LogLevel {
@@ -24,9 +37,22 @@ impl From<LogLevel> for tracing::Level {
     }
 }
 
-pub fn init(log_level: LogLevel) -> Result<(), SetGlobalDefaultError> {
+fn init_impl<T>(format: T, log_level: LogLevel) -> Result<(), SetGlobalDefaultError>
+where
+    T: FormatEvent<Registry, DefaultFields> + Send + Sync + 'static,
+{
     let subscriber = FmtSubscriber::builder()
+        .event_format(format)
         .with_max_level(LevelFilter::from_level(log_level.into()))
         .finish();
+
     tracing::subscriber::set_global_default(subscriber)
+}
+
+pub fn init(log_format: LogFormat, log_level: LogLevel) -> Result<(), SetGlobalDefaultError> {
+    match log_format {
+        LogFormat::Full => init_impl(Format::default(), log_level),
+        LogFormat::Compact => init_impl(Format::default().compact(), log_level),
+        LogFormat::Json => init_impl(Format::default().json(), log_level),
+    }
 }

4fb89c38034a5f0133bdef93f3bd749b5503bc66
Mr-Leshiy <leshiy12345678@gmail.com>
2023-08-10 10:56:19 +0300

    remove deprecated fields

--- src/event-db/migrations/V4__proposal_tables.sql
+++ src/event-db/migrations/V4__proposal_tables.sql
@@ -102,16 +102,6 @@ CREATE TABLE proposal_review (
   assessor VARCHAR NOT NULL,
   assessor_level INTEGER,
   reward_address TEXT,
-
-  -- These fields are deprecated and WILL BE removed in a future migration.
-  -- They MUST only be used for Vit-SS compatibility.
-  impact_alignment_rating_given INTEGER,
-  impact_alignment_note VARCHAR,
-  feasibility_rating_given INTEGER,
-  feasibility_note VARCHAR,
-  auditability_rating_given INTEGER,
-  auditability_note VARCHAR,
-  ranking INTEGER,
   flags JSONB NULL,
 
   FOREIGN KEY (proposal_id) REFERENCES proposal(row_id) ON DELETE CASCADE,
@@ -124,32 +114,6 @@ COMMENT ON COLUMN proposal_review.proposal_id IS 'The Proposal this review is fo
 COMMENT ON COLUMN proposal_review.assessor IS 'Assessors Anonymized ID';
 COMMENT ON COLUMN proposal_review.assessor_level IS 'Assessors level ID';
 COMMENT ON COLUMN proposal_review.reward_address IS 'Assessors reward address';
-
-COMMENT ON COLUMN proposal_review.impact_alignment_rating_given IS
-'The  numeric rating assigned to the proposal by the assessor.
-DEPRECATED: Only used for Vit-SS compatibility.';
-COMMENT ON COLUMN proposal_review.impact_alignment_note IS
-'A note about why the impact rating was given.
-DEPRECATED: Only used for Vit-SS compatibility.';
-
-COMMENT ON COLUMN proposal_review.feasibility_rating_given IS
-'The numeric feasibility rating given.
-DEPRECATED: Only used for Vit-SS compatibility.';
-COMMENT ON COLUMN proposal_review.feasibility_note IS
-'A note about why the feasibility rating was given.
-DEPRECATED: Only used for Vit-SS compatibility.';
-
-COMMENT ON COLUMN proposal_review.auditability_rating_given IS
-'The numeric auditability rating given.
-DEPRECATED: Only used for Vit-SS compatibility.';
-COMMENT ON COLUMN proposal_review.auditability_note IS
-'A note about the auditability rating given.
-DEPRECATED: Only used for Vit-SS compatibility.';
-
-COMMENT ON COLUMN proposal_review.ranking IS
-'Numeric  Measure of quality of this review according to veteran community advisors.
-DEPRECATED: Only used for Vit-SS compatibility.
-';
 
 COMMENT ON COLUMN proposal_review.flags IS
 'OPTIONAL: JSON Array which defines the flags raised for this review.

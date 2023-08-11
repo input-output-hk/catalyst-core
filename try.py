2e852ffd124136b47fa864877078177a68c4cd38
Mr-Leshiy <leshiy12345678@gmail.com>
2023-08-11 13:34:06 +0300

    finish init version of dowloading reviews

--- try.py
+++ try.py
@@ -1,5 +1,12 @@
 import requests
 from lxml import html
+import uuid
+
+def random_string(string_length=6):
+    """Returns a random string of length string_length."""
+    random = str(uuid.uuid4()) # Convert UUID format to a Python string.
+    random = random.replace("-","") # Remove the UUID '-'.
+    return random[0:string_length] # Return the random string.
 
 class IdeaScaleClient:
 
@@ -19,36 +26,56 @@ class IdeaScaleClient:
 
         response = self.client.post(f"{self.ideascale_url}{login}", data=data)
 
-    def get_reviews(self, id):
-        export_endpoint = "/a/admin/workflow/survey-tools/assessment/report/statistic/export/assessment-details/"
-    
-        res = self.client.get(f"{self.ideascale_url}{export_endpoint}{id}") 
+    def download_reviews(self, funnel_id, out_dir):
+        def download_file(self, funnel_id, id, out_dir):
+            export_endpoint = "/a/admin/workflow/survey-tools/assessment/report/statistic/export/assessment-details/"
+            file_name = f"{funnel_id}_{random_string()}"
+
+            res = self.client.get(f"{self.ideascale_url}{export_endpoint}{id}") 
+            tree = html.fromstring(res.content)
+
+            # we are looking for '<div class="card panel export-result-progress" data-features="refresh-processing-item" data-processing-item-id="15622">'
+            # to get "15622" value as for example
+            item  = int(tree.find(".//div[@data-processing-item-id]").get("data-processing-item-id"))
+
+            export_data_endpoint = "/a/reporting/export-data/"
+            while True:
+                res = self.client.get(f"{ideascale_url}{export_data_endpoint}{item}")
+                if "Finished Processing" in res.text:
+                    download_endpoint = "/a/download-export-file/"
+
+                    res = self.client.get(f"{ideascale_url}{download_endpoint}{item}")
+                    f = open(f"{out_dir}/{file_name}.xls", "wb")
+                    f.write(res.content)
+                    return file_name
+
+
+        funnel_endpoint = "/a/admin/workflow/stages/funnel/"
+
+        res = self.client.get(f"{self.ideascale_url}{funnel_endpoint}{funnel_id}")
+        print(res.text)
         tree = html.fromstring(res.content)
 
-        # we are looking for '<div class="card panel export-result-progress" data-features="refresh-processing-item" data-processing-item-id="15622">'
-        # to get "15622" value as for example
-        item  = int(tree.find(".//div[@data-processing-item-id]").get("data-processing-item-id"))
-        print(item)
+        # we are looking for '<a href="/a/admin/workflow/survey-tools/assessment/report/statistic/139?fromStage=1">Assessments</a>'
+        # where we need to get url
+        items = tree.findall('.//a')
+        files = []
+        for item in items:
+            if item.text and "Assessments" in item.text:
+                id = int(item.get("href").replace("/a/admin/workflow/survey-tools/assessment/report/reviews/", "").split("?")[0])
 
-        export_data_endpoint = "/a/reporting/export-data/"
-        while True:
-            res = self.client.get(f"{ideascale_url}{export_data_endpoint}{item}")
-            if "Finished Processing" in res.text:
-                download_endpoint = "/a/download-export-file/"
-
-                url = f"{ideascale_url}{download_endpoint}{item}"
-
-                res = self.client.get(f"{ideascale_url}{download_endpoint}{item}")
-                print(len(res.content))
-                f = open("try.xls", "wb")
-                f.write(res.content)
-                break 
+                # we are intrested in only assessed reviews 
+                files.append(download_file(self, funnel_id, id, out_dir))
+        return files
 
 ideascale_url = "https://temp-cardano-sandbox.ideascale.com"
 email = "alex.pozhylenkov@iohk.io"
 password = "poghilenkov1995"
+out_dir = "tmp"
+funnel_id = 138
 
 client = IdeaScaleClient(ideascale_url)
 
 client.login(email, password)
-client.get_reviews(139)
+files = client.download_reviews(funnel_id, out_dir)
+print(len(files))

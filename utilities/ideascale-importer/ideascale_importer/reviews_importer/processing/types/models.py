58a673e6c9a170a555dd2583bf92ca335114d878
Alex Pozhylenkov <leshiy12345678@gmail.com>
2023-08-24 21:47:11 +0300

    fix: Review's importer processing logic fixing | NPG-7896 (#528)
    # Description
    
    - Fixed issues with the final step of processing reviews.
    - Moved `questions` into the config of the review's importer

--- utilities/ideascale-importer/ideascale_importer/reviews_importer/processing/types/models.py
+++ utilities/ideascale-importer/ideascale_importer/reviews_importer/processing/types/models.py
@@ -2,11 +2,14 @@
 from __future__ import annotations
 from pydantic import BaseModel, HttpUrl, Field, validator, root_validator
 from typing import List, Optional
+from enum import IntEnum
 import re
+import json
 
 
 class Model(BaseModel):
     """Base class for all models."""
+
 
 class Pa(Model):
     """Represents a PA."""
@@ -48,44 +51,56 @@ class Pa(Model):
                 values["challenge_ids"] = []
         return values
 
+class LightPa(Model):
+    pa_anon_id: int = Field(default=0)
+    pa_email: str = Field(default="")
+    pa_level: int = Field(default=0)
+    reviews_count: int = Field(default=0)
+    allocated_reviews_count: int = Field(default=0)
+    unallocated_reviews_count: int = Field(default=0)
 
 class Author(Model):
     """Represents an author."""
 
-    id: int
-    name: str
-    email: str
-    user_name: str
+    id: int = Field(default=0)
+    name: str = Field(default="")
+    email: str = Field(default="")
+    user_name: str = Field(default="")
 
 
 class Challenge(Model):
     """Represents a challenge."""
 
-    id: int
-    title: str
+    id: int = Field(default=0)
+    title: str = Field(default="")
     funds: Optional[int] = Field(default=None)
 
 
 class Proposal(Model):
     """Represents a proposal."""
 
-    id: int
+    id: int = Field(default=0)
     url: HttpUrl
-    title: str
-    challenge_id: int = Field(alias="campaign_id")
+    title: str = Field(default="")
+    challenge_id: int = Field(alias="campaign_id", default=0)
     times_picked: int = Field(default=0)
-    funds: Optional[int]
+    funds: Optional[int] = Field(default=None)
     allocations: List[Allocation] = Field(default=[])
     authors: List[Author] = Field(default=[])
-    public_email: Optional[str]
-    stage_id: Optional[int]
-    extra: Optional[dict]
+    public_email: Optional[str] = Field(default=None)
+    stage_id: Optional[int] = Field(default=None)
+    extra: Optional[dict] = Field(default=None)
+    review_stats: Optional[dict] = Field(default=None)
+    archived: Optional[bool] = Field(default=False)
 
     @root_validator(pre=True)
     @classmethod
     def assign_authors_if_any(cls, values):
         """Assign proposers/co-proposers merging different ideascale fields."""
         authors = []
+        if "stage_label" in values:
+            if values["stage_label"] == "Archive":
+                values["archived"] = True
         if "author_info" in values:
             authors.append(Author(**values["author_info"]))
         if "contributors" in values:
@@ -110,6 +125,14 @@ class Proposal(Model):
         """Return the full text combining all the fields of the proposal."""
         return " ".join(self.extra.values())
 
+class StatsProposal(Model):
+    id: int = Field(default=0)
+    url: HttpUrl
+    title: str = Field(default="")
+    challenge_id: int = Field(default=0)
+    no_reviews: int = Field(default=0)
+    avg_score: float = Field(default=0)
+
 
 class Allocation(Model):
     """Represent a proposal allocated to a PA."""
@@ -130,26 +153,46 @@ class AllocationLight(Model):
 class Review(Model):
     """Represent an imported Assessment."""
 
-    id: int
-    assessor: str = Field(alias="Assessor")
-    impact_note: str = Field(alias="Impact / Alignment Note")
-    impact_rating: int = Field(alias="Impact / Alignment Rating")
-    feasibility_note: str = Field(alias="Feasibility Note")
-    feasibility_rating: int = Field(alias="Feasibility Rating")
-    auditability_note: str = Field(alias="Auditability Note")
-    auditability_rating: int = Field(alias="Auditability Rating")
-    classification: Optional[str] = Field(alias="Result")
-    level: Optional[int]
-    allocated: Optional[bool]
-    pa: Optional[Pa]
-    proposal: Optional[Proposal]
-    fund: Optional[int]
-    proposal_id: Optional[int]
+    id: int = Field(default=0)
+    assessor: str = Field(alias="Assessor", default="")
+    impact_note: str = Field(alias="Impact / Alignment Note", default="")
+    impact_rating: int = Field(alias="Impact / Alignment Rating", default=0)
+    feasibility_note: str = Field(alias="Feasibility Note", default="")
+    feasibility_rating: int = Field(alias="Feasibility Rating", default=0)
+    auditability_note: str = Field(alias="Auditability Note", default="")
+    auditability_rating: int = Field(alias="Auditability Rating", default=0)
+    classification: Optional[str] = Field(alias="Result", default=None)
+    level: Optional[int] = Field(default=None)
+    allocated: Optional[bool] = Field(default=None)
+    pa: Optional[Pa] = Field(default=None)
+    proposal: Optional[Proposal] = Field(default=None)
+    fund: Optional[int] = Field(default=None)
+    proposal_id: Optional[int] = Field(default=None)
 
     @property
     def full_note(self):
         """Return the full not combining the single criteria notes."""
         return " ".join([self.impact_note, self.feasibility_note, self.auditability_note])
+    
+    def valid_by_length(self, min_length):
+        return (len(self.impact_note) >= min_length and len(self.feasibility_note) >= min_length and len(self.auditability_note) >= min_length)
+    
+    @root_validator(pre=True)
+    @classmethod
+    def adjust_id(cls, values):
+        """Set and id based on fund_id if present."""
+        if "fund" in values:
+            values["id"] = int(values["fund"]) * 100000 + int(values["id"])
+        return values
+
+def invert_pair(values):
+    left = values["left"]
+    left_criterium = values["left_criterium"]
+    values["left"] = values["right"]
+    values["left_criterium"] = values["right_criterium"]
+    values["right"] = left
+    values["right_criterium"] = left_criterium
+    return values
 
 
 class SimilarPair(Model):
@@ -165,13 +208,10 @@ class SimilarPair(Model):
     @classmethod
     def lower_always_left(cls, values):
         """Force the lower score to be on `left` field."""
-        if values["left"].id > values["right"].id:
-            left = values["left"]
-            left_criterium = values["left_criterium"]
-            values["left"] = values["right"]
-            values["left_criterium"] = values["right_criterium"]
-            values["right"] = left
-            values["right_criterium"] = left_criterium
+        if values["left"].fund < values["right"].fund:
+            values = invert_pair(values)
+        elif values["left"].id > values["right"].id:
+            values = invert_pair(values)
         return values
 
 
@@ -295,6 +335,11 @@ class IdeascaleExportedReview(Model):
         """Use lowercase only for emails."""
         return value.lower()
 
+    @validator("question", pre=True)
+    @classmethod
+    def tranform_question(cls, value):
+        return value.replace('\n', '').replace('\r', '').replace(' ', '')
+
     class Config:
         """Extra configuration options."""
 
@@ -305,7 +350,7 @@ class IdeascaleExportedReviewResult(Model):
     """Represents a review exported from Ideascale Excel file."""
 
     idea_id: int = Field(alias="Idea ID", default=0)
-    idea_title: str = Field(alias="Idea Title", default="")
+    idea_title: str = Field(alias="Title", default="")
     campaign_title: str = Field(alias="Idea Campaign", default="")
     question: str = Field(alias="Assessment Question", default="")
     email: str = Field(alias="Email", default="")
@@ -317,7 +362,103 @@ class IdeascaleExportedReviewResult(Model):
         """Use lowercase only for emails."""
         return value.lower()
 
+    @validator("question", pre=True)
+    @classmethod
+    def tranform_question(cls, value):
+        return value.replace('\n', '').replace('\r', '').replace(' ', '')
+
     class Config:
         """Extra configuration options."""
 
         anystr_strip_whitespace = True
+
+class LightSimilarPair(Model):
+    """Represent a pair of assessments similar with light information."""
+
+    left: int
+    right: int
+    score: float
+    left_criterium: str
+    right_criterium: str
+
+class LightAiFlag(Model):
+    """Represent a review criteria ai flag with light information."""
+
+    review: int
+    avg_generated_prob: float
+    generated_prob: float
+
+class LightProfanity(Model):
+    """Represent a review criteria profanity flag with light information."""
+
+    review: int = Field(alias="review_id")
+    score: float
+    criterium: str
+
+class FlagType(IntEnum):
+    """Enum to describe possible flag types."""
+
+    profanity = 0
+    similarity = 1
+    ai_generated = 2
+
+
+class Flag(Model):
+    """Submodel to store reviews flags."""
+
+    flag_type: FlagType
+    score: float
+    related_reviews: List[int] = Field([])
+    related_criteria: List[str] = Field([])
+
+class ReviewWithFlags(Review):
+    """Represent a review with flags."""
+
+    flags: List[Flag] | None
+
+class Moderator(Model):
+    """Represents a Moderator."""
+
+    row_id: Optional[int]
+    email: Optional[str]
+    name: Optional[str]
+    id: Optional[str] = Field(alias="ids")
+    encrypted_email: Optional[str]
+    password: Optional[str]
+    salt: Optional[str]
+    hashed_password: Optional[str]
+    hashed_email: Optional[str]
+    allocations: Optional[ReviewWithFlags] = Field(default=[])
+
+    @validator("email", pre=True)
+    @classmethod
+    def lower_email(cls, value):
+        """Use lowercase only for emails."""
+        return value.lower()
+
+class ModeratorSQL(Model):
+    """Represents a Moderator in SQL."""
+
+    row_id: int
+    id: str = Field(alias="hashed_email")
+    id2: str = Field(alias="hashed_password")
+    id3: str = Field(alias="salt")
+    value: str = Field(alias="extra")
+
+    @root_validator(pre=True)
+    @classmethod
+    def parse_value(cls, values):
+        extra = {
+            "active": True,
+            "force_reset": True,
+            "role": 2,
+            "encrypted_email": values["encrypted_email"],
+        }
+        values["extra"] = json.dumps(extra)
+        return values
+
+class ModeratorAllocationSQL(Model):
+    """Represents a Moderator allocation in SQL."""
+
+    review_id: int
+    user_id: int

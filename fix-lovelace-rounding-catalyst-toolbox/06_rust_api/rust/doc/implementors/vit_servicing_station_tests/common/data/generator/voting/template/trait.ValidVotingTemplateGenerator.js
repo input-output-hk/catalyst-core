eff8726bd73a69b4542880aed5ff0b8f80a565d5
kukkok3 <kukkok3@users.noreply.github.com>
2023-08-25 11:25:28 +0000

    deploy: 6f3feb28e9f1618da05d9db3b407d5033e10b789

---
+++ fix-lovelace-rounding-catalyst-toolbox/06_rust_api/rust/doc/implementors/vit_servicing_station_tests/common/data/generator/voting/template/trait.ValidVotingTemplateGenerator.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"vit_servicing_station_tests":[],
+"vitup":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

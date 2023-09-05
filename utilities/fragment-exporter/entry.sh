a3113fad38d4a766972961973b7e9f85981760b6
Joshua Gilman <joshuagilman@gmail.com>
2023-09-05 12:00:59 -0700

    refactor: updates entrypoint to calculate node address

--- utilities/fragment-exporter/entry.sh
+++ utilities/fragment-exporter/entry.sh
@@ -11,6 +11,45 @@ set -o monitor
 set -o posix
 shopt -s dotglob
 
+check_env_vars() {
+  local env_vars=("$@")
+
+  # Iterate over the array and check if each variable is set
+  for var in "${env_vars[@]}"; do
+    echo "Checking $var"
+    if [ -z "${!var}" ]; then
+      echo ">>> Error: $var is required and not set."
+      exit 1
+    fi
+  done
+}
+
+debug_sleep() {
+  if [ -n "${DEBUG_SLEEP:-}" ]; then
+    echo "DEBUG_SLEEP is set. Sleeping for ${DEBUG_SLEEP} seconds..."
+    sleep "${DEBUG_SLEEP}"
+  fi
+}
+
+echo ">>> Starting entrypoint script..."
+
+# Check if all required environment variables are set
+REQUIRED_ENV=(
+  "LEADER_IP_PREFIX"
+  "LEADER_PORT"
+  "FRAGMENT_EXPORTER_INDEX_PATH"
+)
+echo ">>> Checking required env vars..."
+check_env_vars "${REQUIRED_ENV[@]}"
+
+# Calculate the correct leader IP address (e.g. <prefix>.<index>)
+HOSTNAME=$(hostname)
+export FRAGMENT_EXPORTER_URL="http://${LEADER_IP_PREFIX}.${HOSTNAME##*-}:${LEADER_PORT}"
+
+# Print out the environment variables
+echo ">>> Using FRAGMENT_EXPORTER_URL: ${FRAGMENT_EXPORTER_URL}"
+echo ">>> Using FRAGMENT_EXPORTER_INDEX_PATH: ${FRAGMENT_EXPORTER_INDEX_PATH}"
+
 # Define the command to be executed
 CMD_TO_RUN="fragment_exporter"
 
@@ -18,20 +57,5 @@ CMD_TO_RUN="fragment_exporter"
 ARGS="$*"
 CMD="$CMD_TO_RUN $ARGS"
 
-# Wait for DEBUG_SLEEP seconds if the DEBUG_SLEEP environment variable is set
-if [ -n "${DEBUG_SLEEP:-}" ]; then
-  echo "DEBUG_SLEEP is set to ${DEBUG_SLEEP}. Sleeping..."
-  sleep "$DEBUG_SLEEP"
-fi
-
 # Expand the command with arguments and capture the exit code
-set +e
 eval "$CMD"
-EXIT_CODE=$?
-set -e
-
-# If the exit code is 0, the Python executable returned successfully
-if [ $EXIT_CODE -ne 0 ]; then
-  echo "Error: Python executable returned with exit code $EXIT_CODE"
-  exit 1
-fi

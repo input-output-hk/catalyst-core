67b148f933538ee859806096fd9140ef5b82ca89
Mr-Leshiy <leshiy12345678@gmail.com>
2023-09-19 13:38:54 +0300

    update voting_token docs

--- src/cat-data-service/src/service/common/objects/voter_group.rs
+++ src/cat-data-service/src/service/common/objects/voter_group.rs
@@ -13,6 +13,7 @@ pub(crate) struct VoterGroup {
     /// The identifier of voting power token used withing this group.
     /// All vote plans within a group are guaranteed to use the same token.
     #[oai(skip_serializing_if_is_none = true)]
+    #[oai(validator(max_length = 121, min_length = 59, pattern = r"[0-9a-f]{56}\.[0-9a-f]{2,64}"))]
     voting_token: Option<String>,
 }
 
@@ -20,7 +21,7 @@ impl Example for VoterGroup {
     fn example() -> Self {
         Self {
             group: Some(VoterGroupId::Direct),
-            voting_token: Some("voting_token id".to_string()),
+            voting_token: Some("134c2d0a0b5761445d3f2d08492a5c193e3a19194453511426153630.0418401957301613".to_string()),
         }
     }
 }

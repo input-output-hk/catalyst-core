62b2010ac16405c430da145fee5a83e9da76a2d3
stevenj <stevenj@users.noreply.github.com>
2023-11-02 16:33:32 +0000

    deploy: 4a2dc7a2c589f1850c44b0f2cf85ace7f2d782ad

---
+++ refs/tags/f10.0.1/06_rust_api/rust/doc/implementors/snapshot_lib/voting_group/trait.VotingGroupAssigner.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"snapshot_lib":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

0ec3d994f351a5f10e7add15578d90e734c554b0
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2023-11-03 16:00:58 +0100

    fix: fund times

--- src/event-db/stage_data/dev/00001_fund11_event.sql
+++ src/event-db/stage_data/dev/00001_fund11_event.sql
@@ -29,21 +29,21 @@ INSERT INTO event (
     11,
     'Fund 11',
     'Catalyst Testnet - Fund 11',
-    '2023-11-01 21:00:00',  -- Registration Snapshot Time
-    '2023-11-01 22:00:00',  -- Snapshot Start.
+    '2023-11-06 21:00:00',  -- Registration Snapshot Time
+    '2023-11-07 22:00:00',  -- Snapshot Start.
     450000000,              -- Voting Power Threshold
     1,                      -- Max Voting Power PCT
     NULL,                   -- Review Rewards
-    '2023-06-16 19:56:00',  -- Start Time
-    '2023-09-18 00:00:00',  -- End Time
-    '2023-06-22 00:00:00',  -- Insight Sharing Start
-    '2023-06-22 00:00:00',  -- Proposal Submission Start
-    '2023-06-22 00:00:00',  -- Refine Proposals Start
-    '2023-07-13 00:00:00',  -- Finalize Proposals Start
-    '2023-07-20 00:00:00',  -- Proposal Assessment Start
-    '2023-08-10 00:00:00',  -- Assessment QA Start
-    '2023-11-02 11:00:00',  -- Voting Starts
-    '2023-11-07 11:00:00',  -- Voting Ends
+    '2023-11-03 00:00:00',  -- Start Time
+    '2023-11-19 00:00:00',  -- End Time
+    '2023-11-04 00:00:00',  -- Insight Sharing Start
+    '2023-11-04 00:00:00',  -- Proposal Submission Start
+    '2023-11-04 00:00:00',  -- Refine Proposals Start
+    '2023-11-04 00:00:00',  -- Finalize Proposals Start
+    '2023-11-04 00:00:00',  -- Proposal Assessment Start
+    '2023-11-04 00:00:00',  -- Assessment QA Start
+    '2023-11-08 11:00:00',  -- Voting Starts
+    '2023-11-10 11:00:00',  -- Voting Ends
     '2023-11-18 11:00:00',  -- Tallying Ends
     NULL,                   -- Block 0 Data
     NULL,                   -- Block 0 Hash

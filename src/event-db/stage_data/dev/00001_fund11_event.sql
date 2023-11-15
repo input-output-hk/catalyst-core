160c2776ae5d37a3aaf3ee099b4d7e3f020c264c
Felipe Rosa <felipe.rosa@iohk.io>
2023-11-15 13:37:25 -0300

    chore(fix-stage-data-sql): Upsert stage data

--- src/event-db/stage_data/dev/00001_fund11_event.sql
+++ src/event-db/stage_data/dev/00001_fund11_event.sql
@@ -51,4 +51,28 @@ INSERT INTO event (
     1,                      -- Committee Threshold
     NULL,                   -- Extra
     NULL                    -- Cast to
-);
+) ON CONFLICT (row_id) DO UPDATE
+SET name = EXCLUDED.name,
+    description = EXCLUDED.description,
+    registration_snapshot_time = EXCLUDED.registration_snapshot_time,
+    snapshot_start = EXCLUDED.snapshot_start,
+    voting_power_threshold = EXCLUDED.voting_power_threshold,
+    max_voting_power_pct = EXCLUDED.max_voting_power_pct,
+    review_rewards = EXCLUDED.review_rewards,
+    start_time = EXCLUDED.start_time,
+    end_time = EXCLUDED.end_time,
+    insight_sharing_start = EXCLUDED.insight_sharing_start,
+    proposal_submission_start = EXCLUDED.proposal_submission_start,
+    refine_proposals_start = EXCLUDED.refine_proposals_start,
+    finalize_proposals_start = EXCLUDED.finalize_proposals_start,
+    proposal_assessment_start = EXCLUDED.proposal_assessment_start,
+    assessment_qa_start = EXCLUDED.assessment_qa_start,
+    voting_start = EXCLUDED.voting_start,
+    voting_end = EXCLUDED.voting_end,
+    tallying_end = EXCLUDED.tallying_end,
+    block0 = EXCLUDED.block0,
+    block0_hash = EXCLUDED.block0_hash,
+    committee_size = EXCLUDED.committee_size,
+    committee_threshold = EXCLUDED.committee_threshold,
+    extra = EXCLUDED.extra,
+    cast_to = EXCLUDED.cast_to;

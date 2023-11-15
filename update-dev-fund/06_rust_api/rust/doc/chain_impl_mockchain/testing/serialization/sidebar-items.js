4175959ea96ecb6e5cea9913e19643fd257cd63e
jmgilman <jmgilman@users.noreply.github.com>
2023-11-15 19:23:07 +0000

    deploy: 20dc9998a9a7b73b0e9e8d6d5b79b838c21ec33e

---
+++ update-dev-fund/06_rust_api/rust/doc/chain_impl_mockchain/testing/serialization/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["serialization_bijection"]};
---
+++ update-dev-fund/06_rust_api/rust/doc/jormungandr/utils/async_msg/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["channel"],"struct":["MessageBox","MessageQueue","SendError","TrySendError"]};

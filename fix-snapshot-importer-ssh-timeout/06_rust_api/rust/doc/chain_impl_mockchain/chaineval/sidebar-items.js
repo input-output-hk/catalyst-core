81a4c03f4ef6024114d858ac6fc80343c47425a0
FelipeRosa <FelipeRosa@users.noreply.github.com>
2023-11-17 21:22:33 +0000

    deploy: 6bdff1b17bf81f60b59219d932ce059f4e8131f2

---
+++ fix-snapshot-importer-ssh-timeout/06_rust_api/rust/doc/chain_impl_mockchain/chaineval/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ConsensusEvalContext"],"struct":["HeaderContentEvalContext","PraosNonce"]};
---
+++ fix-snapshot-importer-ssh-timeout/06_rust_api/rust/doc/jormungandr_automation/jcli/api/votes/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["committee","crs","tally"],"struct":["Votes"]};

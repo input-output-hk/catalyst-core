dabe2cadb53be83574c75e0fa1a8d8c4471030ef
saibatizoku <saibatizoku@users.noreply.github.com>
2023-11-29 04:36:15 +0000

    deploy: cb8ac84900f9e32b3c0db1d47adebfbd35ff1afc

---
+++ fix/update-catalyst-toolbox-call/06_rust_api/rust/doc/chain_impl_mockchain/update/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Error"],"mod":["tests"],"struct":["UpdateProposalState","UpdateState"]};
---
+++ fix/update-catalyst-toolbox-call/06_rust_api/rust/doc/wallet/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["EitherAccount","Error"],"mod":["account","blockchain","password","scheme","states","time","transaction"],"struct":["AccountId","ScrubbedBytes","Settings","Wallet"],"type":["Password"]};

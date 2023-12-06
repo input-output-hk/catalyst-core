9410272bf94185c61113e4be5cface4572a35eb7
Stefano Cunego <93382903+kukkok3@users.noreply.github.com>
2023-12-06 18:22:54 +0100

    feat: Fund11 prod dates | NPG-000 (#640)
    # Description
    
    Adding fund 11 dates
    The idea scale importer sql file will be updated in another ticket
    Fixes https://github.com/input-output-hk/catalyst-core/issues/639
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce.
    Please also list any relevant details for your test configuration
    
    - [ ] Test A
    - [ ] Test B
    
    **Test Configuration**: _if applicable_
    
    - Firmware version:
    - Hardware:
    - Toolchain:
    - SDK:
    
    **List of new dependencies**: _if applicable_
    
    Provide a list of newly added dependencies in this PR, with the
    description of what are they doing and why do we need them.
    
    - Crate A: description
    - Crate B: description
    
    ## Checklist
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged and published in downstream
    modules

---
+++ src/event-db/stage_data/prod/00001_fund11_event.sql
@@ -0,0 +1,78 @@
+-- F11
+INSERT INTO event (
+    row_id,
+    name,
+    description,
+    registration_snapshot_time,
+    snapshot_start,
+    voting_power_threshold,
+    max_voting_power_pct,
+    review_rewards,
+    start_time,
+    end_time,
+    insight_sharing_start,
+    proposal_submission_start,
+    refine_proposals_start,
+    finalize_proposals_start,
+    proposal_assessment_start,
+    assessment_qa_start,
+    voting_start,
+    voting_end,
+    tallying_end,
+    block0,
+    block0_hash,
+    committee_size,
+    committee_threshold,
+    extra,
+    cast_to
+) VALUES (
+    11,
+    'Fund 11',
+    'Catalyst Prod - Fund 11',
+    '2024-01-15 21:45:00',  -- Registration Snapshot Time
+    '2024-01-20 22:15:00',  -- Snapshot Start.
+    450000000,              -- Voting Power Threshold
+    1,                      -- Max Voting Power PCT
+    NULL,                   -- Review Rewards
+    '2023-11-23 11:00:00',  -- Start Time
+    '2024-02-23 03:00:00',  -- End Time
+    '2023-11-16 11:00:00',  -- Insight Sharing Start
+    '2023-11-16 11:00:00',  -- Proposal Submission Start
+    '2023-11-16 11:00:00',  -- Refine Proposals Start
+    '2023-11-30 11:00:00',  -- Finalize Proposals Start
+    '2023-12-14 11:00:00',  -- Proposal Assessment Start
+    '2024-01-11 11:00:00',  -- Assessment QA Start
+    '2024-01-25 11:00:00',  -- Voting Starts
+    '2024-02-08 11:00:00',  -- Voting Ends
+    '2024-02-23 03:00:00',  -- Tallying Ends
+    NULL,                   -- Block 0 Data
+    NULL,                   -- Block 0 Hash
+    1,                      -- Committee Size
+    1,                      -- Committee Threshold
+    NULL,                   -- Extra
+    NULL                    -- Cast to
+) ON CONFLICT (row_id) DO UPDATE
+SET name = EXCLUDED.name,
+    description = EXCLUDED.description,
+    registration_snapshot_time = EXCLUDED.registration_snapshot_time,
+    snapshot_start = EXCLUDED.snapshot_start,
+    voting_power_threshold = EXCLUDED.voting_power_threshold,
+    max_voting_power_pct = EXCLUDED.max_voting_power_pct,
+    review_rewards = EXCLUDED.review_rewards,
+    start_time = EXCLUDED.start_time,
+    end_time = EXCLUDED.end_time,
+    insight_sharing_start = EXCLUDED.insight_sharing_start,
+    proposal_submission_start = EXCLUDED.proposal_submission_start,
+    refine_proposals_start = EXCLUDED.refine_proposals_start,
+    finalize_proposals_start = EXCLUDED.finalize_proposals_start,
+    proposal_assessment_start = EXCLUDED.proposal_assessment_start,
+    assessment_qa_start = EXCLUDED.assessment_qa_start,
+    voting_start = EXCLUDED.voting_start,
+    voting_end = EXCLUDED.voting_end,
+    tallying_end = EXCLUDED.tallying_end,
+    block0 = EXCLUDED.block0,
+    block0_hash = EXCLUDED.block0_hash,
+    committee_size = EXCLUDED.committee_size,
+    committee_threshold = EXCLUDED.committee_threshold,
+    extra = EXCLUDED.extra,
+    cast_to = EXCLUDED.cast_to;

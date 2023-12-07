39ec2800f7e7572d76460095e1b88e0c05a2d18c
kukkok3 <kukkok3@users.noreply.github.com>
2023-12-07 11:34:57 +0000

    deploy: 718a1bcdb9ee50f8ffd7fe7185f4c2a2ff6772ef

---
+++ dryrun-update-catdataservice-dates/06_rust_api/rust/doc/jormungandr_automation/jormungandr/starter/params/builder/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["create_new_leader_key"],"struct":["JormungandrBootstrapper"]};
---
+++ dryrun-update-catdataservice-dates/06_rust_api/rust/doc/offline/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["main","read_block0"],"struct":["Args"]};

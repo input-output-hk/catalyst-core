04f118f3bbda85533fa534591ea5e6655e3d556f
kukkok3 <kukkok3@users.noreply.github.com>
2024-01-17 14:02:02 +0000

    deploy: ec87eee5812ee0804258749786fe4743caad7829

---
+++ test-branch/06_rust_api/rust/doc/chain_impl_mockchain/block/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["BlockDateParseError"],"fn":["builder"],"mod":["builder","header","test"],"struct":["Block","BlockDate"],"type":["Epoch","SlotId"]};
---
+++ test-branch/06_rust_api/rust/doc/mainnet_tools/cardano_cli/mock/fake/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["address","hash","protocol_parameters","sign","stake_address","stake_certificate","submit","tip","transaction","utxo"],"struct":["QueryUTxO","Tip","UTxO"]};

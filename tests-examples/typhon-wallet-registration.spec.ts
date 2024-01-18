ade6d33e62cc37236a03a0c800ac457d4230fdfe
vasapornchaiyakul <134899205+vasapornchaiyakul@users.noreply.github.com>
2024-01-18 20:51:35 +0700

    fix: reaching the typhon wallet extension

--- tests-examples/typhon-wallet-registration.spec.ts
+++ tests-examples/typhon-wallet-registration.spec.ts
@@ -1,12 +1,14 @@
 import { test, chromium } from '@playwright/test';
 
 test('Open Extension Page', async ({}) => {
-  const extensionPath: string = 'extensions/kfdniefadaanbjodldohaedphafoffoh/3.0.23_0/manifest.json';
+  const extensionPath: string = '/Users/alicechaiyakul/typhon-wallet-registration/catalyst-core/extensions';
   const extensionId: string = 'kfdniefadaanbjodldohaedphafoffoh'; // Replace with your extension's ID
   const extensionPage: string = 'tab.html'; // Replace with the specific page
+  const userDataDir = '/Users/alicechaiyakul/typhon-wallet-registration/catalyst-core/tests-examples/src'; // Path to the user data directory
+
 
   // Launch Chromium with the extension
-  const browser = await chromium.launch({
+  const browser = await chromium.launchPersistentContext(userDataDir, {
     headless: false, // Extensions only work in headful mode
     args: [
       `--disable-extensions-except=${extensionPath}`,
@@ -15,15 +17,12 @@ test('Open Extension Page', async ({}) => {
   });
 
   // Creating a new context and page
-  const context = await browser.newContext();
-  const page = await context.newPage();
+  const page = await browser.newPage();
 
   // Navigating to the extension's page
   await page.goto(`chrome-extension://kfdniefadaanbjodldohaedphafoffoh/tab.html#/wallet/access`);
 
   // Keeping the browser open (remove the timeout or adjust as needed)
   await page.waitForTimeout(300000); // Adjust the time as needed
-
-  // Optionally, close the context if you want to close the browser programmatically
-  // await context.close();
+  await new Promise(resolve => { /* never resolves */ });
 });

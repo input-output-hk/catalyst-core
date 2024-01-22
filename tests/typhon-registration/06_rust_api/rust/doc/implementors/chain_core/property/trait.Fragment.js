de5a26b2cb50a2537fc753e04155e68bce847055
alicechaitea <alicechaitea@users.noreply.github.com>
2024-01-22 10:15:41 +0000

    deploy: 73e11615b8a1bbf4f3eb29cf3550fc06934f33f7

--- tests/typhon-registration/06_rust_api/rust/doc/implementors/chain_core/property/trait.Fragment.js
+++ tests/typhon-registration/06_rust_api/rust/doc/implementors/chain_core/property/trait.Fragment.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"chain_impl_mockchain":[["impl <a class=\"trait\" href=\"chain_core/property/trait.Fragment.html\" title=\"trait chain_core::property::Fragment\">Fragment</a> for <a class=\"enum\" href=\"chain_impl_mockchain/fragment/enum.Fragment.html\" title=\"enum chain_impl_mockchain::fragment::Fragment\">Fragment</a>"]]
+"chain_impl_mockchain":[["impl Fragment for <a class=\"enum\" href=\"chain_impl_mockchain/fragment/enum.Fragment.html\" title=\"enum chain_impl_mockchain::fragment::Fragment\">Fragment</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

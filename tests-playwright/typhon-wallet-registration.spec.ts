7da08e586a95d1dd27877a33ca891d2f706456a3
vasapornchaiyakul <134899205+vasapornchaiyakul@users.noreply.github.com>
2024-01-23 11:55:03 +0700

    fix: wrong redirection

--- tests-playwright/typhon-wallet-registration.spec.ts
+++ tests-playwright/typhon-wallet-registration.spec.ts
@@ -2,7 +2,7 @@ import { test, chromium, BrowserContext, Page } from '@playwright/test';
 import { getWalletCredentials } from './credentials';
 import { getSeedPhrase } from './seed-phrase';
 
-test('Open Extension Page and Click Button with XPath', async ({}) => {
+test('Open Extension Page and Click Button with XPath', async ({ }) => {
   const extensionPath: string = '/Users/alicechaiyakul/typhon-wallet-registration/catalyst-core/extensions';
   // const extensionId: string = 'kfdniefadaanbjodldohaedphafoffoh'; // Replace with your extension's ID
   // const extensionPage: string = 'tab.html'; // Replace with the specific page
@@ -65,70 +65,65 @@ test('Open Extension Page and Click Button with XPath', async ({}) => {
   await newTab.waitForSelector(continueButton, { state: 'visible' });
   await newTab.click(continueButton);
 
+  async function clickBlankSpace(newTab) {
+    const blankSpace = '#app > div > div > div.flex-grow.overflow-auto > div > div.container.mx-auto.flex.justify-between.items-center > a > img';
+    await newTab.waitForSelector(blankSpace, { state: 'visible' });
+    await newTab.click(blankSpace);
+  }
+
   const seedPhrase = getSeedPhrase();
 
-for (let i = 0; i < seedPhrase.length; i++) {
-    // Construct the selector dynamically for each element in the seedPhrase
+  for (let i = 0; i < seedPhrase.length; i++) {
     const ftSeedPhraseSelector = `#app > div > div > div.flex-grow.overflow-auto > div > div.my-5.flex.justify-center.py-16 > div > div > div > div:nth-child(1) > div:nth-child(2) > div > div > div > div > div:nth-child(${i + 1}) > div > input`;
-
     await newTab.waitForSelector(ftSeedPhraseSelector, { state: 'visible' });
     await newTab.fill(ftSeedPhraseSelector, seedPhrase[i]);
-}
-const blankSpace = '#app > div > div > div.flex-grow.overflow-auto > div > div.container.mx-auto.flex.justify-between.items-center > a > img';
-await newTab.waitForSelector(blankSpace, { state: 'visible' });
-await newTab.click(blankSpace);
+  }
 
-const unlockWallet = '#app > div > div > div.flex-grow.overflow-auto > div > div.my-5.flex.justify-center.py-16 > div > div > div > div.mt-6.text-center.flex.justify-center > button';
-await newTab.waitForSelector(unlockWallet, { state: 'visible' });
-await newTab.click(unlockWallet);
+  const unlockWallet = '#app > div > div > div.flex-grow.overflow-auto > div > div.my-5.flex.justify-center.py-16 > div > div > div > div.mt-6.text-center.flex.justify-center > button';
+  await clickBlankSpace(newTab);
+  await newTab.waitForSelector(unlockWallet, { state: 'visible' });
+  await newTab.click(unlockWallet);
 
-// const logOut = '#app > div > div > div.flex-grow.overflow-auto > div > div > div.hidden.h-full.overflow-y-scroll.flex-grow-0.flex-shrink-0.lg\:block.lg\:w-64 > div > div > div.mt-5.flex-grow.flex.flex-col.pl-3.pr-7.text-sm.labelColor2 > div.p-3.flex.space-x-3.items-center.cursor-pointer.hover\:text-primary > div:nth-child(2)';
-// await newTab.waitForSelector(logOut, { state: 'visible' });
-// await newTab.click(logOut);
+  // const logOut = '#app > div > div > div.flex-grow.overflow-auto > div > div > div.hidden.h-full.overflow-y-scroll.flex-grow-0.flex-shrink-0.lg\:block.lg\:w-64 > div > div > div.mt-5.flex-grow.flex.flex-col.pl-3.pr-7.text-sm.labelColor2 > div.p-3.flex.space-x-3.items-center.cursor-pointer.hover\:text-primary > div:nth-child(2)';
+  // await newTab.waitForSelector(logOut, { state: 'visible' });
+  // await newTab.click(logOut);
 
-// const chooseAccount = '#app > div > div > div.flex-grow.overflow-auto > div > div.flex.justify-center > div > div > div.w-full.flex-grow.flex.flex-col.overflow-scroll.dark\:bg-dark-2.border-b.dark\:border-dark-3.pt-7.pb-2.px-4.space-y-2 > div';
-// await newTab.waitForSelector(chooseAccount, { state: 'visible' });
-// await newTab.click(chooseAccount);
+  // const chooseAccount = '#app > div > div > div.flex-grow.overflow-auto > div > div.flex.justify-center > div > div > div.w-full.flex-grow.flex.flex-col.overflow-scroll.dark\:bg-dark-2.border-b.dark\:border-dark-3.pt-7.pb-2.px-4.space-y-2 > div';
+  // await newTab.waitForSelector(chooseAccount, { state: 'visible' });
+  // await newTab.click(chooseAccount);
 
-// const removeAccount = '#app > div > div > div.flex-grow.overflow-auto > div > div.flex.justify-center > div > div > div.flex-grow.flex.flex-col.items-center.pt-5.w-full > div.flex.items-center.justify-between.border-t.border-primary-8.dark\:border-dark-3.bg-primary-2.dark\:bg-dark-2.pl-3.pr-2.py-3.space-x-2 > button';
-// await newTab.waitForSelector(removeAccount, { state: 'visible' });
-// await newTab.click(removeAccount);
+  // const removeAccount = '#app > div > div > div.flex-grow.overflow-auto > div > div.flex.justify-center > div > div > div.flex-grow.flex.flex-col.items-center.pt-5.w-full > div.flex.items-center.justify-between.border-t.border-primary-8.dark\:border-dark-3.bg-primary-2.dark\:bg-dark-2.pl-3.pr-2.py-3.space-x-2 > button';
+  // await newTab.waitForSelector(removeAccount, { state: 'visible' });
+  // await newTab.click(removeAccount);
 
-// const confirmRemove = '#headlessui-dialog-17 > div > div.relative.inline-block.w-full.text-left.align-middle.cardColor.rounded-lg.shadow-xl.transform.p-6.pt-4.mx-3.my-8.max-w-2xl > div.max-w-lg > div.mt-5.flex.justify-end.items-center.space-x-3 > button.btn.bg-primary.text-xs.w-full.sm\:w-20.uppercase.px-4.py-2.font-medium.text-white.hover\:bg-opacity-80';
-// await newTab.waitForSelector(confirmRemove, { state: 'visible' });
-// await newTab.click(confirmRemove);
+  // const confirmRemove = '#headlessui-dialog-17 > div > div.relative.inline-block.w-full.text-left.align-middle.cardColor.rounded-lg.shadow-xl.transform.p-6.pt-4.mx-3.my-8.max-w-2xl > div.max-w-lg > div.mt-5.flex.justify-end.items-center.space-x-3 > button.btn.bg-primary.text-xs.w-full.sm\:w-20.uppercase.px-4.py-2.font-medium.text-white.hover\:bg-opacity-80';
+  // await newTab.waitForSelector(confirmRemove, { state: 'visible' });
+  // await newTab.click(confirmRemove);
 
-// const addNew = '#app > div > div > div.flex-grow.overflow-auto > div > div.flex.justify-center > div > div > div.absolute.py-2.pl-2.pr-3.flex.justify-center.items-center.rounded-full.text-sm.bg-primary-80.hover\:bg-primary-100.text-white.cursor-pointer.bottom-14.right-3.shadow-xl';
-// await newTab.waitForSelector(addNew, { state: 'visible' });
-// await newTab.click(addNew);
+  // const addNew = '#app > div > div > div.flex-grow.overflow-auto > div > div.flex.justify-center > div > div > div.absolute.py-2.pl-2.pr-3.flex.justify-center.items-center.rounded-full.text-sm.bg-primary-80.hover\:bg-primary-100.text-white.cursor-pointer.bottom-14.right-3.shadow-xl';
+  // await newTab.waitForSelector(addNew, { state: 'visible' });
+  // await newTab.click(addNew);
 
-const copyAddress ='#receiveAddress > div > div.grow > button';
-await newTab.waitForSelector(copyAddress, { state: 'visible' });
-await newTab.click(copyAddress);
+  const copyAddress = '#receiveAddress > div > div.grow > button';
+  await newTab.waitForSelector(copyAddress, { state: 'visible' });
+  await newTab.click(copyAddress);
 
-const newTab2 = await browser.newPage();
-await newTab2.goto('https://docs.cardano.org/cardano-testnet/tools/faucet/');
+  const newTab2 = await browser.newPage();
+  await newTab2.goto('https://docs.cardano.org/cardano-testnet/tools/faucet/');
 
-(async () => {
-    const newTab2 = await browser.newPage();
-    await newTab2.goto('https://docs.cardano.org/cardano-testnet/tools/faucet/');
+  // let buttonVisible = false;
+  // while (!buttonVisible) {
+  //   // Scroll down a bit
+  //   await page.evaluate(() => window.scrollBy(0, window.innerHeight));
 
-    let buttonVisible = false;
-    while (!buttonVisible) {
-        // Scroll down a bit
-        await page.evaluate(() => window.scrollBy(0, window.innerHeight));
+  //   // Check if the 'Request Funds' button is visible
+  //   buttonVisible = await page.isVisible('#gatsby-focus-wrapper > div > div > div.css-14y15z9.eh2b2dx0 > div > main > div > div.pageWrap > div.css-12kimzl.egxxftl2 > div > form > div.MuiBox-root.jss9 > button > span.MuiButton-label');
 
-        // Check if the 'Request Funds' button is visible
-        buttonVisible = await page.isVisible('#gatsby-focus-wrapper > div > div > div.css-14y15z9.eh2b2dx0 > div > main > div > div.pageWrap > div.css-12kimzl.egxxftl2 > div > form > div.MuiBox-root.jss9 > button > span.MuiButton-label');
-        
-        // Optionally, add a delay here to throttle the scrolling
-        await page.waitForTimeout(100); // wait for 100 milliseconds
-    }
+  //   // Optionally, add a delay here to throttle the scrolling
+  //   await page.waitForTimeout(100); // wait for 100 milliseconds
+  // }
 
-// Navigate to the specified URL
-await page.goto('https://docs.cardano.org/cardano-testnet/tools/faucet/');
-
-// Keeping the browser open for debugging and verifying (remove the timeout or adjust as needed)
-await page.waitForTimeout(3000000); // Adjust the time as needed
-await new Promise(resolve => { /* never resolves */ });
-})});
+  // Keeping the browser open for debugging and verifying (remove the timeout or adjust as needed)
+  await page.waitForTimeout(3000000); // Adjust the time as needed
+  await new Promise(resolve => { /* never resolves */ });
+});

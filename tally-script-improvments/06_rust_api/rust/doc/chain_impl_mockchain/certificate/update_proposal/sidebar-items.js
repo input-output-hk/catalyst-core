22e4e3510cb8b9b22f703747798c39e274f76313
kukkok3 <kukkok3@users.noreply.github.com>
2024-02-05 13:32:21 +0000

    deploy: 642eb76ab8e418a65516d16888814a263f459326

---
+++ tally-script-improvments/06_rust_api/rust/doc/chain_impl_mockchain/certificate/update_proposal/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["UpdateProposal"],"type":["BftLeaderBindingSignature","UpdateProposerId"]};
---
+++ tally-script-improvments/06_rust_api/rust/doc/iapyx/load/scenario/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["artificial_users","node","servicing_station"]};

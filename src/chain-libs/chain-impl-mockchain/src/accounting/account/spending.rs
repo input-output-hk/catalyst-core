15b7764f8fdee83d6e6edc189b1d3325e423a937
cong-or <conchubhar.gannon@gmail.com>
2024-02-06 19:50:41 +0000

    fix(rm spending counter tests): audit tooling f11

--- src/chain-libs/chain-impl-mockchain/src/accounting/account/spending.rs
+++ src/chain-libs/chain-impl-mockchain/src/accounting/account/spending.rs
@@ -58,7 +58,7 @@ impl SpendingCounterIncreasing {
     /// an error reported.
     ///
     /// If the counter match succesfully, then the counter at this lane is incremented by one.
-    pub fn next_verify(&mut self, _counter: SpendingCounter) -> Result<(), Error> {
+    pub fn next_verify(&mut self, counter: SpendingCounter) -> Result<(), Error> {
         let actual_counter = self.nexts[counter.lane()];
 
         if actual_counter != counter {
@@ -73,7 +73,7 @@ impl SpendingCounterIncreasing {
     }
 
     /// Increases the spending counter on the given lane.
-    pub(crate) fn next_unchecked(&mut self, _unchecked_counter: SpendingCounter) {
+    pub(crate) fn next_unchecked(&mut self, unchecked_counter: SpendingCounter) {
         let lane = unchecked_counter.lane();
         let counter_to_update = self.nexts[lane];
         if counter_to_update != unchecked_counter {

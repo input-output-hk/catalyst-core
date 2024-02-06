c1924b557376e3decdd3dadc030e6ce196456de3
cong-or <conchubhar.gannon@gmail.com>
2024-02-06 18:19:33 +0000

    fix(ci): revert spending counter to fix legacy tests

--- src/chain-libs/chain-impl-mockchain/src/accounting/account/spending.rs
+++ src/chain-libs/chain-impl-mockchain/src/accounting/account/spending.rs
@@ -59,13 +59,33 @@ impl SpendingCounterIncreasing {
     ///
     /// If the counter match succesfully, then the counter at this lane is incremented by one.
     pub fn next_verify(&mut self, _counter: SpendingCounter) -> Result<(), Error> {
-        // spending counter has been removed
-        Ok(())
+        let actual_counter = self.nexts[counter.lane()];
+
+        if actual_counter != counter {
+            Err(Error::SpendingCredentialInvalid {
+                expected: actual_counter,
+                actual: counter,
+            })
+        } else {
+            self.next_unchecked(counter);
+            Ok(())
+        }
     }
 
     /// Increases the spending counter on the given lane.
     pub(crate) fn next_unchecked(&mut self, _unchecked_counter: SpendingCounter) {
-        // spending counter removed
+        let lane = unchecked_counter.lane();
+        let counter_to_update = self.nexts[lane];
+        if counter_to_update != unchecked_counter {
+            tracing::warn!(
+                "Invalid spending counter, {}",
+                Error::SpendingCredentialInvalid {
+                    expected: counter_to_update,
+                    actual: unchecked_counter,
+                }
+            );
+        }
+        self.nexts[lane] = counter_to_update.increment();
     }
 }
 

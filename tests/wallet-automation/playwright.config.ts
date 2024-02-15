443782a8d4a45c9d9b82101f0220cce1c305d6ce
alicechai <vasapornch@gmail.com>
2024-02-15 14:44:06 +0700

    fix: save state

--- tests/wallet-automation/playwright.config.ts
+++ tests/wallet-automation/playwright.config.ts
@@ -13,14 +13,22 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-  },
+    
+  },    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+
 
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'setup',
+      testMatch: [
+      /wallet\-setup\.ts/,
+      ],
+    },
+    {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+      dependencies: ['setup'],
+    }, 
 ]});

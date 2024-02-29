579db7c0d6dd575edbac8780da17a0be0f173f8e
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2024-02-29 11:59:09 +0100

    feat: adds fund100 to dev and prod

--- src/event-db/stage_data/prod/00001_fund100_event.sql
+++ src/event-db/stage_data/prod/00001_fund100_event.sql
@@ -1,4 +1,4 @@
--- F11
+-- F100
 INSERT INTO event (
     row_id,
     name,
@@ -26,25 +26,25 @@ INSERT INTO event (
     extra,
     cast_to
 ) VALUES (
-    11,
-    'Fund 11',
-    'Catalyst Testnet - Fund 11',
-    '2024-01-05 18:00:00',  -- Registration Snapshot Time
-    '2024-01-05 18:30:00',  -- Snapshot Start.
-    450000000,              -- Voting Power Threshold
+    100,
+    'Fund 100',
+    'Catalyst Testnet - Fund 100',
+    '2024-01-15 21:45:00',  -- Registration Snapshot Time
+    '2024-01-15 22:00:00',  -- Snapshot Start.
+    50000000,              -- Voting Power Threshold
     1,                      -- Max Voting Power PCT
     NULL,                   -- Review Rewards
-    '2024-01-04 08:00:00',  -- Start Time
+    '2024-02-28 04:00:00',  -- Start Time
     '2023-12-30 18:00:00',  -- End Time
-    '2024-01-04 08:00:00',  -- Insight Sharing Start
-    '2024-01-04 08:00:00',  -- Proposal Submission Start
-    '2024-01-04 08:00:00',  -- Refine Proposals Start
-    '2024-01-04 08:00:00',  -- Finalize Proposals Start
-    '2024-01-04 08:00:00',  -- Proposal Assessment Start
-    '2024-01-04 08:00:00',  -- Assessment QA Start
-    '2024-01-06 08:00:00',  -- Voting Starts
-    '2024-01-09 20:00:00',  -- Voting Ends
-    '2024-01-16 20:00:00',  -- Tallying Ends
+    '2024-02-28 04:00:00',  -- Insight Sharing Start
+    '2024-02-28 04:00:00',  -- Proposal Submission Start
+    '2024-02-28 04:00:00',  -- Refine Proposals Start
+    '2024-02-28 04:00:00',  -- Finalize Proposals Start
+    '2024-02-28 04:00:00',  -- Proposal Assessment Start
+    '2024-02-28 04:00:00',  -- Assessment QA Start
+    '2024-02-28 09:00:00',  -- Voting Starts
+    '2024-03-13 13:00:00',  -- Voting Ends
+    '2024-03-22 02:00:00',  -- Tallying Ends
     NULL,                   -- Block 0 Data
     NULL,                   -- Block 0 Hash
     1,                      -- Committee Size

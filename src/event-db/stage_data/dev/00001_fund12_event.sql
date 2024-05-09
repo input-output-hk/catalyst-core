37f55c6a57e385e18f43a329e364e44235bc7332
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2024-05-09 19:16:12 +0200

    feat: update times

--- src/event-db/stage_data/dev/00001_fund12_event.sql
+++ src/event-db/stage_data/dev/00001_fund12_event.sql
@@ -29,22 +29,22 @@ INSERT INTO event (
     12,
     'Fund 12',
     'Catalyst Testnet - Fund 12',
-    '2024-04-25 09:00:00',  -- Registration Snapshot Time
-    '2024-04-25 09:15:00',  -- Snapshot Start.
+    '2024-05-12 11:00:00',  -- Registration Snapshot Time
+    '2024-05-12 11:15:00',  -- Snapshot Start.
     50000000,              -- Voting Power Threshold
     1,                      -- Max Voting Power PCT
     NULL,                   -- Review Rewards
-    '2024-04-17 11:00:00',  -- Start Time
-    '2024-07-24 09:00:00',  -- End Time
+    '2024-05-02 15:00:00',  -- Start Time
+    '2024-06-13 07:00:00',  -- End Time
     '2024-04-17 11:00:00',  -- Insight Sharing Start
     '2024-04-17 11:00:00',  -- Proposal Submission Start
     '2024-04-17 11:00:00',  -- Refine Proposals Start
     '2024-04-17 11:00:00',  -- Finalize Proposals Start
     '2024-04-17 11:00:00',  -- Proposal Assessment Start
     '2024-04-17 11:00:00',  -- Assessment QA Start
-    '2024-04-25 14:00:00',  -- Voting Starts
-    '2024-04-27 11:00:00',  -- Voting Ends
-    '2024-07-24 09:00:00',  -- Tallying Ends
+    '2024-05-13 11:00:00',  -- Voting Starts
+    '2024-05-17 10:00:00',  -- Voting Ends
+    '2024-06-13 07:00:00',  -- Tallying Ends
     NULL,                   -- Block 0 Data
     NULL,                   -- Block 0 Hash
     1,                      -- Committee Size

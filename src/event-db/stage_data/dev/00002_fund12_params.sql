95b7ca5d81e02ebcaf43e18564309d7bf0139fc4
Stefano Cunego <93382903+kukkok3@users.noreply.github.com>
2024-07-11 18:08:03 +0200

    chore: restore event db dev times | NPG-000 (#705)
    # Description
    
    Please include a summary of the changes and the related issue.
    Please also include relevant motivation and context.
    List any dependencies that are required for this change.
    
    Fixes # (issue) _if applicable_
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce.
    Please also list any relevant details for your test configuration
    
    - [ ] Test A
    - [ ] Test B
    
    **Test Configuration**: _if applicable_
    
    - Firmware version:
    - Hardware:
    - Toolchain:
    - SDK:
    
    **List of new dependencies**: _if applicable_
    
    Provide a list of newly added dependencies in this PR, with the
    description of what are they doing and why do we need them.
    
    - Crate A: description
    - Crate B: description
    
    ## Checklist
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged and published in downstream
    modules

--- src/event-db/stage_data/dev/00002_fund12_params.sql
+++ src/event-db/stage_data/dev/00002_fund12_params.sql
@@ -1,6 +1,61 @@
 -- Define F12 IdeaScale parameters.
-DELETE FROM config WHERE id2='100';
+INSERT INTO config (id, id2, id3, value) VALUES (
+    'ideascale',
+    '12',
+    '',
+    '{
+        "group_id": 31051,
+        "review_stage_ids": [143, 145],
+        "nr_allocations": [30, 80],
+        "campaign_group_id": 63,
+        "questions": {
+            "You are reviewing the positive IMPACT this project will have on the Cardano Ecosystem.\nHas this project clearly demonstrated in all aspects of the proposal that it will have a positive impact on the Cardano Ecosystem?": "Impact / Alignment",
+            "You are reviewing the FEASIBILITY of this project.\nIs this project feasible based on the proposal submitted? Does the plan and associated budget and milestones look achievable? Does the team have the skills, experience, capability and capacity to complete the project successfully?": "Feasibility",
+            "You are reviewing the VALUE FOR MONEY this represents for the Treasury and the Community\nIs the funding amount requested for this project reasonable and does it provide good Value for Money to the Treasury?": "Auditability"
+        },
+        "stage_ids": [4590, 4596, 4602, 4608, 4614, 4620, 4626, 4632, 4638, 4644, 4650, 4656, 4662, 4591, 4597, 4603, 4609, 4615, 4621, 4627, 4633, 4639, 4645, 4651, 4657, 4663, 4592, 4598, 4604, 4610, 4616, 4622, 4628, 4634, 4640, 4646, 4652, 4658, 4664],
+        "proposals": {
+            "field_mappings": {
+                "proposer_url": ["relevant_link_1", "website__github_repository__or_any_other_relevant_link__", "relevant_link_3"],
+                "proposer_relevant_experience": "f11_project_team",
+                "public_key": "ada_payment_address__",
+                "funds": ["f11_requested_funds", "requested_funds_in_ada","requested_funds_coti"]
+            },
+            "extra_field_mappings": {
+                "metrics": "key_metrics_to_measure",
+                "goal": "how_does_success_look_like_",
+                "solution": "f11_proposal_solution",
+                "brief": "challenge_brief",
+                "importance": "importance",
+                "full_solution": "please_describe_your_proposed_solution",
+                "team_details":  "please_provide_details_of_the_people_who_will_work_on_the_project_",
+                "auto_translated": "auto_translated",
+                "budget_breakdown": "please_provide_a_detailed_budget_breakdown",
+                "challenges_or_risks": "what_main_challenges_or_risks_do_you_foresee_to_deliver_this_project_successfully_",
+                "timeline_and_key_milestones": "please_provide_a_detailed_plan__a_timeline__and_key_milestones_for_delivering_your_proposal_",
+                "how_solution_address_challenge": "please_describe_how_your_proposed_solution_will_address_the_challenge_",
+                "sdg_rating": "sdg_rating",
+                "return_in_a_later_round": "if_you_are_funded__will_you_return_to_catalyst_in_a_later_round_for_further_funding__please_explain",
+                "relevant_link_1": "relevant_link_1",
+                "relevant_link_2": "website__github_repository__or_any_other_relevant_link__",
+                "relevant_link_3": "relevant_link_3",
+                "progress_metrics": "what_will_you_measure_to_track_your_project_s_progress__and_how_will_you_measure_it_",
+                "new_proposal": "is_this_proposal_is_a_continuation_of_a_previously_funded_project_in_catalyst__or_an_entirely_new_o"
+            }
+        },
+        "proposals_scores_csv": {
+            "id_field": "proposal_id",
+            "score_field": "Rating"
+        }
+     }'
+) ON CONFLICT (id, id2, id3) DO UPDATE
+SET value = EXCLUDED.value;
 
 -- Use F12 params for event with row_id = 12.
-DELETE FROM config WHERE id3='100';
-
+INSERT INTO config (id, id2, id3, value) VALUES (
+    'event',
+    'ideascale_params',
+    '12',
+    '{"params_id": "F12"}'
+) ON CONFLICT (id, id2, id3) DO UPDATE
+SET value = EXCLUDED.value;

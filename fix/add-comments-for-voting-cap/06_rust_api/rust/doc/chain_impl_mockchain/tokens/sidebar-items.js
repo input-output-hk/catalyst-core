f8a5afef56d071b4e982711668b3c7b648629c79
kukkok3 <kukkok3@users.noreply.github.com>
2024-08-09 08:02:51 +0000

    Removed Draft Docs for fix/add-comments-for-voting-cap

--- fix/add-comments-for-voting-cap/06_rust_api/rust/doc/chain_impl_mockchain/tokens/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["identifier","minting_policy","name","policy_hash"]};
--- fix/add-comments-for-voting-cap/06_rust_api/rust/doc/jormungandr_automation/jcli/command/genesis/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["decode","encode","hash"],"struct":["GenesisCommand"]};

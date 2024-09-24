c6949a982d8aec64bc9cdeb563aa71e5d241f9fd
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2024-09-24 14:16:21 +0200

    chore: restore fund12 times

--- src/event-db/stage_data/dev/00001_fund12_event.sql
+++ src/event-db/stage_data/dev/00001_fund12_event.sql
@@ -1,4 +1,4 @@
--- F13
+-- F12
 INSERT INTO event (
     row_id,
     name,
@@ -26,25 +26,25 @@ INSERT INTO event (
     extra,
     cast_to
 ) VALUES (
-    13,
-    'Fund 13',
-    'Catalyst Testnet - Fund 13',
-    '2024-10-12 11:00:00',  -- Registration Snapshot Time
-    '2024-10-12 11:15:00',  -- Snapshot Start.
-    2500000,              -- Voting Power Threshold
-    100,                      -- Max Voting Power PCT
+    12,
+    'Fund 12',
+    'Catalyst Testnet - Fund 12',
+    '2024-05-12 11:00:00',  -- Registration Snapshot Time
+    '2024-05-12 11:15:00',  -- Snapshot Start.
+    50000000,              -- Voting Power Threshold
+    1,                      -- Max Voting Power PCT
     NULL,                   -- Review Rewards
-    '2024-09-20 10:00:00',  -- Start Time
-    '2024-12-20 10:00:00',  -- End Time
-    '2024-09-20 11:00:00',  -- Insight Sharing Start
-    '2024-09-20 11:00:00',  -- Proposal Submission Start
-    '2024-09-20 11:00:00',  -- Refine Proposals Start
-    '2024-09-20 11:00:00',  -- Finalize Proposals Start
-    '2024-09-20 11:00:00',  -- Proposal Assessment Start
-    '2024-09-20 11:00:00',  -- Assessment QA Start
-    '2024-10-13 11:00:00',  -- Voting Starts
-    '2024-10-13 20:00:00',  -- Voting Ends
-    '2024-12-20 10:00:00',  -- Tallying Ends
+    '2024-05-02 15:00:00',  -- Start Time
+    '2024-06-13 07:00:00',  -- End Time
+    '2024-04-17 11:00:00',  -- Insight Sharing Start
+    '2024-04-17 11:00:00',  -- Proposal Submission Start
+    '2024-04-17 11:00:00',  -- Refine Proposals Start
+    '2024-04-17 11:00:00',  -- Finalize Proposals Start
+    '2024-04-17 11:00:00',  -- Proposal Assessment Start
+    '2024-04-17 11:00:00',  -- Assessment QA Start
+    '2024-05-13 11:00:00',  -- Voting Starts
+    '2024-05-17 10:00:00',  -- Voting Ends
+    '2024-06-13 07:00:00',  -- Tallying Ends
     NULL,                   -- Block 0 Data
     NULL,                   -- Block 0 Hash
     1,                      -- Committee Size

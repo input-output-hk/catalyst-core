50d9065b4b7daab9182e316daeef5081dd19427e
kukkok3 <kukkok3@users.noreply.github.com>
2024-10-21 09:50:06 +0000

    deploy: 2ffecd06a193a2471b07175441a42dfe1d09b355

--- chore/update-dev-snapshot-times/06_rust_api/rust/doc/implementors/chain_core/property/trait.Header.js
+++ chore/update-dev-snapshot-times/06_rust_api/rust/doc/implementors/chain_core/property/trait.Header.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"chain_impl_mockchain":[["impl <a class=\"trait\" href=\"chain_core/property/trait.Header.html\" title=\"trait chain_core::property::Header\">Header</a> for <a class=\"enum\" href=\"chain_impl_mockchain/header/header/enum.Header.html\" title=\"enum chain_impl_mockchain::header::header::Header\">Header</a>"]]
+"chain_impl_mockchain":[["impl Header for <a class=\"enum\" href=\"chain_impl_mockchain/header/header/enum.Header.html\" title=\"enum chain_impl_mockchain::header::header::Header\">Header</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

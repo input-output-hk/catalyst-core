2ffecd06a193a2471b07175441a42dfe1d09b355
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2024-10-21 11:41:52 +0200

    chore: update times

--- src/event-db/stage_data/dev/00001_fund13_event.sql
+++ src/event-db/stage_data/dev/00001_fund13_event.sql
@@ -34,17 +34,17 @@ INSERT INTO event (
     25000000,              -- Voting Power Threshold
     100,                      -- Max Voting Power PCT
     NULL,                   -- Review Rewards
-    '2024-10-21 10:00:00',  -- Start Time
+    '2024-10-21 11:00:00',  -- Start Time
     '2024-10-30 10:00:00',  -- End Time
-    '2024-10-21 10:00:00',  -- Insight Sharing Start
-    '2024-10-21 10:00:00',  -- Proposal Submission Start
-    '2024-10-21 10:00:00',  -- Refine Proposals Start
-    '2024-10-21 10:00:00',  -- Finalize Proposals Start
-    '2024-10-21 10:00:00',  -- Proposal Assessment Start
-    '2024-10-21 10:00:00',  -- Assessment QA Start
+    '2024-10-21 11:00:00',  -- Insight Sharing Start
+    '2024-10-21 11:00:00',  -- Proposal Submission Start
+    '2024-10-21 11:00:00',  -- Refine Proposals Start
+    '2024-10-21 11:00:00',  -- Finalize Proposals Start
+    '2024-10-21 11:00:00',  -- Proposal Assessment Start
+    '2024-10-21 11:00:00',  -- Assessment QA Start
     '2024-10-23 08:00:00',  -- Voting Starts
-    '2024-10-24 20:00:00',  -- Voting Ends
-    '2024-10-30 10:00:00',  -- Tallying Ends
+    '2024-10-25 00:00:00',  -- Voting Ends
+    '2024-10-30 09:00:00',  -- Tallying Ends
     NULL,                   -- Block 0 Data
     NULL,                   -- Block 0 Hash
     1,                      -- Committee Size

0b529212498787dd735256f08cdad24c2734dbf4
kukkok3 <kukkok3@users.noreply.github.com>
2024-11-12 14:11:05 +0000

    deploy: 57a93c8cc90f4c5924622614f4d775b2ef6d39cd

---
+++ chore/update-fund13-dev-times-again/06_rust_api/rust/doc/chain_impl_mockchain/chaintypes/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ConsensusType"],"mod":["tests"],"struct":["ChainLength"],"type":["ConsensusVersion","HeaderId"]};
---
+++ chore/update-fund13-dev-times-again/06_rust_api/rust/doc/vitup/cli/start/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["MockError"],"mod":["advanced","mock","quick"],"struct":["AdvancedStartCommandArgs","MockFarmCommand","MockStartCommandArgs","QuickStartCommandArgs"]};

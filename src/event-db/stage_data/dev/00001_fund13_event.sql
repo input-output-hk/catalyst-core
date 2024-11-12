b4a8b5e5a1147cac2dff9c825e2756ef64aa1536
Stefano Cunego <93382903+kukkok3@users.noreply.github.com>
2024-11-12 15:24:00 +0100

    chore: update dryrun times | NPG-000 (#723)
    # Description
    
    Please include a summary of the changes and the related issue.
    Please also include relevant motivation and context.
    List any dependencies that are required for this change.
    
    Fixes # (issue) _if applicable_
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce.
    Please also list any relevant details for your test configuration
    
    - [ ] Test A
    - [ ] Test B
    
    **Test Configuration**: _if applicable_
    
    - Firmware version:
    - Hardware:
    - Toolchain:
    - SDK:
    
    **List of new dependencies**: _if applicable_
    
    Provide a list of newly added dependencies in this PR, with the
    description of what are they doing and why do we need them.
    
    - Crate A: description
    - Crate B: description
    
    ## Checklist
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged and published in downstream
    modules

--- src/event-db/stage_data/dev/00001_fund13_event.sql
+++ src/event-db/stage_data/dev/00001_fund13_event.sql
@@ -29,22 +29,22 @@ INSERT INTO event (
     13,
     'Fund 13',
     'Catalyst Testnet - Fund 13',
-    '2024-11-15 08:00:00',  -- Registration Snapshot Time
-    '2024-11-15 08:15:00',  -- Snapshot Start.
+    '2024-11-07 11:00:00',  -- Registration Snapshot Time
+    '2024-11-07 11:15:00',  -- Snapshot Start.
     25000000,              -- Voting Power Threshold
     100,                      -- Max Voting Power PCT
     NULL,                   -- Review Rewards
-    '2024-11-06 13:00:00',  -- Start Time
-    '2024-12-07 19:00:00',  -- End Time
-    '2024-11-06 13:00:00',  -- Insight Sharing Start
-    '2024-11-06 13:00:00',  -- Proposal Submission Start
-    '2024-11-06 13:00:00',  -- Refine Proposals Start
-    '2024-11-06 13:00:00',  -- Finalize Proposals Start
-    '2024-11-06 13:00:00',  -- Proposal Assessment Start
-    '2024-11-06 13:00:00',  -- Assessment QA Start
-    '2024-11-15 12:00:00',  -- Voting Starts
-    '2024-11-18 15:00:00',  -- Voting Ends
-    '2024-12-07 19:00:00',  -- Tallying Ends
+    '2024-11-06 12:00:00',  -- Start Time
+    '2024-11-27 03:00:00',  -- End Time
+    '2024-10-21 10:00:00',  -- Insight Sharing Start
+    '2024-10-21 10:00:00',  -- Proposal Submission Start
+    '2024-10-21 10:00:00',  -- Refine Proposals Start
+    '2024-10-21 10:00:00',  -- Finalize Proposals Start
+    '2024-10-21 10:00:00',  -- Proposal Assessment Start
+    '2024-11-06 12:00:00',  -- Assessment QA Start
+    '2024-11-08 09:00:00',  -- Voting Starts
+    '2024-11-12 13:00:00',  -- Voting Ends
+    '2024-11-27 03:00:00',  -- Tallying Ends
     NULL,                   -- Block 0 Data
     NULL,                   -- Block 0 Hash
     1,                      -- Committee Size

5380dfc67c49bcf865eebd1b8fa00278a922472d
kukkok3 <kukkok3@users.noreply.github.com>
2025-05-15 13:34:35 +0000

    Removed Draft Docs for chore/update-preprod-cont-snap

--- chore/update-preprod-cont-snap/06_rust_api/rust/doc/iapyx_load/load/constant/count/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["ProgressBarMode"],"fn":["parse_progress_bar_mode_from_str"],"struct":["ConstantCountIapyxLoadCommand"]};
--- chore/update-preprod-cont-snap/06_rust_api/rust/doc/mainnet_tools/cardano_cli/mock/command/stake_address/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["StakeAddress"],"struct":["BuildCommand","RegistrationCertificateCommand"]};

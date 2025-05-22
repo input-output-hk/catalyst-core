cdba23d837bc41f441e0c1ba4dc73d84deb7198a
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2025-05-22 11:00:23 +0200

    chore: update eventdb migrations

--- src/event-db/stage_data/preprod/00001_fund14_event.sql
+++ src/event-db/stage_data/preprod/00001_fund14_event.sql
@@ -29,22 +29,22 @@ INSERT INTO event (
     14,
     'Fund 14',
     'Catalyst Testnet - Fund 14',
-    '2025-05-16 12:00:00',  -- Registration Snapshot Time
-    '2025-05-16 12:15:00',  -- Snapshot Start.
+    '2025-05-23 12:00:00',  -- Registration Snapshot Time
+    '2025-05-23 12:15:00',  -- Snapshot Start.
     25000000,              -- Voting Power Threshold
     100,                      -- Max Voting Power PCT
     NULL,                   -- Review Rewards
-    '2025-05-12 11:00:00',  -- Start Time
-    '2025-06-06 21:00:00',  -- End Time
-    '2025-05-12 11:00:00',  -- Insight Sharing Start
-    '2025-05-12 11:00:00',  -- Proposal Submission Start
-    '2025-05-12 11:00:00',  -- Refine Proposals Start
-    '2025-05-12 11:00:00',  -- Finalize Proposals Start
-    '2025-05-12 11:00:00',  -- Proposal Assessment Start
-    '2025-05-12 11:00:00',  -- Assessment QA Start
-    '2025-05-18 17:00:00',  -- Voting Starts
-    '2025-06-04 09:00:00',  -- Voting Ends
-    '2025-06-06 21:00:00',  -- Tallying Ends
+    '2025-05-21 11:00:00',  -- Start Time
+    '2025-06-15 21:00:00',  -- End Time
+    '2025-05-21 11:00:00',  -- Insight Sharing Start
+    '2025-05-21 11:00:00',  -- Proposal Submission Start
+    '2025-05-21 11:00:00',  -- Refine Proposals Start
+    '2025-05-21 11:00:00',  -- Finalize Proposals Start
+    '2025-05-21 11:00:00',  -- Proposal Assessment Start
+    '2025-05-21 11:00:00',  -- Assessment QA Start
+    '2025-05-27 17:00:00',  -- Voting Starts
+    '2025-06-13 09:00:00',  -- Voting Ends
+    '2025-06-15 21:00:00',  -- Tallying Ends
     NULL,                   -- Block 0 Data
     NULL,                   -- Block 0 Hash
     1,                      -- Committee Size

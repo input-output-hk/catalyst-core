61c54d1c9b2ed106ae18bba6690ea48897158d71
jmgilman <jmgilman@users.noreply.github.com>
2025-07-10 23:14:14 +0000

    deploy: 46a38098a7ec7b07e8098f41d64615c0038c3c22

---
+++ updates-preprod-dates/06_rust_api/rust/doc/chain_impl_mockchain/certificate/test/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["arbitrary_decrypted_private_tally"]};
---
+++ updates-preprod-dates/06_rust_api/rust/doc/chain_network/grpc/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["client","convert","legacy","proto","server","streaming","watch"]};

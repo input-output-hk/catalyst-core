921c5e90d3ac2fc507c0edef722219f6424b67dc
kukkok3 <kukkok3@users.noreply.github.com>
2025-07-22 20:21:23 +0000

    Removed Draft Docs for chore/updates-forge-180

--- chore/updates-forge-180/06_rust_api/rust/doc/vit_servicing_station_lib/db/schema/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["api_tokens","challenges","community_advisors_reviews","contributions","funds","goals","groups","proposal_community_choice_challenge","proposal_simple_challenge","proposals","proposals_voteplans","snapshots","voteplans","voters","votes"]};
